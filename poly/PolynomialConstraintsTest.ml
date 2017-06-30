863e7f5e1c4c81d1bc62c5c8db31593804bd1263
Marcel Hark <marcel.hark@rwth-aachen.de>
2017-06-30 15:47:00 +0200

    Examples added

--- poly/PolynomialConstraintsTest.ml
+++ poly/PolynomialConstraintsTest.ml
@@ -31,16 +31,31 @@ let () =
                     Printf.printf "poly 1 >= poly 2 : %s \n" (PolynomialConstraintsAtoms.to_string greater_equal);
                     Printf.printf "poly 1 >= poly 2 in Z3 :\n %s \n" (Z3.Expr.to_string (PolynomialConstraintsAtoms.to_z3 ctx greater_equal));
                     Printf.printf "poly 2 = poly 3 : %s \n" (PolynomialConstraintsAtoms.to_string equal);
-                    Printf.printf "poly 2 = poly3 in Z3 :\n %s \n" (Z3.Expr.to_string (PolynomialConstraintsAtoms.to_z3 ctx equal))
+                    Printf.printf "poly 2 = poly3 in Z3 :\n %s \n" (Z3.Expr.to_string (PolynomialConstraintsAtoms.to_z3 ctx equal));
+                    Printf.printf "The variables in equal are : %s \n" (Variables.varlist_to_string (PolynomialConstraintsAtoms.get_variables equal));
                     
-                    (*let varmapping = VarMap.empty in
+                    let varmapping = VarMap.empty in
                     let varmapping = VarMap.add "x" "a" varmapping in
                     let varmapping = VarMap.add "y" "b" varmapping in
-                    let varmapping = VarMap.add "z" "c" varmapping in  
+                    let varmapping = VarMap.add "z" "c" varmapping in 
+                        let greater_equal_ren = PolynomialConstraintsAtoms.rename_vars varmapping greater_equal in
+                        let equal_ren = PolynomialConstraintsAtoms.rename_vars varmapping equal in
+                        Printf.printf "poly 1 >= poly 2 : %s \n" (PolynomialConstraintsAtoms.to_string greater_equal_ren);
+                        Printf.printf "poly 1 >= poly 2 in Z3 :\n %s \n" (Z3.Expr.to_string (PolynomialConstraintsAtoms.to_z3 ctx greater_equal_ren));
+                        Printf.printf "poly 2 = poly 3 : %s \n" (PolynomialConstraintsAtoms.to_string equal_ren);
+                        Printf.printf "poly 2 = poly3 in Z3 :\n %s \n" (Z3.Expr.to_string (PolynomialConstraintsAtoms.to_z3 ctx equal_ren));
+                        Printf.printf "The variables in equal_ren are : %s \n" (Variables.varlist_to_string (PolynomialConstraintsAtoms.get_variables equal_ren));
                     
-                    let intmapping = VarMap.empty in
-                    let intmapping = VarMap.add "x" (Big_int.big_int_of_int 2) intmapping in
-                    let intmapping = VarMap.add "y" (Big_int.big_int_of_int 5) intmapping in
-                    let intmapping = VarMap.add "z" (Big_int.big_int_of_int 3) intmapping in  *)
-                      
+                        let intmapping = VarMap.empty in
+                        let intmapping = VarMap.add "x" (Big_int.big_int_of_int 2) intmapping in
+                        let intmapping = VarMap.add "y" (Big_int.big_int_of_int 5) intmapping in
+                        let intmapping = VarMap.add "z" (Big_int.big_int_of_int 3) intmapping in 
+        
+                            let greater_equal_in = PolynomialConstraintsAtoms.instantiate_with_big_int intmapping greater_equal in
+                            let equal_in = PolynomialConstraintsAtoms.instantiate_with_big_int intmapping equal in
+                            Printf.printf "poly1 evaluates to : %s\n" (Big_int.string_of_big_int (Polynomials.instantiate_with_big_int intmapping poly1)); 
+                            Printf.printf "poly2 evaluates to : %s\n" (Big_int.string_of_big_int (Polynomials.instantiate_with_big_int intmapping poly2)); 
+                            Printf.printf "poly3 evaluates to : %s\n" (Big_int.string_of_big_int (Polynomials.instantiate_with_big_int intmapping poly3)); 
+                            Printf.printf "poly 1 >= poly 2 : %B \n" (greater_equal_in);
+                            Printf.printf "poly 2 = poly 3 : %B \n" (equal_in)
 ;;
