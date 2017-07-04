70654b87b4bd69e6821cd6c0346d00137161aad1
Marcel Hark <marcel.hark@rwth-aachen.de>
2017-07-04 14:17:55 +0200

    directory constraints included into OMakefiles

--- constraints/PolynomialConstraintsAtoms.mli
+++ constraints/PolynomialConstraintsAtoms.mli
@@ -1,5 +1,5 @@
 type constraint_atom =
-  |GreaterThan of Polynomials.t * Polynomials.t
+    GreaterThan of Polynomials.t * Polynomials.t
   | GreaterEqual of Polynomials.t * Polynomials.t
   | LessThan of Polynomials.t * Polynomials.t
   | LessEqual of Polynomials.t * Polynomials.t
@@ -13,10 +13,12 @@ val mk_lt : Polynomials.t -> Polynomials.t -> constraint_atom
 val mk_le : Polynomials.t -> Polynomials.t -> constraint_atom
 val mk_eq : Polynomials.t -> Polynomials.t -> constraint_atom
 val mk_neq : Polynomials.t -> Polynomials.t -> constraint_atom
-val one : ScaledMonomials.scaled_mon list
+val one : Polynomials.t
 val remove_strictness : constraint_atom -> constraint_atom
 val to_string : constraint_atom -> string
 val to_z3 : Z3.context -> constraint_atom -> Z3.Expr.expr
 val get_variables : constraint_atom -> Variables.t list
-val rename_vars : string Mapping.VarMap.t -> constraint_atom -> constraint_atom
-val instantiate_with_big_int : Big_int.big_int Mapping.VarMap.t -> constraint_atom -> bool
+val rename_vars :
+  string Mapping.VarMap.t -> constraint_atom -> constraint_atom
+val instantiate_with_big_int :
+  Big_int.big_int Mapping.VarMap.t -> constraint_atom -> bool
