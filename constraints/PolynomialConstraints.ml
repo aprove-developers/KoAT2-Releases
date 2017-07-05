a4cdf09af8438c49e865871ae1ab071c95c8963a
Marcel Hark <marcel.hark@rwth-aachen.de>
2017-07-05 16:42:39 +0200

    PolynomialConstraints type is now called t

--- constraints/PolynomialConstraints.ml
+++ constraints/PolynomialConstraints.ml
@@ -1,8 +1,8 @@
 (*Polynomial Constraints are conjunctions of PolynomialConstraintsAtoms*)
-type constraints = PolynomialConstraintsAtoms.constraint_atom list
+type t = PolynomialConstraintsAtoms.constraint_atom list
 
-let to_string (constr : constraints) =
+let to_string (constr : t) =
     let atom_strings = List.concat " /\ " (List.map PolynomialConstraintsAtoms.to_string constr)
     
-let to_z3 (ctx : Z3.context) (constr : constraints) =
+let to_z3 (ctx : Z3.context) (constr : t) =
     mk_and ctx (List.map (PolynomialConstraintsAtoms.to_z3 ctx) constr)
