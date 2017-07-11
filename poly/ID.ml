dd9e4aa6ea5029d7992500fb011f43d605699a5a
Fabian Boeller <fabianchina@googlemail.com>
2017-07-11 09:23:17 +0200

    Added PrePostID module

--- poly/ID.ml
+++ poly/ID.ml
@@ -17,3 +17,25 @@ module StringID =
     let to_string var = var
     let compare = String.compare
   end
+
+module PrePostID =
+  struct
+    type t = Pre of StringID.t | Post of StringID.t
+                                       
+    let (==) a b = match (a, b) with
+        (Pre _, Post _) -> false
+      | (Post _, Pre _) -> false
+      | (Pre id1, Pre id2) -> id1 == id2
+      | (Post id1, Post id2) -> id1 == id2
+                              
+    let of_string str =
+      if String.ends_with str "'" then
+        Post (String.rchop str)
+      else Pre str
+      
+    let to_string = function
+        Pre str -> str
+      | Post str -> String.concat "" [str;"'"]
+                  
+    let compare a b = 0 (* TODO *)
+  end
