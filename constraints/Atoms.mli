e1d357acf511bb9ea5d7610de5c0b4de6da9bcb7
Fabian Boeller <fabianchina@googlemail.com>
2017-08-16 23:55:15 +0200

    Added comment for constraint atom

--- constraints/Atoms.mli
+++ constraints/Atoms.mli
@@ -2,6 +2,8 @@ open Batteries
 open PolyTypes
 open ConstraintTypes
 
-(** TODO Internal *)
+(** Provides default implementations of an atom *)
+
+(** Constructs a default constraint using a simple record *)
 module Make(P : Polynomial) : Atom with   
          module Polynomial_ = P
