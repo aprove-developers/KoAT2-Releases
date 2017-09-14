ca4d04af34f0c75e94e3985c95b4279167ed162e
Marcel Hark <marcel.hark@rwth-aachen.de>
2017-09-14 17:18:54 +0200

    reimplemented buggy smt solving method, added tests

--- src/smt/SMT.ml
+++ src/smt/SMT.ml
@@ -8,6 +8,8 @@ module type Solver =
     module Constraint : ConstraintTypes.Constraint
 
     val satisfiable : Constraint.t -> bool
+    
+    (*val to_string : Constraint.t -> string*)
   end
 
 (** Constructs an SMT solver which uses the microsoft project Z3 *)
@@ -26,7 +28,10 @@ module MakeZ3Solver(C : ConstraintTypes.Constraint) : (Solver with module Constr
 
     let from_constraint (constraints : Constraint.t) =
       Constraint.fold ~const:(fun value -> Z3.Arithmetic.Integer.mk_numeral_i !context (Polynomial.Value.to_int value))
-                      ~var:(fun var -> Z3.Arithmetic.Integer.mk_const_s !context (Polynomial.Var.to_string var))
+                      ~var:(fun var ->  if (Polynomial.Var.is_helper var) then 
+                                            Z3.Arithmetic.Real.mk_const_s !context (Polynomial.Var.to_string var) 
+                                        else
+                                            Z3.Arithmetic.Integer.mk_const_s !context (Polynomial.Var.to_string var))
                       ~neg:(Z3.Arithmetic.mk_unary_minus !context)
                       ~plus:(fun p1 p2 -> Z3.Arithmetic.mk_add !context [p1; p2])
                       ~times:(fun p1 p2 -> Z3.Arithmetic.mk_mul !context [p1; p2])
@@ -35,11 +40,24 @@ module MakeZ3Solver(C : ConstraintTypes.Constraint) : (Solver with module Constr
                       ~correct:(Z3.Boolean.mk_true !context)
                       ~conj:(fun a1 a2 -> Z3.Boolean.mk_and !context [a1; a2])
                       constraints
-      
+                      
+(*    let to_string (constraints : Constraint.t) =
+        let solver = Z3.Solver.mk_simple_solver !context in
+        let formula = from_constraint constraints in
+        let optimisation_goal = Z3.Optimize.mk_opt !context in
+        Z3.Optimize.add optimisation_goal [formula];
+        Z3.Optimize.to_string optimisation_goal*)
+
+        
+    (** checks if there exists a satisfying assignment for a given constraint
+        uses Z3 optimisation methods*)
     let satisfiable (constraints : Constraint.t) =
       let solver = Z3.Solver.mk_simple_solver !context in
       let formula = from_constraint constraints in
-      Z3.Solver.add solver [formula];
-      (Z3.Solver.check solver []) == Z3.Solver.SATISFIABLE
+      let optimisation_goal = Z3.Optimize.mk_opt !context in
+      Z3.Optimize.add optimisation_goal [formula];
+      (*Z3.Solver.add solver [formula];
+      (Z3.Solver.check solver []) == Z3.Solver.SATISFIABLE*)
+      (Z3.Optimize.check optimisation_goal) == Z3.Solver.SATISFIABLE
 
   end
--- src/test/SMTTest.ml
+++ src/test/SMTTest.ml
@@ -2,17 +2,42 @@ open Batteries
 open OUnit2
 open Helper
    
-module Z3Solver = SMT.MakeZ3Solver(Constraints.Make(PolyImpl.Polynomial))
+module Constraints = Constraints.Make(PolyImpl.Polynomial)
+module Z3Solver = SMT.MakeZ3Solver(Constraints)
 module Reader = Readers.Make(ProgramImpl.StdProgram)
 
+let print_str (str : string) = str
+
 let suite =
-  "Satisfiable" >::: (
-    List.map (fun (testname, constr) ->
-        testname >:: (fun _ -> assert_true (Z3Solver.satisfiable (Reader.read_constraint constr))))
-             [
-               ("Empty", "");
-               ("Constant Equality", "1 = 1");
-               ("Variable Equality", "x = x");
-               (* Should work? ("Different Variable Equality", "x = y"); *)
-             ]
-  )
+  "SMT Tests" >::: [
+        (*"from_constraint" >::: (
+        List.map (fun (testname, expected, constr) ->
+            testname >:: (fun _ -> assert_equal ~cmp:String.equal ~printer:print_str expected (Z3Solver.to_string (Reader.read_constraint constr))))
+                    [
+                        ("Variable Equality","Hallo", "x = y");
+                        
+                    ]
+        );*)  
+        "Satisfiable" >::: (
+        List.map (fun (testname,expected, constr) ->
+            testname >:: (fun _ -> assert_equal_bool expected (Z3Solver.satisfiable (Reader.read_constraint constr))))
+                    [
+                    ("Empty",true, "");
+                    ("Constant Equality",true, "1 = 1");
+                    ("Variable Equality",true, "x = x");
+                    ("Different Variable Equality",true, "x = y"); 
+                    ("Obvious contradiction", false, "0 = 1");
+                    ("Contradiction over the integers", false, "x>x^2");
+                    
+                    ]
+        );
+        "Satisfiable_Farkas" >::: (
+        List.map (fun (expected,constr,atom) ->
+            constr >:: (fun _ -> assert_equal_bool expected (Z3Solver.satisfiable (Constraints.farkas_transform (Reader.read_constraint constr) (Reader.read_atom atom)))))
+                    [
+                    (true, "x>=0", "x>=0");
+                    (false, "x>=0", "x < -10");
+                    (true ,"x >= 0 && y>=0 && x+y <= 4", "2*x+y >= 0");
+                    ]
+        );
+  ]
