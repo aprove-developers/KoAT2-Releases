2daf4a3d219ffa31e0a28e072d6dbab6e475b636
Fabian Boeller <fabianchina@googlemail.com>
2017-09-29 21:58:15 +0200

    Fixed indentation of transitionsystem

--- src/transitionsystem/Program.ml
+++ src/transitionsystem/Program.ml
@@ -1,223 +1,223 @@
 open Batteries
 
-    module TransitionLabel =
-      struct
-        module Guard = Constraints.Make(Polynomials.Make(PolyTypes.OurInt))         
-        module Polynomial = Polynomials.Make(PolyTypes.OurInt)
-        module Map = Map.Make(Var)
-        module Bound = MinMaxPolynomial.Make(Polynomials.Make(PolyTypes.OurInt))
-                          
-        exception RecursionNotSupported
-                
-        type kind = Lower | Upper [@@deriving eq, ord]
-
-        type t = {
-            name : string;
-            start : string;
-            target : string;
-            update : Polynomial.t Map.t;
-            guard : Guard.t;
-            (* TODO Transitions should have costs *)
-          }
-
-        let make ~name ~start ~target ~update ~guard = {
-            name; start; target; update; guard
-          }
-          
-               
-        (* TODO Pattern <-> Assigment relation *)
-        let mk ~name ~start ~targets ~patterns ~guard ~vars =
-          if List.length targets != 1 then raise RecursionNotSupported else
-            let (target, assignments) = List.hd targets in
-            List.combine patterns assignments
-            |> List.map (fun (var, assignment) -> Map.add var assignment)
-            |> List.fold_left (fun map adder -> adder map) Map.empty 
-            |> fun update -> { name; start; target; update; guard }
-                           
-        let equal t1 t2 =
-          t1.name == t2.name
-          
-        let compare t1 t2 = 
-          if (t1 == t2) then 0
-          else if (t1.name < t1.name) then (-1)
-          else 1
-          
-        let start t = t.start
-                    
-        let target t = t.target
-                     
-        let update t var = Map.Exceptionless.find var t.update                    
+module TransitionLabel =
+  struct
+    module Guard = Constraints.Make(Polynomials.Make(PolyTypes.OurInt))         
+    module Polynomial = Polynomials.Make(PolyTypes.OurInt)
+    module Map = Map.Make(Var)
+    module Bound = MinMaxPolynomial.Make(Polynomials.Make(PolyTypes.OurInt))
                  
-        let guard t = t.guard
-                    
-        let default = {   
-            name = "default";
-            start = "";
-            target = "";
-            update = Map.empty;
-            guard = Guard.mk_true;
-          }
-                    
-        let sizebound_local kind label var =
-          (* If we have an update pattern, it's like x'=b and therefore x'<=b and x >=b and b is a bound for both kinds. *)
-          (* TODO Should we also try to substitute vars in the bound if it leads to a simpler bound? E.g. x<=10 && x'=x : b:=x or b:=10? *)
-          match update label var with
-          | Some bound -> Bound.of_poly bound
-          | None ->
-             match kind with
-             (* TODO Use SMT-Solving to find bounds *)
-             | Upper -> Bound.infinity
-             | Lower -> Bound.minus_infinity
-
-        let update_to_string_list update =
-          if Map.is_empty update then
-            "true"
-          else
-            let entry_string var poly = Var.to_string var ^ "' := " ^ Polynomial.to_string poly
-            and ((var, poly), without_first) = Map.pop update in
-            Map.fold (fun var poly result -> result ^ " && " ^ entry_string var poly) without_first (entry_string var poly)
-
-        let to_string label =          
-          let guard = if Guard.is_true label.guard then "" else " && " ^ Guard.to_string label.guard in
-          update_to_string_list label.update ^ guard
-          
-      end
-
-    module Location =
-      struct
-        type t = {
-            name : string;
-            (* TODO Possible optimization: invariant : PolynomialConstraints.t*)
-          } [@@deriving eq, ord]
-               
-        (*Needed by ocamlgraph*)    
-        let hash l = Hashtbl.hash l.name
-                   
-        let to_string l = l.name
-                        
-        let of_string name = { name }
-                           
-      end
-
-    module TransitionGraph = Graph.Persistent.Digraph.ConcreteBidirectionalLabeled(Location)(TransitionLabel)
-
-    module Transition = struct
-      include TransitionGraph.E
-      let to_string (l,t,l') = Location.to_string l ^ "->" ^ Location.to_string l' ^ ", " ^ TransitionLabel.to_string t
-      let equal = (=)
-    end
-
-    module RV =
-      struct
-        type t = Transition.t * Var.t [@@deriving eq, ord]
-        let hash v = raise (Failure "Not yet implemented")
-        let transition (t,v) = t
-        let variable (t,v) = v
-        let to_string ((l,t,l'),v) = TransitionLabel.(Bound.to_string (sizebound_local Upper t v)) ^ " >= " ^
-                                       "|" ^ Location.to_string l ^ " -> " ^ Location.to_string l' ^ "," ^ Var.to_string v ^ "|"
-                                   ^ " >= " ^ TransitionLabel.(Bound.to_string (sizebound_local Lower t v))
-      end
-    module RVG = Graph.Persistent.Digraph.ConcreteBidirectional(RV)
+    exception RecursionNotSupported
+            
+    type kind = Lower | Upper [@@deriving eq, ord]
 
     type t = {
-        graph: TransitionGraph.t;
-        vars: Var.t list;
-        start: Location.t;
-      }
-                     
-    let add_vertices graph vertices =
-      vertices
-      |> List.map (fun vertex -> fun gr -> TransitionGraph.add_vertex gr vertex)
-      |> List.fold_left (fun gr adder -> adder gr) graph
-      
-    let add_edges graph edges =
-      edges
-      |> List.map (fun edge -> fun gr -> TransitionGraph.add_edge_e gr edge)
-      |> List.fold_left (fun gr adder -> adder gr) graph
-      
-    let mk vertices edges =
-      add_edges (add_vertices TransitionGraph.empty vertices) edges
-
-    let from vars transitions start =
-      let edges = List.map (fun t -> (Location.of_string (TransitionLabel.start t),
-                                      t,
-                                      Location.of_string (TransitionLabel.target t)))
-                           transitions in
-      let vertices = List.unique (List.append
-                                    (List.map (fun (start, _, _) -> start) edges)
-                                    (List.map (fun (_, _, target) -> target) edges)) in
-      {
-        graph = mk vertices edges;
-        vars = vars;
-        start = start;
+        name : string;
+        start : string;
+        target : string;
+        update : Polynomial.t Map.t;
+        guard : Guard.t;
+        (* TODO Transitions should have costs *)
       }
 
-    let vars program =
-      Set.of_list program.vars
+    let make ~name ~start ~target ~update ~guard = {
+        name; start; target; update; guard
+      }
+                                                 
+                                                 
+    (* TODO Pattern <-> Assigment relation *)
+    let mk ~name ~start ~targets ~patterns ~guard ~vars =
+      if List.length targets != 1 then raise RecursionNotSupported else
+        let (target, assignments) = List.hd targets in
+        List.combine patterns assignments
+        |> List.map (fun (var, assignment) -> Map.add var assignment)
+        |> List.fold_left (fun map adder -> adder map) Map.empty 
+        |> fun update -> { name; start; target; update; guard }
+                       
+    let equal t1 t2 =
+      t1.name == t2.name
       
-    let add_vertices_to_rvg vertices rvg =
-      vertices
-      |> List.map (flip RVG.add_vertex)
-      |> List.fold_left (fun rvg adder -> adder rvg) rvg
-
-    let graph g = g.graph
-
-    let pre program (l,t,l') =
-      Set.of_list (TransitionGraph.pred_e (graph program) l)
-
-    let rvg program =
-      let add_transition post_transition (rvg: RVG.t) =
-        let rvg_with_vertices: RVG.t = add_vertices_to_rvg (List.map (fun var -> (post_transition,var)) program.vars) rvg in
-        let pre_nodes post_transition post_var =
-          TransitionLabel.sizebound_local TransitionLabel.Upper (Transition.label post_transition) post_var
-          |> TransitionLabel.Bound.vars
-          |> Set.cartesian_product (pre program post_transition)
-          |> Set.map (fun (pre_transition,pre_var) -> (pre_transition,pre_var,post_var))
-        in
-        Set.map (pre_nodes post_transition) (vars program)
-        |> fun set -> Set.fold Set.union set Set.empty
-        |> fun set -> Set.fold (fun (pre_transition,pre_var,post_var) rvg -> RVG.add_edge rvg (pre_transition,pre_var) (post_transition,post_var)) set rvg_with_vertices                          
-      in
-      TransitionGraph.fold_edges_e add_transition program.graph RVG.empty
+    let compare t1 t2 = 
+      if (t1 == t2) then 0
+      else if (t1.name < t1.name) then (-1)
+      else 1
       
-    let print_graph out_dir name graph output_graph =
-      (* Create output directory if not existing *)
-      ignore (Sys.command ("mkdir -p " ^ out_dir));
-      (* Write a graphviz dot file *)
-      output_graph (Pervasives.open_out_bin (out_dir ^ "/" ^ name ^ ".dot")) graph;
-      (* Generate a png from the dot file with an external call to graphviz *)
-      ignore (Sys.command ("dot -T png -o " ^ (out_dir ^ "/" ^ name ^ ".png ") ^ (out_dir ^ "/" ^ name ^ ".dot")))
+    let start t = t.start
                 
-    let print_system ~outdir ~file program =
-      (* Definition of some graphviz options how it should be layout *)
-      let module Dot = Graph.Graphviz.Dot(struct
-                                           include TransitionGraph
-                                           let edge_attributes (a, e, b) = [`Label (TransitionLabel.to_string e); `Color 4711]
-                                           let default_edge_attributes _ = []
-                                           let get_subgraph _ = None
-                                           let vertex_attributes _ = [`Shape `Box]
-                                           let vertex_name v = Location.to_string v
-                                           let default_vertex_attributes _ = []
-                                           let graph_attributes _ = []
-                                         end) in
-      print_graph outdir (file ^ "_system") (graph program) Dot.output_graph
+    let target t = t.target
+                 
+    let update t var = Map.Exceptionless.find var t.update                    
+                     
+    let guard t = t.guard
+                
+    let default = {   
+        name = "default";
+        start = "";
+        target = "";
+        update = Map.empty;
+        guard = Guard.mk_true;
+      }
+                
+    let sizebound_local kind label var =
+      (* If we have an update pattern, it's like x'=b and therefore x'<=b and x >=b and b is a bound for both kinds. *)
+      (* TODO Should we also try to substitute vars in the bound if it leads to a simpler bound? E.g. x<=10 && x'=x : b:=x or b:=10? *)
+      match update label var with
+      | Some bound -> Bound.of_poly bound
+      | None ->
+         match kind with
+         (* TODO Use SMT-Solving to find bounds *)
+         | Upper -> Bound.infinity
+         | Lower -> Bound.minus_infinity
 
-    let print_rvg ~outdir ~file program =
-      (* Definition of some graphviz options how it should be layout *)
-      let module Dot = Graph.Graphviz.Dot(struct
-                                           include RVG
-                                           let edge_attributes _ = [`Label ""; `Color 4711]
-                                           let default_edge_attributes _ = []
-                                           let get_subgraph _ = None
-                                           let vertex_attributes _ = [`Shape `Box]
-                                           let vertex_name v = "\"" ^ RV.to_string v ^ "\""
-                                           let default_vertex_attributes _ = []
-                                           let graph_attributes _ = []
-                                         end) in
-      print_graph outdir (file ^ "_rvg") (rvg program) Dot.output_graph
+    let update_to_string_list update =
+      if Map.is_empty update then
+        "true"
+      else
+        let entry_string var poly = Var.to_string var ^ "' := " ^ Polynomial.to_string poly
+        and ((var, poly), without_first) = Map.pop update in
+        Map.fold (fun var poly result -> result ^ " && " ^ entry_string var poly) without_first (entry_string var poly)
 
-    let is_initial graph (l,t,l') =
-      graph.start == l
+    let to_string label =          
+      let guard = if Guard.is_true label.guard then "" else " && " ^ Guard.to_string label.guard in
+      update_to_string_list label.update ^ guard
+      
+  end
 
-    let to_string graph =
-      "TODO"
+module Location =
+  struct
+    type t = {
+        name : string;
+        (* TODO Possible optimization: invariant : PolynomialConstraints.t*)
+      } [@@deriving eq, ord]
+           
+    (*Needed by ocamlgraph*)    
+    let hash l = Hashtbl.hash l.name
+               
+    let to_string l = l.name
+                    
+    let of_string name = { name }
+                       
+  end
+
+module TransitionGraph = Graph.Persistent.Digraph.ConcreteBidirectionalLabeled(Location)(TransitionLabel)
+
+module Transition = struct
+  include TransitionGraph.E
+  let to_string (l,t,l') = Location.to_string l ^ "->" ^ Location.to_string l' ^ ", " ^ TransitionLabel.to_string t
+  let equal = (=)
+end
+
+module RV =
+  struct
+    type t = Transition.t * Var.t [@@deriving eq, ord]
+    let hash v = raise (Failure "Not yet implemented")
+    let transition (t,v) = t
+    let variable (t,v) = v
+    let to_string ((l,t,l'),v) = TransitionLabel.(Bound.to_string (sizebound_local Upper t v)) ^ " >= " ^
+                                   "|" ^ Location.to_string l ^ " -> " ^ Location.to_string l' ^ "," ^ Var.to_string v ^ "|"
+                                   ^ " >= " ^ TransitionLabel.(Bound.to_string (sizebound_local Lower t v))
+  end
+module RVG = Graph.Persistent.Digraph.ConcreteBidirectional(RV)
+
+type t = {
+    graph: TransitionGraph.t;
+    vars: Var.t list;
+    start: Location.t;
+  }
+       
+let add_vertices graph vertices =
+  vertices
+  |> List.map (fun vertex -> fun gr -> TransitionGraph.add_vertex gr vertex)
+  |> List.fold_left (fun gr adder -> adder gr) graph
+  
+let add_edges graph edges =
+  edges
+  |> List.map (fun edge -> fun gr -> TransitionGraph.add_edge_e gr edge)
+  |> List.fold_left (fun gr adder -> adder gr) graph
+  
+let mk vertices edges =
+  add_edges (add_vertices TransitionGraph.empty vertices) edges
+
+let from vars transitions start =
+  let edges = List.map (fun t -> (Location.of_string (TransitionLabel.start t),
+                                  t,
+                                  Location.of_string (TransitionLabel.target t)))
+                       transitions in
+  let vertices = List.unique (List.append
+                                (List.map (fun (start, _, _) -> start) edges)
+                                (List.map (fun (_, _, target) -> target) edges)) in
+  {
+    graph = mk vertices edges;
+    vars = vars;
+    start = start;
+  }
+
+let vars program =
+  Set.of_list program.vars
+  
+let add_vertices_to_rvg vertices rvg =
+  vertices
+  |> List.map (flip RVG.add_vertex)
+  |> List.fold_left (fun rvg adder -> adder rvg) rvg
+
+let graph g = g.graph
+
+let pre program (l,t,l') =
+  Set.of_list (TransitionGraph.pred_e (graph program) l)
+
+let rvg program =
+  let add_transition post_transition (rvg: RVG.t) =
+    let rvg_with_vertices: RVG.t = add_vertices_to_rvg (List.map (fun var -> (post_transition,var)) program.vars) rvg in
+    let pre_nodes post_transition post_var =
+      TransitionLabel.sizebound_local TransitionLabel.Upper (Transition.label post_transition) post_var
+      |> TransitionLabel.Bound.vars
+      |> Set.cartesian_product (pre program post_transition)
+      |> Set.map (fun (pre_transition,pre_var) -> (pre_transition,pre_var,post_var))
+    in
+    Set.map (pre_nodes post_transition) (vars program)
+    |> fun set -> Set.fold Set.union set Set.empty
+                  |> fun set -> Set.fold (fun (pre_transition,pre_var,post_var) rvg -> RVG.add_edge rvg (pre_transition,pre_var) (post_transition,post_var)) set rvg_with_vertices                          
+  in
+  TransitionGraph.fold_edges_e add_transition program.graph RVG.empty
+  
+let print_graph out_dir name graph output_graph =
+  (* Create output directory if not existing *)
+  ignore (Sys.command ("mkdir -p " ^ out_dir));
+  (* Write a graphviz dot file *)
+  output_graph (Pervasives.open_out_bin (out_dir ^ "/" ^ name ^ ".dot")) graph;
+  (* Generate a png from the dot file with an external call to graphviz *)
+  ignore (Sys.command ("dot -T png -o " ^ (out_dir ^ "/" ^ name ^ ".png ") ^ (out_dir ^ "/" ^ name ^ ".dot")))
+  
+let print_system ~outdir ~file program =
+  (* Definition of some graphviz options how it should be layout *)
+  let module Dot = Graph.Graphviz.Dot(struct
+                                       include TransitionGraph
+                                       let edge_attributes (a, e, b) = [`Label (TransitionLabel.to_string e); `Color 4711]
+                                       let default_edge_attributes _ = []
+                                       let get_subgraph _ = None
+                                       let vertex_attributes _ = [`Shape `Box]
+                                       let vertex_name v = Location.to_string v
+                                       let default_vertex_attributes _ = []
+                                       let graph_attributes _ = []
+                                     end) in
+  print_graph outdir (file ^ "_system") (graph program) Dot.output_graph
+
+let print_rvg ~outdir ~file program =
+  (* Definition of some graphviz options how it should be layout *)
+  let module Dot = Graph.Graphviz.Dot(struct
+                                       include RVG
+                                       let edge_attributes _ = [`Label ""; `Color 4711]
+                                       let default_edge_attributes _ = []
+                                       let get_subgraph _ = None
+                                       let vertex_attributes _ = [`Shape `Box]
+                                       let vertex_name v = "\"" ^ RV.to_string v ^ "\""
+                                       let default_vertex_attributes _ = []
+                                       let graph_attributes _ = []
+                                     end) in
+  print_graph outdir (file ^ "_rvg") (rvg program) Dot.output_graph
+
+let is_initial graph (l,t,l') =
+  graph.start == l
+
+let to_string graph =
+  "TODO"
--- src/transitionsystem/Program.mli
+++ src/transitionsystem/Program.mli
@@ -2,98 +2,98 @@ open Batteries
 
 (** Provides default modules to create locations, transitions and transitionsystems *)
 
-    (** A transition is an edge of a transition system.
+(** A transition is an edge of a transition system.
         It connects two locations and is annotated with a guard and an update
         A guard has to be fulfiled for a state to reach another state via the transition
         An update assigns variables a new value as a linear combination of the old values *)
-    module TransitionLabel :
-    sig
-      module Guard : module type of Constraints.Make(Polynomials.Make(PolyTypes.OurInt))         
-      module Polynomial : module type of Polynomials.Make(PolyTypes.OurInt)
-      module Map : module type of Map.Make(Var)
-      module Bound : module type of MinMaxPolynomial.Make(Polynomials.Make(PolyTypes.OurInt))
-                                  
-      type kind = Lower | Upper  [@@deriving eq, ord]
-      type t
-      exception RecursionNotSupported
-      val make : name:string -> start:string -> target:string -> update:Polynomial.t Map.t -> guard:Guard.t -> t
-      val mk : name:string ->
-               start:string ->
-               targets:(string * (Polynomial.t list)) list ->
-               patterns:Var.t list ->
-               guard:Guard.t ->
-               vars:Var.t list ->
-               t
-      val equal : t -> t -> bool
-      val compare : t -> t -> int
-      val start : t -> string
-      val target : t -> string
-      val update : t -> Var.t -> Polynomial.t Option.t
-      val guard : t -> Guard.t
-      val default : t
-      (** Returns a local sizebound of the specified kind for the var of the transition. 
+module TransitionLabel :
+sig
+  module Guard : module type of Constraints.Make(Polynomials.Make(PolyTypes.OurInt))         
+  module Polynomial : module type of Polynomials.Make(PolyTypes.OurInt)
+  module Map : module type of Map.Make(Var)
+  module Bound : module type of MinMaxPolynomial.Make(Polynomials.Make(PolyTypes.OurInt))
+                              
+  type kind = Lower | Upper  [@@deriving eq, ord]
+  type t
+  exception RecursionNotSupported
+  val make : name:string -> start:string -> target:string -> update:Polynomial.t Map.t -> guard:Guard.t -> t
+  val mk : name:string ->
+           start:string ->
+           targets:(string * (Polynomial.t list)) list ->
+           patterns:Var.t list ->
+           guard:Guard.t ->
+           vars:Var.t list ->
+           t
+  val equal : t -> t -> bool
+  val compare : t -> t -> int
+  val start : t -> string
+  val target : t -> string
+  val update : t -> Var.t -> Polynomial.t Option.t
+  val guard : t -> Guard.t
+  val default : t
+  (** Returns a local sizebound of the specified kind for the var of the transition. 
           A local sizebound is expressed in relation to the values directly before executing the transition. *)
-      val sizebound_local : kind -> t -> Var.t -> Bound.t
-      val to_string : t -> string
-    end
-         
-    (** A location is a node of a transition system and can be connected to other locations via transitions *)
-    module Location :
-    sig
-      type t [@@deriving eq, ord]
-      val hash : t -> int
-      val to_string : t -> string
-      val of_string : string -> t
-    end
+  val sizebound_local : kind -> t -> Var.t -> Bound.t
+  val to_string : t -> string
+end
+     
+(** A location is a node of a transition system and can be connected to other locations via transitions *)
+module Location :
+sig
+  type t [@@deriving eq, ord]
+  val hash : t -> int
+  val to_string : t -> string
+  val of_string : string -> t
+end
 
-    module TransitionGraph : module type of Graph.Persistent.Digraph.ConcreteBidirectionalLabeled(Location)(TransitionLabel)
-                                          
-    module Transition : module type of TransitionGraph.E
-      
-    module RV :
-      sig
-        type t = Transition.t * Var.t [@@deriving eq]
-        val compare : t -> t -> int
-        val hash : t -> int
-        val transition : t -> Transition.t
-        val variable : t -> Var.t
-      end
-         
-    module RVG : module type of Graph.Persistent.Digraph.ConcreteBidirectional(RV)
+module TransitionGraph : module type of Graph.Persistent.Digraph.ConcreteBidirectionalLabeled(Location)(TransitionLabel)
+                                      
+module Transition : module type of TransitionGraph.E
+                                 
+module RV :
+sig
+  type t = Transition.t * Var.t [@@deriving eq]
+  val compare : t -> t -> int
+  val hash : t -> int
+  val transition : t -> Transition.t
+  val variable : t -> Var.t
+end
+     
+module RVG : module type of Graph.Persistent.Digraph.ConcreteBidirectional(RV)
 
-    type t
+type t
 
-    val add_vertices : TransitionGraph.t -> TransitionGraph.vertex list -> TransitionGraph.t
+val add_vertices : TransitionGraph.t -> TransitionGraph.vertex list -> TransitionGraph.t
 
-    val add_edges : TransitionGraph.t -> TransitionGraph.edge list -> TransitionGraph.t
+val add_edges : TransitionGraph.t -> TransitionGraph.edge list -> TransitionGraph.t
 
-    val mk : TransitionGraph.vertex list -> TransitionGraph.edge list -> TransitionGraph.t
+val mk : TransitionGraph.vertex list -> TransitionGraph.edge list -> TransitionGraph.t
 
-    val from : Var.t list
-               -> TransitionLabel.t list
-               -> Location.t
-               -> t
+val from : Var.t list
+           -> TransitionLabel.t list
+           -> Location.t
+           -> t
 
-    val rvg : t -> RVG.t
+val rvg : t -> RVG.t
 
-    val graph : t -> TransitionGraph.t
+val graph : t -> TransitionGraph.t
 
-    (** Returns a set of all transitions which occur directly before the given transition in the graph. 
+(** Returns a set of all transitions which occur directly before the given transition in the graph. 
        Corresponds to pre(t). *)
-    val pre : t -> Transition.t -> Transition.t Set.t
+val pre : t -> Transition.t -> Transition.t Set.t
 
-    (** Prints a png file in the given directory with the given filename (the extension .png will be generated) for the transition graph of the program. 
+(** Prints a png file in the given directory with the given filename (the extension .png will be generated) for the transition graph of the program. 
         For this operation graphviz need to be installed and the 'dot' command must be accessible in the PATH. *)
-    val print_system : outdir:string -> file:string -> t -> unit
+val print_system : outdir:string -> file:string -> t -> unit
 
-    (** Prints a png file in the given directory with the given filename (the extension .png will be generated) for the result variable graph of the program. 
+(** Prints a png file in the given directory with the given filename (the extension .png will be generated) for the result variable graph of the program. 
         For this operation graphviz need to be installed and the 'dot' command must be accessible in the PATH. *)
-    val print_rvg : outdir:string -> file:string -> t -> unit
+val print_rvg : outdir:string -> file:string -> t -> unit
 
-    (** Returns if the given transition is an initial transition. *)
-    val is_initial : t -> Transition.t -> bool
+(** Returns if the given transition is an initial transition. *)
+val is_initial : t -> Transition.t -> bool
 
-    val to_string : t -> string
-    
-    val vars : t -> Var.t Set.t
-    
+val to_string : t -> string
+  
+val vars : t -> Var.t Set.t
+  
