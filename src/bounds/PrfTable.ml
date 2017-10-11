75d4edc856bf2c782d3b1b9bc51f7a64e2a5756e
Marcel Hark <marcel.hark@rwth-aachen.de>
2017-10-11 17:52:30 +0200

    To_string method implemented

--- src/bounds/PrfTable.ml
+++ src/bounds/PrfTable.ml
@@ -3,3 +3,8 @@ open Batteries
 include Hashtbl.Make(Program.TransitionGraph.V)
 
 type table = (Polynomials.ParameterPolynomial.t) t
+
+let to_string hashtable =
+  let output = IO.output_string () in
+  print (fun output key -> IO.nwrite output (Program.Location.to_string key)) (fun output parapoly -> IO.nwrite output (Polynomials.ParameterPolynomial.to_string parapoly)) output hashtable;
+  IO.close_out output
--- src/bounds/PrfTable.mli
+++ src/bounds/PrfTable.mli
@@ -3,3 +3,5 @@ open Batteries
 include module type of Hashtbl.Make(Program.TransitionGraph.V)
 
 type table = (Polynomials.ParameterPolynomial.t) t
+
+val to_string : table -> string
