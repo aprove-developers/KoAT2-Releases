806a9774591f39d3ddde53de3d7d24ada27ebd6d
Fabian Boeller <fabianchina@googlemail.com>
2017-10-27 09:36:37 +0200

    Forget to check in the test

---
+++ src/test/PreprocessorTest.ml
@@ -0,0 +1,24 @@
+open Batteries
+open OUnit2
+open Helper
+
+let tests = 
+  "Preprocessors" >::: [
+      
+      ("CutUnreachable" >:::
+         List.map (fun (expected_program, program) ->
+             program >:: (fun _ -> assert_equal_program
+                                     (Readers.read_transitiongraph_simple expected_program)
+                                     (PreprocessorTypes.CutUnreachable.transform_program (Readers.read_transitiongraph_simple program))))
+                  [
+                    ("l1 -> l2(x)", "l1 -> l2(x), l3 -> l4(x)");
+                    ("l1 -> l2(x), l2 -> l3(x)", "l1 -> l2(x), l2 -> l3(x), l4 -> l5(x)");
+                    ("l1 -> l2(x)", "l1 -> l2(x), l3 -> l3(x)");
+                    ("l1 -> l1(x)", "l1 -> l1(x), l2 -> l2(x)");
+                    ("l1 -> l2(x)", "l1 -> l2(x), l3 -> l4(x), l4 -> l5(x)");
+                  ]
+      );
+      
+    ]
+
+    
