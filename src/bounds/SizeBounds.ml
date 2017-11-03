8b2254eaee1d0358c77fffb73909503c52670bb7
Fabian Boeller <fabianchina@googlemail.com>
2017-11-03 20:12:30 +0100

    Improved testability of SizeBounds

--- src/bounds/SizeBounds.ml
+++ src/bounds/SizeBounds.ml
@@ -7,9 +7,9 @@ let logger = Logger.make_log "size"
 
 type kind = [ `Lower | `Upper ] [@@deriving show]
            
-let incoming_bound kind program appr local_sizebound t =
+let incoming_bound kind program get_sizebound local_sizebound t =
   let execute () =
-    let substitute_with_prevalues t' = Bound.substitute_f (Approximation.sizebound kind appr t') local_sizebound in
+    let substitute_with_prevalues t' = Bound.substitute_f (get_sizebound kind t') local_sizebound in
     t
     |> Program.pre program
     |> Enum.map substitute_with_prevalues
@@ -17,30 +17,22 @@ let incoming_bound kind program appr local_sizebound t =
         | `Lower -> Bound.minimum
         | `Upper -> Bound.maximum
   in Logger.with_log logger Logger.DEBUG
-                  (fun () -> "compute highest incoming bound", ["lsb", Bound.to_string local_sizebound; "transition", Transition.to_id_string t])
+                     (fun () -> "compute highest incoming bound", ["lsb", Bound.to_string local_sizebound;
+                                                                   "transition", Transition.to_id_string t])
                   ~result:Bound.to_string
                   execute
 
-let compute_trivial_bound kind program appr lsb t =
+let compute_trivial_bound kind program get_sizebound (t,v) =
   let execute () =
+    let (lsb: Bound.t) = LocalSizeBound.(as_bound (sizebound_local kind (Transition.label t) v)) in
     if Program.is_initial program t then
       lsb
-    else incoming_bound kind program appr lsb t
+    else incoming_bound kind program get_sizebound lsb t
   in Logger.with_log logger Logger.DEBUG
-                     (fun () -> "compute trivial bound", ["lsb", Bound.to_string lsb;
-                                                          "transition", Transition.to_id_string t])
+                     (fun () -> "compute trivial bound", ["kind", show_kind kind;
+                                                          "rv", RV.to_id_string (t,v)])
                      ~result:Bound.to_string
                      execute
-
-let improve_trivial_scc kind program appr (t,v) =
-  let execute () =
-    let (local_sizebound: Bound.t) = LocalSizeBound.(as_bound (sizebound_local kind (Transition.label t) v)) in
-    let new_bound = compute_trivial_bound kind program appr local_sizebound t in
-    Approximation.add_sizebound kind new_bound t v appr
-  in Logger.with_log logger Logger.DEBUG
-                     (fun () -> "improve trivial scc", ["kind", show_kind kind;
-                                                        "rv", RV.to_id_string (t,v)])
-                  execute
 
 let extreme_scaling_factor kind ct =
   let execute () =
@@ -75,24 +67,24 @@ let extreme_affecting_scc_variables kind rvg scc ct =
                      ~result:Int.to_string
                      execute
 
-let transition_scaling_factor kind rvg appr scc ct =
+let transition_scaling_factor kind rvg get_timebound scc ct =
   let execute () =
     let (transition, _) = List.hd ct (* We require ct to be non-empty *) in
     Bound.exp (OurInt.of_int (extreme_scaling_factor kind ct *
                                 extreme_affecting_scc_variables kind rvg scc ct))
-              (Approximation.timebound appr transition) 
+              (get_timebound transition) 
   in Logger.with_log logger Logger.DEBUG
                      (fun () -> "transition scaling factor", ["ct", RVG.rvs_to_id_string ct; "scc", RVG.rvs_to_id_string scc])
                      ~result:Bound.to_string
                      execute
   
-let overall_scaling_factor kind rvg appr scc=
+let overall_scaling_factor kind rvg get_timebound scc=
   let execute () =
     scc
     |> List.enum
     |> Enum.group_by (fun (t1, v1) (t2, v2) -> Transition.equal t1 t2)
     |> Enum.map List.of_enum
-    |> Enum.map (transition_scaling_factor kind rvg appr scc)
+    |> Enum.map (transition_scaling_factor kind rvg get_timebound scc)
     |> Bound.product
   in Logger.with_log logger Logger.DEBUG
                      (fun () -> "overall scaling factor", ["scc", RVG.rvs_to_id_string scc])
@@ -100,7 +92,7 @@ let overall_scaling_factor kind rvg appr scc=
                      execute
 
 
-let incoming_vars_effect kind rvg appr scc vars transition alpha =
+let incoming_vars_effect kind rvg get_sizebound scc vars transition alpha =
   let execute () =
     vars
     |> VarSet.enum
@@ -109,7 +101,7 @@ let incoming_vars_effect kind rvg appr scc vars transition alpha =
            RVG.pre rvg alpha
            |> Util.without RV.equal (List.enum scc)
            |> Enum.filter (fun (t,v') -> Var.equal v v')
-           |> Enum.map (fun (t,v) -> Approximation.sizebound kind appr t v)
+           |> Enum.map (fun (t,v) -> get_sizebound kind t v)
            |> Bound.maximum
          )
     |> Bound.sum
@@ -121,13 +113,13 @@ let incoming_vars_effect kind rvg appr scc vars transition alpha =
                      ~result:Bound.to_string
                      execute
 
-let transition_effect kind rvg appr scc ct transition=
+let transition_effect kind rvg get_sizebound scc ct transition=
   let execute () =
     ct
     |> Enum.map (fun alpha -> (alpha, Option.get (LocalSizeBound.sizebound_local_rv kind alpha)))
     (* Should exist *)
     |> Enum.map (fun (alpha, lsb) ->
-           Bound.(max zero (of_int lsb.LocalSizeBound.constant + incoming_vars_effect kind rvg appr scc lsb.LocalSizeBound.vars transition alpha))
+           Bound.(max zero (of_int lsb.LocalSizeBound.constant + incoming_vars_effect kind rvg get_sizebound scc lsb.LocalSizeBound.vars transition alpha))
          )
     |> Bound.maximum
   in Logger.with_log logger Logger.DEBUG
@@ -137,14 +129,14 @@ let transition_effect kind rvg appr scc ct transition=
                      ~result:Bound.to_string
                      execute
 
-let effects kind rvg appr scc =
+let effects kind rvg get_timebound get_sizebound scc =
   let execute () =
     scc
     |> List.enum
     |> Enum.group_by (fun (t1, v1) (t2, v2) -> Transition.equal t1 t2)
     |> Enum.map (fun ct ->
            let (transition, _) = Option.get (Enum.peek ct) (* We require ct to be non-empty *) in
-           Bound.(Approximation.timebound appr transition * transition_effect kind rvg appr scc ct transition)
+           Bound.(get_timebound transition * transition_effect kind rvg get_sizebound scc ct transition)
          )
     |> Bound.sum
   in Logger.with_log logger Logger.DEBUG
@@ -157,9 +149,7 @@ let sign = function
   | `Lower -> Bound.neg Bound.one
   | `Upper -> Bound.one
   
-(* Improves a nontrivial scc. That is an scc which consists of more than one result variable.
-       Corresponds to 'SizeBounds for nontrivial SCCs'. *)
-let improve_nontrivial_scc kind program rvg appr scc =
+let compute_nontrivial_bound kind program rvg get_timebound get_sizebound scc =
   let execute () =
     (* Only if all lsbs have a bound of our required form *)
     if scc
@@ -167,25 +157,34 @@ let improve_nontrivial_scc kind program rvg appr scc =
        |> Enum.map (LocalSizeBound.sizebound_local_rv kind)
        |> Enum.for_all Option.is_some
     then
-      let new_bound = Bound.(sign kind * overall_scaling_factor kind rvg appr scc * effects kind rvg appr scc) in
-      Approximation.add_sizebounds kind new_bound scc appr
-    else appr
+      Bound.(sign kind
+             * overall_scaling_factor kind rvg get_timebound scc
+             * effects kind rvg get_timebound get_sizebound scc)
+    else
+      match kind with
+      | `Lower -> Bound.minus_infinity
+      | `Upper -> Bound.infinity
   in Logger.with_log logger Logger.DEBUG
-                     (fun () -> "improve nontrivial scc", ["kind", show_kind kind;
-                                                           "scc", RVG.rvs_to_id_string scc])
+                     (fun () -> "compute nontrivial bound", ["kind", show_kind kind;
+                                                             "scc", RVG.rvs_to_id_string scc])
+                     ~result:Bound.to_string
                      execute
-         
-(* Improves a whole scc. *)
-let improve_scc program rvg appr scc =
-  match scc with
+    
+let improve_scc program rvg appr = function
   | [((l,t,l'),v)] when not (Location.equal l l') ->
-     appr
-     |> fun appr -> improve_trivial_scc `Upper program appr ((l,t,l'),v)
-     |> fun appr -> improve_trivial_scc `Lower program appr ((l,t,l'),v)         
+     let add_trivial_bound kind =
+       let new_bound = compute_trivial_bound kind program (fun kind -> Approximation.sizebound kind appr) ((l,t,l'),v) in
+       Approximation.add_sizebound kind new_bound (l,t,l') v appr
+     in appr
+     |> fun appr -> add_trivial_bound `Upper
+     |> fun appr -> add_trivial_bound `Lower
   | scc ->
-     appr
-     |> fun appr -> improve_nontrivial_scc `Upper program rvg appr scc
-     |> fun appr -> improve_nontrivial_scc `Lower program rvg appr scc
+     let add_nontrivial_bound kind =
+       let new_bound = compute_nontrivial_bound kind program rvg (Approximation.timebound appr) (fun kind -> Approximation.sizebound kind appr) scc in
+       Approximation.add_sizebounds kind new_bound scc appr
+     in appr
+     |> fun appr -> add_nontrivial_bound `Upper
+     |> fun appr -> add_nontrivial_bound `Lower
          
 let improve program appr =
   let execute () =
--- src/bounds/SizeBounds.mli
+++ src/bounds/SizeBounds.mli
@@ -14,23 +14,20 @@ type kind = [ `Lower | `Upper ] [@@deriving show]
 val improve_scc : Program.t -> RVG.t -> Approximation.t -> RV.t list -> Approximation.t
 
           
-(** Improves a trivial scc. That is an scc which consists only of one result variable without a loop to itself.
+(** Computes a bound for a trivial scc. That is an scc which consists only of one result variable without a loop to itself.
     Corresponds to 'SizeBounds for trivial SCCs'. *)
-val improve_trivial_scc : kind -> Program.t -> Approximation.t -> RV.t-> Approximation.t
+val compute_trivial_bound : kind -> Program.t -> (kind -> Transition.t -> Var.t -> Bound.t) -> RV.t -> Bound.t
 
-(** Improves a nontrivial scc. That is an scc which consists of a loop.
+(** Computes a bound for a nontrivial scc. That is an scc which consists of a loop.
     Corresponds to 'SizeBounds for nontrivial SCCs'. *)
-val improve_nontrivial_scc : kind -> Program.t -> RVG.t -> Approximation.t -> RV.t list -> Approximation.t
+val compute_nontrivial_bound : kind -> Program.t -> RVG.t -> (Transition.t -> Bound.t) -> (kind -> Transition.t -> Var.t -> Bound.t) -> RV.t list -> Bound.t
 
   
 (** Returns the maximum of all incoming sizebounds applicated to the local sizebound.
     Corresponds to 'SizeBounds for trivial SCCs':
     S'(alpha) = max(S_l(alpha)(S(t',v_1),...,S(t',v_n)) for all t' in pre(t)) *)
-val incoming_bound : kind -> Program.t -> Approximation.t -> Bound.t -> Transition.t -> Bound.t
+val incoming_bound : kind -> Program.t -> (kind -> Transition.t -> Var.t -> Bound.t) -> Bound.t -> Transition.t -> Bound.t
 
-(** Computes a trivial sizebound for the given transition with the specified local sizebound.  *)
-val compute_trivial_bound : kind -> Program.t -> Approximation.t -> Bound.t -> Transition.t -> Bound.t
-  
 (** Computes for each transition max(s_alpha for all alpha in C_t) and multiplies the results. *)
 val extreme_scaling_factor : kind -> RV.t list -> int
 
@@ -41,12 +38,12 @@ val scc_variables : RVG.t -> RV.t list -> RV.t -> Var.t Enum.t
 (* TODO Kind only relevant for only positive and only negative effects. *)
 val extreme_affecting_scc_variables : kind -> RVG.t -> RV.t list -> RV.t list -> int
 
-val transition_scaling_factor : kind -> RVG.t -> Approximation.t -> RV.t list -> RV.t list -> Bound.t
+val transition_scaling_factor : kind -> RVG.t -> (Transition.t -> Bound.t) -> RV.t list -> RV.t list -> Bound.t
 
-val overall_scaling_factor : kind -> RVG.t -> Approximation.t -> RV.t list -> Bound.t
+val overall_scaling_factor : kind -> RVG.t -> (Transition.t -> Bound.t) -> RV.t list -> Bound.t
 
-val incoming_vars_effect : kind -> RVG.t -> Approximation.t -> RV.t list -> VarSet.t -> Transition.t -> RV.t -> Bound.t
+val incoming_vars_effect : kind -> RVG.t -> (kind -> Transition.t -> Var.t -> Bound.t) -> RV.t list -> VarSet.t -> Transition.t -> RV.t -> Bound.t
 
-val transition_effect : kind -> RVG.t -> Approximation.t -> RV.t list -> RV.t Enum.t -> Transition.t -> Bound.t
+val transition_effect : kind -> RVG.t -> (kind -> Transition.t -> Var.t -> Bound.t) -> RV.t list -> RV.t Enum.t -> Transition.t -> Bound.t
 
-val effects : kind -> RVG.t -> Approximation.t -> RV.t list -> Bound.t
+val effects : kind -> RVG.t -> (Transition.t -> Bound.t) -> (kind -> Transition.t -> Var.t -> Bound.t) -> RV.t list -> Bound.t
