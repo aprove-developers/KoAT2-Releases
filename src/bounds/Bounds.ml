06c2cce0c9bd618d8515ad2cb7b416578f61b6f8
Fabian Boeller <fabianchina@googlemail.com>
2017-12-11 16:08:29 +0100

    Removed unnecessary SizeBounds step

--- src/bounds/Bounds.ml
+++ src/bounds/Bounds.ml
@@ -4,6 +4,5 @@ let rec find_bounds (program: Program.t) (appr: Approximation.t): Approximation.
   appr
   |> SizeBounds.improve program
   |> TimeBounds.improve program
-  |> MaybeChanged.if_changed (SizeBounds.improve program)
   |> MaybeChanged.if_changed (find_bounds program)
   |> MaybeChanged.unpack
