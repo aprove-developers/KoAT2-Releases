1660dadc761aa310c07a813815e6442d3d4468d9
Fabian Boeller <fabianchina@googlemail.com>
2018-01-14 14:40:43 +0100

    Refactoring SMT

--- src/smt/SMT.ml
+++ src/smt/SMT.ml
@@ -16,28 +16,6 @@ module Z3Solver =
                         ]
                     )
 
-    let from_constraint (constraints : formula) =
-      Formula.fold 
-        ~subject:(
-          Polynomial.fold
-            ~const:(fun value -> Z3.Arithmetic.Integer.mk_numeral_i !context (OurInt.to_int value))
-            ~var:(fun var -> if Var.is_real var then
-                               Z3.Arithmetic.Real.mk_const_s !context (Var.to_string var)
-                             else
-                               Z3.Arithmetic.Integer.mk_const_s !context (Var.to_string var))
-            ~neg:(Z3.Arithmetic.mk_unary_minus !context)
-            ~plus:(fun p1 p2 -> Z3.Arithmetic.mk_add !context [p1; p2])
-            ~times:(fun p1 p2 -> Z3.Arithmetic.mk_mul !context [p1; p2])
-            ~pow:(fun b e -> Z3.Arithmetic.mk_power !context b (Z3.Arithmetic.Integer.mk_numeral_i !context e))
-        )
-        ~le:(Z3.Arithmetic.mk_le !context)
-        ~correct:(Z3.Boolean.mk_true !context)
-        ~conj:(fun a1 a2 -> Z3.Boolean.mk_and !context [a1; a2])
-        ~wrong:(Z3.Boolean.mk_false !context)
-        ~disj:(fun a1 a2 -> Z3.Boolean.mk_or !context [a1; a2])
-        constraints
-        
-        
     let from_poly = 
       Polynomial.fold
             ~const:(fun value -> Z3.Arithmetic.Integer.mk_numeral_i !context (OurInt.to_int value))
@@ -49,24 +27,30 @@ module Z3Solver =
             ~plus:(fun p1 p2 -> Z3.Arithmetic.mk_add !context [p1; p2])
             ~times:(fun p1 p2 -> Z3.Arithmetic.mk_mul !context [p1; p2])
             ~pow:(fun b e -> Z3.Arithmetic.mk_power !context b (Z3.Arithmetic.Integer.mk_numeral_i !context e))
-(*    let to_string (constraints : Constraint.t) =
-        let solver = Z3.Solver.mk_simple_solver !context in
-        let formula = from_constraint constraints in
-        let optimisation_goal = Z3.Optimize.mk_opt !context in
-        Z3.Optimize.add optimisation_goal [formula];
-        Z3.Optimize.to_string optimisation_goal*)
 
+    let from_formula =
+      Formula.fold 
+        ~subject:from_poly
+        ~le:(Z3.Arithmetic.mk_le !context)
+        ~correct:(Z3.Boolean.mk_true !context)
+        ~conj:(fun a1 a2 -> Z3.Boolean.mk_and !context [a1; a2])
+        ~wrong:(Z3.Boolean.mk_false !context)
+        ~disj:(fun a1 a2 -> Z3.Boolean.mk_or !context [a1; a2])
+        
+    let search formula =
+      let formula = from_formula formula in
+      let optimisation_goal = Z3.Optimize.mk_opt !context in
+      Z3.Optimize.add optimisation_goal [formula];
+      Z3.Optimize.check optimisation_goal
+      
     let result_is expected_result formula =
-        let formula = from_constraint formula in
-        let optimisation_goal = Z3.Optimize.mk_opt !context in
-        Z3.Optimize.add  optimisation_goal [formula];
-        let result = Z3.Optimize.check optimisation_goal in
-        if result == Z3.Solver.UNKNOWN then
-          raise (Failure "SMT-Solver does not know a solution")
-        else
-          result == expected_result
+      let result = search formula in
+      if result == Z3.Solver.UNKNOWN then
+        raise (Failure "SMT-Solver does not know a solution")
+      else
+        result == expected_result
 
-    (** checks if there exists a satisfying assignment for a given constraint
+    (** checks if there exists a satisfying assignment for a given formula
         uses Z3 optimisation methods*)
     let satisfiable formula =
       result_is Z3.Solver.SATISFIABLE formula      
@@ -90,11 +74,11 @@ module Z3Solver =
     let check_negativity (formula : formula) (poly: Polynomial.t) =
       tautology Formula.Infix.(formula => (poly <= Polynomial.zero))
     
-    let generate_minimise (constraints : formula) (coeffs_to_minimise: Var.t list) =
+    let generate_minimise (formula : formula) (coeffs_to_minimise: Var.t list) =
       let generator poly vrbl =
-        if check_positivity constraints Polynomial.(of_var vrbl) then
+        if check_positivity formula Polynomial.(of_var vrbl) then
           Polynomial.(poly + (of_var vrbl))
-        else if check_negativity constraints Polynomial.(of_var vrbl) then
+        else if check_negativity formula Polynomial.(of_var vrbl) then
           Polynomial.(poly - (of_var vrbl))
         else
           poly
@@ -102,17 +86,12 @@ module Z3Solver =
       from_poly (List.fold_left generator Polynomial.zero coeffs_to_minimise)
     
     let get_model ?(coeffs_to_minimise=[]) formula =
-      let z3_expr = from_constraint formula in
+      let z3_expr = from_formula formula in
       let optimisation_goal = Z3.Optimize.mk_opt !context in
       Z3.Optimize.add optimisation_goal [z3_expr];
-      (*let minimise_constr = List.map (fun var -> from_poly (Polynomial.pow (Polynomial.of_var var) 2)) coeffs_to_minimise in
-        for i = 0 to (List.length minimise_constr) - 1 do
-        Z3.Optimize.minimize optimisation_goal (List.at minimise_constr i);
-        done;*)
       Z3.Optimize.minimize optimisation_goal (generate_minimise formula coeffs_to_minimise);
-      (*print_string ("\nOptimise to_string:\n"^(Z3.Optimize.to_string optimisation_goal)^"\n");*)
       let status = Z3.Optimize.check optimisation_goal in
-      if status == Z3.Solver.SATISFIABLE (*|| status == Z3.Solver.UNKNOWN*) then
+      if status == Z3.Solver.SATISFIABLE then
         optimisation_goal
         |> Z3.Optimize.get_model
         |> Option.map (fun model ->
