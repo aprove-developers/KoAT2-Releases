d6fe5b062b12f565223019ece82dff0534256c41
Fabian Boeller <fabianchina@googlemail.com>
2018-01-17 16:08:48 +0100

    First bleeding edge incremental smt solver

--- src/smt/SMT.ml
+++ src/smt/SMT.ml
@@ -2,6 +2,27 @@ open Batteries
 open Formulas
 open Polynomials
    
+let from_poly context = 
+  Polynomial.fold
+    ~const:(fun value -> Z3.Arithmetic.Integer.mk_numeral_i !context (OurInt.to_int value))
+    ~var:(fun var -> if Var.is_real var then
+                       Z3.Arithmetic.Real.mk_const_s !context (Var.to_string var)
+                     else
+                       Z3.Arithmetic.Integer.mk_const_s !context (Var.to_string var))
+    ~neg:(Z3.Arithmetic.mk_unary_minus !context)
+    ~plus:(fun p1 p2 -> Z3.Arithmetic.mk_add !context [p1; p2])
+    ~times:(fun p1 p2 -> Z3.Arithmetic.mk_mul !context [p1; p2])
+    ~pow:(fun b e -> Z3.Arithmetic.mk_power !context b (Z3.Arithmetic.Integer.mk_numeral_i !context e))
+  
+let from_formula context =
+  Formula.fold 
+    ~subject:(from_poly context)
+    ~le:(Z3.Arithmetic.mk_le !context)
+    ~correct:(Z3.Boolean.mk_true !context)
+    ~conj:(fun a1 a2 -> Z3.Boolean.mk_and !context [a1; a2])
+    ~wrong:(Z3.Boolean.mk_false !context)
+    ~disj:(fun a1 a2 -> Z3.Boolean.mk_or !context [a1; a2])
+
 (** SMT solver which uses the microsoft project Z3 *)
 module Z3Solver =
   struct
@@ -14,29 +35,8 @@ module Z3Solver =
                         ]
                     )
 
-    let from_poly = 
-      Polynomial.fold
-            ~const:(fun value -> Z3.Arithmetic.Integer.mk_numeral_i !context (OurInt.to_int value))
-            ~var:(fun var -> if Var.is_real var then
-                               Z3.Arithmetic.Real.mk_const_s !context (Var.to_string var)
-                             else
-                               Z3.Arithmetic.Integer.mk_const_s !context (Var.to_string var))
-            ~neg:(Z3.Arithmetic.mk_unary_minus !context)
-            ~plus:(fun p1 p2 -> Z3.Arithmetic.mk_add !context [p1; p2])
-            ~times:(fun p1 p2 -> Z3.Arithmetic.mk_mul !context [p1; p2])
-            ~pow:(fun b e -> Z3.Arithmetic.mk_power !context b (Z3.Arithmetic.Integer.mk_numeral_i !context e))
-
-    let from_formula =
-      Formula.fold 
-        ~subject:from_poly
-        ~le:(Z3.Arithmetic.mk_le !context)
-        ~correct:(Z3.Boolean.mk_true !context)
-        ~conj:(fun a1 a2 -> Z3.Boolean.mk_and !context [a1; a2])
-        ~wrong:(Z3.Boolean.mk_false !context)
-        ~disj:(fun a1 a2 -> Z3.Boolean.mk_or !context [a1; a2])
-        
     let result_is expected_result formula =
-      let formula = from_formula formula in
+      let formula = from_formula context formula in
       let optimisation_goal = Z3.Optimize.mk_opt !context in
       Z3.Optimize.add optimisation_goal [formula];
       let result = Z3.Optimize.check optimisation_goal in
@@ -78,10 +78,10 @@ module Z3Solver =
         else
           poly
       in
-      from_poly (List.fold_left generator Polynomial.zero coeffs_to_minimise)
+      from_poly context (List.fold_left generator Polynomial.zero coeffs_to_minimise)
     
     let get_model ?(coeffs_to_minimise=[]) formula =
-      let z3_expr = from_formula formula in
+      let z3_expr = from_formula context formula in
       let optimisation_goal = Z3.Optimize.mk_opt !context in
       Z3.Optimize.add optimisation_goal [z3_expr];
       let handle =
@@ -125,4 +125,116 @@ module Z3Solver =
         |> Option.some
       else None
       
+  end
+
+module IncrementalZ3Solver =
+  struct
+    type t = Z3.Optimize.optimize
+    
+    module Valuation = Valuation.Make(OurInt)
+
+    let context = ref (
+                      Z3.mk_context [
+                          ("model", "true");
+                          ("proof", "false");
+                        ]
+                    )
+
+    let create () =
+      Z3.Optimize.mk_opt !context
+
+    let push =
+      Z3.Optimize.push
+       
+    let pop =
+      Z3.Optimize.pop
+
+    let result_is expected opt =
+      opt
+      |> Z3.Optimize.check
+      |> fun result ->
+         if result == Z3.Solver.UNKNOWN then
+           raise (Failure ("SMT-Solver does not know a solution due to: " ^ Z3.Optimize.get_reason_unknown opt))
+         else
+           result == expected
+
+    let satisfiable =
+      result_is Z3.Solver.SATISFIABLE
+
+    let unsatisfiable =
+      result_is Z3.Solver.UNSATISFIABLE
+
+    let add opt formula =
+      formula
+      |> from_formula context
+      |> fun formula -> Z3.Optimize.add opt [formula]
+
+    (** Returns true iff the formula implies the positivity of the variable. *)
+    let is_positive opt (var: Var.t) =
+      push opt;
+      add opt Formula.Infix.(Polynomial.of_var var >= Polynomial.zero);
+      let result = unsatisfiable opt in
+      pop opt;
+      result
+
+    (** Returns true iff the formula implies the negativity of the variable. *)
+    let is_negative opt (var: Var.t) =
+      push opt;
+      add opt Formula.Infix.(Polynomial.of_var var <= Polynomial.zero);
+      let result = unsatisfiable opt in
+      pop opt;
+      result
+
+    let minimisation_goal opt (vars: Var.t list): Z3.Expr.expr =
+      let generator poly v =
+        if is_positive opt v then
+          Polynomial.(poly + of_var v)
+        else if is_negative opt v then
+          Polynomial.(poly - of_var v)
+        else
+          poly
+      in
+      from_poly context (List.fold_left generator Polynomial.zero vars)
+
+    let minimize opt vars =
+      ignore (Z3.Optimize.minimize opt (minimisation_goal opt vars))
+                      
+    let model opt =
+      if Z3.Optimize.check opt == Z3.Solver.SATISFIABLE then
+        opt
+        |> Z3.Optimize.get_model
+        |> Option.map (fun model ->
+               model
+               |> Z3.Model.get_const_decls
+               |> List.map (fun func_decl ->
+                      let var =
+                        func_decl
+                        |> Z3.FuncDecl.get_name
+                        |> Z3.Symbol.get_string
+                        |> Var.of_string
+                      in
+                      let value =
+                        func_decl
+                        |> Z3.Model.get_const_interp model                        
+                        |> Option.get (* Should be fine here *)
+                        |> (fun expr ->
+                          if Z3.Arithmetic.is_int expr then
+                            expr
+                            |> Z3.Arithmetic.Integer.get_int
+                            |> OurInt.of_int
+                          else
+                            expr
+                            |> Z3.Arithmetic.Real.get_ratio
+                            (* TODO Round shouldnt be the solution, but do we need this anyway, since we ignore the values of helper variables? *)
+                            |> Ratio.round_ratio
+                        )
+                      in
+                      (var, value)
+                    )
+             )
+        |? []
+        |> Valuation.from
+        |> Option.some
+      else None
+
   end
--- src/smt/SMT.mli
+++ src/smt/SMT.mli
@@ -21,3 +21,30 @@ sig
     
     val check_negativity : Formula.t -> Polynomial.t -> bool
   end
+
+module IncrementalZ3Solver :
+sig
+  type t
+
+  (** Creates a new incremental smt solver. *)
+  val create : unit -> t
+     
+  (** Creates a backtracking point. *)
+  val push : t -> unit
+
+  (** Backtrack one backtracking point. Note that an exception is thrown if Pop is called without a corresponding push. *)
+  val pop : t -> unit
+
+  (** Checks if the current state is satisfiable. *)
+  val satisfiable : t -> bool
+
+  (** Asserts the formula. *)
+  val add : t -> Formula.t -> unit
+
+  (** Sets the variables, which absolute value should be minimized. *)
+  val minimize : t -> Var.t list -> unit
+
+  (** Returns a model of the current state, if the state is satisfiable. *)
+  val model : t -> Polynomial.valuation Option.t
+
+end
