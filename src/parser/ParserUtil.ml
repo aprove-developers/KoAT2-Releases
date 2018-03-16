f2054fa874b40dfb48e1a6cfd20f443c304570da
Marcel Hark <marcel.hark@rwth-aachen.de>
2018-03-16 16:55:02 +0100

    Parser probabilistisch gemacht

--- src/parser/ParserUtil.ml
+++ src/parser/ParserUtil.ml
@@ -13,7 +13,24 @@ let mk_transition lhs (cost: Polynomial.t) rhs (formula: Formula.t) (vars:Var.t 
             ~targets:(Tuple2.second rhs)
             ~patterns:(List.map Var.of_string (Tuple2.second lhs))
             ~guard:constr 
-            ~cost:cost,
+            ~cost:cost
+            ~probability:1.0,
+          (Location.of_string (Tuple2.first (List.hd (Tuple2.second rhs)))))
+       )
+  |> List.map (fun (l,t,l') -> (l,t ~vars,l'))
+  
+let mk_transition_prob lhs (cost: Polynomial.t) (probability: float) rhs (formula: Formula.t) (vars:Var.t list): Transition.t list =
+  formula
+  |> Formula.constraints
+  |> List.map (fun constr ->
+	 (Location.of_string (Tuple2.first lhs),
+          TransitionLabel.mk
+            ~com_kind:(Tuple2.first rhs)
+            ~targets:(Tuple2.second rhs)
+            ~patterns:(List.map Var.of_string (Tuple2.second lhs))
+            ~guard:constr 
+            ~cost:cost
+            ~probability:probability,
           (Location.of_string (Tuple2.first (List.hd (Tuple2.second rhs)))))
        )
   |> List.map (fun (l,t,l') -> (l,t ~vars,l'))
@@ -22,12 +39,13 @@ let default_vars =
   ["x"; "y"; "z"; "u"; "v"; "w"; "p"; "q"]
   |> List.map Var.of_string
 
-let mk_transition_simple (start: string) (cost: Polynomial.t) (rhs: string * (string * Polynomial.t list) list) (formula: Formula.t): Transition.t list =
+let mk_transition_prob_simple (start: string) (cost: Polynomial.t) (probability: float) (rhs: string * (string * Polynomial.t list) list) (formula: Formula.t): Transition.t list =
   formula
   |> Formula.constraints
   |> List.map (fun constr ->
 	 (Location.of_string start,
           TransitionLabel.mk
+            ~probability:probability
             ~com_kind:(Tuple2.first rhs)
             ~targets:(Tuple2.second rhs)
             ~patterns:default_vars
@@ -36,6 +54,22 @@ let mk_transition_simple (start: string) (cost: Polynomial.t) (rhs: string * (st
             ~vars:default_vars, (Location.of_string (Tuple2.first (List.hd (Tuple2.second rhs)))))
        )
 
+let mk_transition_simple (start: string) (cost: Polynomial.t) (rhs: string * (string * Polynomial.t list) list) (formula: Formula.t): Transition.t list =
+  formula
+  |> Formula.constraints
+  |> List.map (fun constr ->
+	 (Location.of_string start,
+          TransitionLabel.mk
+            ~probability:1.0
+            ~com_kind:(Tuple2.first rhs)
+            ~targets:(Tuple2.second rhs)
+            ~patterns:default_vars
+            ~guard:constr 
+            ~cost:cost
+            ~vars:default_vars, (Location.of_string (Tuple2.first (List.hd (Tuple2.second rhs)))))
+       )
+
+       
 let mk_program_simple (transitions: Transition.t list): Program.t =
   transitions
   |> List.hd
