786d0d5678515b3039d113156a171d2734a4c726
Marcel Hark <marcel.hark@rwth-aachen.de>
2018-04-11 13:43:21 +0200

    mk_transition_prob and mk_transition_simple_prob implemented, not yet tested!

--- src/parser/ParserUtil.ml
+++ src/parser/ParserUtil.ml
@@ -18,45 +18,33 @@ let mk_transition lhs (cost: Polynomial.t) rhs (formula: Formula.t) (vars:Var.t 
        )
   |> List.map (fun (l,t,l') -> (l,t ~vars,l'))
 
-let cartesian l l' = 
-  List.concat (List.map (fun e -> List.map (fun e' -> (e,e')) l') l)
-  
-  
-let mk_transition_prob lhs (costs: Polynomial.t list) (probabilities: float list) rhs (formula: Formula.t) (vars:Var.t list): Transition.t list =
+  (*So far recursion cannot be parsed, therefore the location is taken as the head of the list as non singleton lists yield an exception*)
+  let mk_transition_prob lhs (cost: Polynomial.t) (rhs: (float * string * (string * (Polynomial.t list)) list) list) (formula: Formula.t) (vars:Var.t list): Transition.t list =
   formula
   |> Formula.constraints
-  |> fun constraint_list -> cartesian constraint_list (Tuple2.first (Tuple2.second rhs))
-  |> List.map (fun (constr,goal) ->
-	 (Location.of_string (Tuple2.first lhs),
-          TransitionLabel.mk_prob
-            ~costs:costs
-            ~com_kinds:(Tuple2.first rhs)
-            ~targets_list:(Tuple2.second (Tuple2.second rhs))
-            ~patterns:(List.map Var.of_string (Tuple2.second lhs))
-            ~guard:constr 
-            ~probabilities:probabilities,
-          (Location.of_string goal))
-       )
+  |> List.map (fun constr ->
+          List.map (fun (prob, comkind, targets) ->
+          let id = unique() in
+          (Location.of_string (Tuple2.first lhs),
+            TransitionLabel.mk_prob
+              ~cost:cost
+              ~com_kind:comkind
+              ~targets:targets
+              ~patterns:(List.map Var.of_string (Tuple2.second lhs))
+              ~guard:constr
+              ~id:id
+              ~probability:prob,
+            (Location.of_string (Tuple2.first (List.hd targets))))
+       ) rhs)
+  |> List.concat
   |> List.map (fun (l,t,l') -> (l,t ~vars,l'))
+  
+let cartesian l l' = 
+  List.concat (List.map (fun e -> List.map (fun e' -> (e,e')) l') l)
 
 let default_vars =
   ["x"; "y"; "z"; "u"; "v"; "w"; "p"; "q"]
   |> List.map Var.of_string
-
-let mk_transition_prob_simple (start: string) (cost: Polynomial.t) (probability: float) (rhs: string * (string * Polynomial.t list) list) (formula: Formula.t): Transition.t list =
-  formula
-  |> Formula.constraints
-  |> List.map (fun constr ->
-	 (Location.of_string start,
-          TransitionLabel.mk
-            ~probability:probability
-            ~com_kind:(Tuple2.first rhs)
-            ~targets:(Tuple2.second rhs)
-            ~patterns:default_vars
-            ~guard:constr 
-            ~cost:cost
-            ~vars:default_vars, (Location.of_string (Tuple2.first (List.hd (Tuple2.second rhs)))))
-       )
 
 let mk_transition_simple (start: string) (cost: Polynomial.t) (rhs: string * (string * Polynomial.t list) list) (formula: Formula.t): Transition.t list =
   formula
@@ -64,7 +52,6 @@ let mk_transition_simple (start: string) (cost: Polynomial.t) (rhs: string * (st
   |> List.map (fun constr ->
 	 (Location.of_string start,
           TransitionLabel.mk
-            ~probability:1.0
             ~com_kind:(Tuple2.first rhs)
             ~targets:(Tuple2.second rhs)
             ~patterns:default_vars
@@ -72,6 +59,26 @@ let mk_transition_simple (start: string) (cost: Polynomial.t) (rhs: string * (st
             ~cost:cost
             ~vars:default_vars, (Location.of_string (Tuple2.first (List.hd (Tuple2.second rhs)))))
        )
+       
+let mk_transition_simple_prob (start: string) (cost: Polynomial.t) (rhs: (float * string * (string * Polynomial.t list) list) list) (formula: Formula.t): Transition.t list =
+  formula
+  |> Formula.constraints
+  |> List.map (fun constr ->
+          List.map (fun (prob, comkind, targets) ->
+          let id = unique() in
+          (Location.of_string start,
+            TransitionLabel.mk_prob
+              ~cost:cost
+              ~com_kind:comkind
+              ~targets:targets
+              ~patterns:default_vars
+              ~guard:constr
+              ~vars:default_vars
+              ~id:id
+              ~probability:prob,
+            (Location.of_string (Tuple2.first (List.hd targets))))
+       ) rhs)
+  |> List.concat
 
        
 let mk_program_simple (transitions: Transition.t list): Program.t =
