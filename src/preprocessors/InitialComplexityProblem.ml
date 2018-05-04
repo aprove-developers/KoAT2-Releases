39767fd3baa88130d974de73cf061fc9d8e189d4
Marcel Hark <marcel.hark@rwth-aachen.de>
2018-05-04 13:59:07 +0200

    New preprocessor for a neat input

---
+++ src/preprocessors/InitialComplexityProblem.ml
@@ -0,0 +1,27 @@
+open Batteries
+open ProgramTypes
+   
+(** This preprocessor cuts all unreachable locations (and all transitions connected to them) from the program. *)
+
+let logger = Logging.(get Preprocessor)
+   
+module LocationSet = Set.Make(Location)
+
+(** Returns a set of all locations which are reachable from the given start location. *)
+let reachable_locations graph start : LocationSet.t  =
+  let module Traverse = Graph.Traverse.Bfs(TransitionGraph) in
+  Traverse.fold_component LocationSet.add LocationSet.empty graph start
+
+let unreachable_locations graph start : LocationSet.t =
+  LocationSet.diff (TransitionGraph.locations graph) (reachable_locations graph start)
+
+let transform_program program = 
+  let unreachable_locations = unreachable_locations (Program.graph program) (Program.start program) in
+  if LocationSet.is_empty unreachable_locations then
+    MaybeChanged.same program
+  else
+    let remove location program =
+      Logger.(log logger INFO (fun () -> "cut_unreachable_locations", ["location", Location.to_string location]));
+      Program.remove_location program location
+    in
+    MaybeChanged.changed (LocationSet.fold remove unreachable_locations program)
