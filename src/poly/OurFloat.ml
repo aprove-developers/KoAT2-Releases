b7d0c238352d31b38dd833e00448dabf4ad91428
Tom Küspert <tom.kuespert@gmail.com>
2018-05-29 00:29:55 +0200

    Added OurFloat

---
+++ src/poly/OurFloat.ml
@@ -0,0 +1,19 @@
+open Batteries
+
+include Number.MakeNumeric(Float)
+let (=~=) = equal
+
+(* TODO Make it possible to pow with Big_int *)
+let pow i (n: int) = pow i (of_int n)  
+
+let max a b =
+  if Compare.(a >= b) then
+    a
+  else
+    b
+
+let min a b =
+  if Compare.(a <= b) then
+    a
+  else
+    b
