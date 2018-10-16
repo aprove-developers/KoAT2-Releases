43155558a3bc9da8b1bdcd09abb620a5a15e22db
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2018-10-16 20:25:08 +0200

    Added a RVG Type for expected result variables

--- src/transitionsystem/RVGTypes.mli
+++ src/transitionsystem/RVGTypes.mli
@@ -2,7 +2,7 @@ open Batteries
 open ProgramTypes
 
 module Make_RV :
-  functor (Trans : 
+  functor (Trans :
                   sig
                     type t
                     val same: t -> t -> bool
@@ -43,7 +43,30 @@ sig
 
   (** Returns all transitions that are used in the SCC of the RVG. *)
   val transitions : RV.t list -> Transition.t Enum.t
-    
+
   val rvg : [`Lower | `Upper] -> Program.t -> t
 
+end
+
+module ERVG :
+sig
+  module RV : sig include module type of Make_RV (RVTransitions.TransitionForExpectedSize) end
+  include module type of Graph.Persistent.Digraph.ConcreteBidirectional(struct
+                             include RV
+                             let equal = same
+                             let compare = compare_same
+                           end)
+
+  val rvs_to_id_string : RV.t list -> string
+
+  val pre : t -> RV.t -> RV.t Enum.t
+
+  (** Returns all the entry points of the SCC.
+      Those are all result variables that are in the RVG, but not in the SCC and lead to any result variable in the RVG. *)
+  val entry_points : t -> RV.t list -> RV.t Enum.t
+
+  (** Returns all transitions that are used in the SCC of the RVG. *)
+  val transitions : RV.t list -> RVTransitions.TransitionForExpectedSize.t Enum.t
+
+  val rvg : [`Lower | `Upper] -> Program.t -> t
 end
