71dddec2fca0652b3be33dbdcd689e7694461858
Tom Küspert <tom.kuespert@gmail.com>
2018-10-25 16:40:45 +0200

    updated the input for the probabilities

--- src/main/ExactCommand.ml
+++ src/main/ExactCommand.ml
@@ -7,9 +7,16 @@ let description = "Testing for exactRuntime"
 let command = "exact"
 
 type params = {
-    input : string; [@aka ["i"]] [@pos 0]
-    (** Polynomial in String form we want to get the roots from*)
-    
+    m : string; [@pos 0] [@aka ["m"]]
+    (** biggest increase in the variable *)
+    k : string; [@pos 1]
+    (** biggest decrease in the variable *) 
+    p' : string; [@pos 2]
+    (** Probability to set the variable to 0. *)
+    probs : string list; [@pos 3] [@sep ',']
+    (** Probabilities for the program, starting at the top*)
+    precision : int; [@default 100] [@aka ["prec"]]
+    (** Bit precision for Calculations in Sage Math *)
   } [@@deriving cmdliner, show]
 
 let read_process_lines command =
@@ -30,14 +37,24 @@ let run (params: params) =
   Logging.(use_loggers [ExactRuntime, Logger.DEBUG]); 
   let logger = Logging.(get ExactRuntime) in
   let execute () =
-    params.input
-    |> fun input -> read_process_lines ("sage src/exact_runtime/exact_runtime.sage " ^ input)
+    (* No idea if this actaully works, may need adjustment in the future *)
+    let sage_path = 
+    Sys.argv.(0)
+    |> Filename.dirname
+    |> fun koat_path -> koat_path ^ "/../exact_runtime/exact_runtime.sage"
+    in
+    Filename.current_dir_name
+    |> print_string;
+    [[string_of_int params.precision; params.m; params.k; params.p']; params.probs]
+    |> List.concat
+    |> String.concat " "
+    |> fun input -> read_process_lines (sage_path ^ " " ^ input)
     |> String.concat ""
+    |> tap(fun string -> print_string (string ^ "\n"))
   in 
   Logger.with_log logger Logger.DEBUG 
                   (fun () -> "exact runtime", [])
                   ~result:(fun bound ->
-                    print_string (bound ^ "\n");
                     bound
                   )
                   execute
