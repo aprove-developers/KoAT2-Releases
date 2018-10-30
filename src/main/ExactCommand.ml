c391f122c59cf8b309e3bf92427f6c850ef663c8
Tom Küspert <tom.kuespert@gmail.com>
2018-10-30 23:22:12 +0100

    added time to output and improved the path finding for the sage file

--- src/main/ExactCommand.ml
+++ src/main/ExactCommand.ml
@@ -33,29 +33,40 @@ let read_process_lines command =
   end;
   List.rev !lines
 
+let get_koat_path (command: String.t) = 
+  let which_output = read_process_lines ("which " ^ command) |> String.concat "" in
+  (* Returns nothing when it is called as the executable at the current working directory *)
+  if String.equal which_output "" then
+    Filename.current_dir_name
+  (* Else a path to the koatP executable is given whether it is the 
+  relative path or the total path. Though it is handled the same way
+  I have split it up for now in case it has to be changed later *)
+  else if Char.equal which_output.[0] '/' then
+    Filename.dirname which_output
+  else Filename.dirname which_output
+
+
 let run (params: params) =
   Logging.(use_loggers [ExactRuntime, Logger.DEBUG]); 
   let logger = Logging.(get ExactRuntime) in
   let execute () =
     (* No idea if this actaully works, may need adjustment in the future *)
     let sage_path = 
-    Sys.argv.(0)
-    |> Filename.dirname
+    get_koat_path Sys.argv.(0)
     |> fun koat_path -> koat_path ^ "/../exact_runtime/exact_runtime.sage"
     in
-    Filename.current_dir_name
-    |> print_string;
     [[string_of_int params.precision; params.m; params.k; params.p']; params.probs]
     |> List.concat
     |> String.concat " "
-    |> fun input -> read_process_lines (sage_path ^ " " ^ input)
-    |> String.concat ""
-    |> tap(fun string -> print_string (string ^ "\n"))
+    |> fun input -> "sage " ^ sage_path ^ " " ^ input
+    |> read_process_lines
+    (* Printed here for output reasons only *)
+    |> tap(fun output -> output |> String.concat "\n" |> print_string; print_string "\n")
   in 
   Logger.with_log logger Logger.DEBUG 
                   (fun () -> "exact runtime", [])
                   ~result:(fun bound ->
-                    bound
+                    String.concat "; " bound
                   )
                   execute
   |> ignore
