c6714d417b1b75afbdf9669b37e925859a4c0891
Tom Küspert <tom.kuespert@gmail.com>
2018-10-31 01:08:52 +0100

    Changed the koat command to use the file instead

--- src/main/ExactCommand.ml
+++ src/main/ExactCommand.ml
@@ -7,17 +7,9 @@ let description = "Testing for exactRuntime"
 let command = "exact"
 
 type params = {
-    m : string; [@pos 0] [@aka ["m"]]
-    (** biggest increase in the variable *)
-    k : string; [@pos 1]
-    (** biggest decrease in the variable *) 
-    p' : string; [@pos 2]
-    (** Probability to set the variable to 0. *)
-    probs : string list; [@pos 3] [@sep ',']
-    (** Probabilities for the program, starting at the top*)
-    precision : int; [@default 100] [@aka ["prec"]]
-    (** Bit precision for Calculations in Sage Math *)
-  } [@@deriving cmdliner, show]
+  in_file : string; [@pos 0] [@aka ["f"]]
+  (** path to input file *)
+} [@@deriving cmdliner, show]
 
 let read_process_lines command =
   let lines = ref [] in
@@ -36,7 +28,7 @@ let read_process_lines command =
 let get_koat_path (command: String.t) = 
   let which_output = read_process_lines ("which " ^ command) |> String.concat "" in
   (* Returns nothing when it is called as the executable at the current working directory *)
-  if String.equal which_output "" then
+  if String.is_empty which_output then
     Filename.current_dir_name
   (* Else a path to the koatP executable is given whether it is the 
   relative path or the total path. Though it is handled the same way
@@ -45,7 +37,6 @@ let get_koat_path (command: String.t) =
     Filename.dirname which_output
   else Filename.dirname which_output
 
-
 let run (params: params) =
   Logging.(use_loggers [ExactRuntime, Logger.DEBUG]); 
   let logger = Logging.(get ExactRuntime) in
@@ -53,11 +44,9 @@ let run (params: params) =
     (* No idea if this actaully works, may need adjustment in the future *)
     let sage_path = 
     get_koat_path Sys.argv.(0)
-    |> fun koat_path -> koat_path ^ "/../exact_runtime/exact_runtime.sage"
+    |> fun koat_path -> koat_path ^ "/../exact_runtime/exact_runtime_from_file.sage"
     in
-    [[string_of_int params.precision; params.m; params.k; params.p']; params.probs]
-    |> List.concat
-    |> String.concat " "
+    params.in_file
     |> fun input -> "sage " ^ sage_path ^ " " ^ input
     |> read_process_lines
     (* Printed here for output reasons only *)
