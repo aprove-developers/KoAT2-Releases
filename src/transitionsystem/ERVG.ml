c623fde2064cd5957b3b24948ad1eea7921788f5
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2018-10-31 16:17:04 +0100

    Correction in the construction of the ERVG graph

--- src/transitionsystem/ERVG.ml
+++ src/transitionsystem/ERVG.ml
@@ -49,22 +49,21 @@ let rvg kind (program: Program.t) =
          |> List.cartesian_product (GeneralTransition.targets post_gt |> LocationSet.to_list)
          |> List.map (fun (l,(gt,v)) -> ((gt,l),v) ) ) rvg
     in
-    let pre_nodes (post_transition: GeneralTransition.t) (l: Location.t) (post_var: Var.t) =
-      ExpLocalSizeBound.vars ((post_transition,l),post_var)
+    let pre_nodes (post_transition: GeneralTransition.t) (post_l: Location.t) (post_var: Var.t) =
+      ExpLocalSizeBound.vars ((post_transition,post_l),post_var)
       |> VarSet.enum
       |> Enum.cartesian_product (Program.pre_gt program post_transition
-                                 |> GeneralTransitionSet.enum
-                                 |> Enum.map (fun gt' -> (gt',GeneralTransition.start post_transition)))
-      |> failwith ""
-      |> Enum.map (fun (gt,pre_transition,pre_var) -> ((gt,pre_transition),pre_var,post_var))
-      |> failwith ""
+                                 |> GeneralTransitionSet.enum)
+      |> Enum.map (fun (pre_gt,pre_var) -> ((pre_gt,GeneralTransition.start post_transition, post_l),pre_var,post_var))
     in
     Program.vars program
     |> VarSet.enum
     |> Enum.cartesian_product (GeneralTransition.targets post_gt |> LocationSet.enum)
     |> Enum.map (fun (l,v) -> pre_nodes post_gt l v)
     |> Enum.flatten
-    |> Enum.fold (fun rvg ((pre_gt,l),pre_var,post_var) -> add_edge rvg ((pre_gt,l),pre_var) ((pre_gt,l),post_var)) rvg_with_vertices
+    |> Enum.fold 
+         (fun rvg ((pre_gt,pre_l, post_l),pre_var,post_var) -> add_edge rvg ((pre_gt,pre_l),pre_var) ((post_gt,post_l),post_var)) 
+         rvg_with_vertices
   in
   Program.transitions program
   |> GeneralTransitionSet.from_transitionset
