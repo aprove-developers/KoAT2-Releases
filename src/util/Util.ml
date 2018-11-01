b8627215411bb0f9d4c18bf72b79f3535d66f863
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2018-11-01 21:52:11 +0100

    Make_SafeAny is unnecessary

--- src/util/Util.ml
+++ src/util/Util.ml
@@ -69,11 +69,3 @@ let safe_head l =
   match l with
   | [] -> None
   | (a :: _) -> Some a
-
-module Make_SafeAny (S : Set.S) = 
-  struct
-    let safe_any set = 
-      match S.cardinal set with
-      | 0 -> None
-      | _ -> Some (S.any set)
-  end
