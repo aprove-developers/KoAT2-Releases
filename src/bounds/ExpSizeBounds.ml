af80b54ad540c3bb86b2ae0c371ab81e4a3aa116
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2018-11-01 21:56:34 +0100

    Adjusted logger and some minor changes

--- src/bounds/ExpSizeBounds.ml
+++ src/bounds/ExpSizeBounds.ml
@@ -2,7 +2,7 @@ open Batteries
 open ProgramTypes
 open RVGTypes
 
-let logger = Logging.(get Size)
+let logger = Logging.(get ExpSize)
 
 module ERV = Make_RV(RVTransitions.TransitionForExpectedSize)
 
@@ -12,12 +12,13 @@ let improve_scc kind program ervg appr = function
                       (fun kind -> Approximation.sizebound kind appr)
                       (fun kind -> Approximation.expsizebound kind appr)
                       (Approximation.timebound appr) ((gt,l),v) in
+
      Approximation.add_expsizebound kind new_bound (gt,l) v appr
+
   | scc ->
      let new_bound = ExpNontrivialSizeBounds.compute kind program ervg (Approximation.timebound appr) (Approximation.exptimebound appr)
                                                                        (fun kind -> Approximation.sizebound kind appr)
                                                                        (fun kind -> Approximation.expsizebound kind appr) scc in
-     Printf.printf "scc expsize\n";
      Approximation.add_expsizebounds kind new_bound scc appr
 
 let improve program appr =
@@ -25,7 +26,7 @@ let improve program appr =
     let module C = Graph.Components.Make(ERVG) in
     [`Lower; `Upper]
     |> List.fold_left (fun appr kind ->
-           let rvg = ERVG.rvg kind program in
+           let rvg = ERVG.rvg program in
            List.fold_left (fun appr scc -> improve_scc kind program rvg appr scc) appr (List.rev (C.scc_list rvg))
          ) appr
   in Logger.with_log logger Logger.INFO
