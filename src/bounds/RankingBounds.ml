2cc785128060dc9777d2b0e26b02509cfe3d1a79
nils.m.lommen <nils.lommen@rwth-aachen.de>
2019-07-03 14:34:36 +0200

    coef. implemented

--- src/bounds/RankingBounds.ml
+++ src/bounds/RankingBounds.ml
@@ -1,6 +1,9 @@
 open Batteries
 open Polynomials
+open Atoms
+open Constraints
 open ProgramTypes
+open Formulas
 
 let logger = Logging.(get Time)
 
@@ -32,44 +35,74 @@ let apply (get_sizebound: [`Lower | `Upper] -> Transition.t -> Var.t -> Bound.t)
        ~higher:(get_sizebound `Upper transition)
 
 
+(* method transforms polynome to parapolynom*)
+let as_parapoly label var =
+    match TransitionLabel.update label var with
+    (** Correct? In the nondeterministic case we just make it deterministic? *)
+    | None -> ParameterPolynomial.of_var var
+    | Some p -> ParameterPolynomial.of_polynomial p
+
 (* Compute new Timebounds for MRFs*)
 
-(* Computes eta_1 to eta_k*)
-let compute_eta_k (k:int) = List.init k (fun i -> 2)
+module Valuation = Valuation.Make(OurInt)
 
-(* Returns sum_i list1(i) *)
-let rec sum list =
-  match list with
-  | [] -> 0
-  | [x] -> x
-  | x::xs -> x + sum xs
+(*Generates formula for SMT Solver for mues *)
+let template_formula (rank : (Location.t -> Polynomial.t) list) (k:int) (l,t,l') newVariables =
+  (*generate templates*)
+  let polynomials = (List.init k (fun i -> 
+    (ParameterPolynomial.mult_with_const (Polynomial.of_var (List.nth newVariables i)) (ParameterPolynomial.of_polynomial ((List.nth rank i) l))))) in
+  let f_k = (ParameterPolynomial.of_polynomial (((List.nth rank k) l))) in
+  let f_k_update = (ParameterPolynomial.substitute_f (as_parapoly t) (ParameterPolynomial.of_polynomial((List.nth rank k) l'))) in
+  let last_poly = ParameterPolynomial.sub f_k f_k_update in
+  let complete_poly = (List.fold_left (fun a b -> (ParameterPolynomial.add a b)) ParameterPolynomial.zero (last_poly::polynomials)) in
+  (*Built formula*)
+  let atom = ParameterAtom.Infix.(complete_poly >= ParameterPolynomial.one) in 
+  let formula = Formula.mk (ParameterConstraint.farkas_transform (TransitionLabel.guard t) atom ) in 
 
-(* Returns sum_i list1(i) * list2(i) *)
+  let formulas_mues_positve = List.init k (fun i -> 
+                    (Formula.lift (Atom.Infix.(Polynomial.of_var(List.nth newVariables i) >= Polynomial.zero)))) in
+  let formula_mues_positve = List.fold_left 
+                        (fun a b -> Formula.mk_and a b) 
+                              (Formula.lift (Atom.Infix.(Polynomial.of_var(List.nth newVariables (k - 1)) >= Polynomial.one))) 
+                              formulas_mues_positve in
+  Formula.mk_and formula_mues_positve formula
+
+(*Calculates mues for coefficients  1,...,k - 1 *)
+let compute_mue_k (rank : (Location.t -> Polynomial.t) list) (k:int) (l,t,l') =  
+    (*Use SMT Solver *)
+    let newVariables = Var.fresh_id_list Var.Int k in
+    let formula = template_formula rank k (l,t,l') newVariables in 
+    let opt = SMT.IncrementalZ3Solver.create () in
+    SMT.IncrementalZ3Solver.add opt formula;
+    let model = SMT.IncrementalZ3Solver.model opt in
+    List.init k (fun i ->  OurInt.to_float (Valuation.eval_opt (List.nth newVariables i) (Option.get model) |? OurInt.zero))
+
+(* Returns sum_i (list1(i) := (c_i,d_i)_i -> c_i )* (list2(i) := (mu_i)_i) *)
 let rec sumProduct (list1, list2) =
   match (list1,list2) with
-    | ([x],[y]) -> x * y
-    | (x::xs,y::ys) -> x * y + sumProduct (xs, ys)
-    | _ -> 0
+    | ([(x,_)],[y]) -> x *. y
+    | ((x,_)::xs,y::ys) -> x *. y +. sumProduct (xs, ys)
+    | _ -> 0.
 
 (* Calculates recursive all coefficient c_k,d_k*)
-let rec compute_coefficients (degree:int) list =
+let rec compute_coefficients (degree:int) (rank : (Location.t -> Polynomial.t) list) (l,t,l') list =
   match list with
-  | [] -> compute_coefficients degree [(1,1)]
+  | [] -> compute_coefficients degree rank (l,t,l') [(1.,1.)]
   | _  ->
     if degree == List.length list then list
      else
        let k = List.length list in
-       let etas = compute_eta_k k in
-       let cks = List.init k (fun i -> fst (List.nth list i)) in
-       let ck = (1 + sum etas) + int_of_float(ceil (float (sumProduct (cks, etas)) /. float k)) + (List.nth etas (k - 1)) * (snd (List.nth list (k - 1))) in
-       let dk = (List.nth etas (k - 1)) * (int_of_float (ceil (float (snd (List.nth list (k - 1))) /. float k))) in
-       compute_coefficients (degree:int) (List.append list [(ck,dk)])
+       let mues = compute_mue_k rank k (l,t,l') in
+       let sum_mues = (List.fold_left (fun a b -> a +. b) 0. mues) in
+       let ck = (1. +. sum_mues) +. (sumProduct (list, mues)) /. (float k) +. (List.nth mues (k - 1)) *.   (snd (List.nth list (k - 1))) in
+       let dk = (List.nth mues (k - 1)) *. (snd (List.nth list (k - 1))) /. (float (k + 1)) in
+       compute_coefficients (degree:int) rank (l,t,l') (List.append list [(ck,dk)])
 
 (* Returns max_i c_i /. d_i *)
 let rec maximum_coefficients list =
   match list with
-  | [(x,y)] -> int_of_float(ceil (float x /. float y))
-  | (x,y) :: rest -> max (int_of_float (ceil (float x /. float y))) (maximum_coefficients rest)
+  | [(x,y)] -> int_of_float(ceil (x /. y))
+  | (x,y) :: rest -> max (int_of_float (ceil  (x /. y))) (maximum_coefficients rest)
   | _ -> 0
 
 (* Constructs the nested max bounds of all functions of the mrf*)
@@ -87,12 +120,9 @@ let compute_bound_mrf (appr: Approximation.t) (program: Program.t) (rank: Multip
    |> entry_transitions program
    |> List.enum
    |> Enum.map (fun (l,t,l') ->
-       let timebound = Approximation.timebound appr (l,t,l') in
-       let coefficients = (compute_coefficients (MultiphaseRankingFunction.degree rank) []) in
-       Printf.printf "%i" (MultiphaseRankingFunction.degree rank);
-       for i = 0 to List.length coefficients - 1 do
-         Printf.printf "coef. c_%i: %i - d_%i: %i \n" i (fst (List.nth coefficients i)) i (snd (List.nth coefficients i));
-       done;
+       let decreasing = MultiphaseRankingFunction.decreasing rank in
+       let timebound = Approximation.timebound appr (l,t,l') in 
+       let coefficients = (compute_coefficients (MultiphaseRankingFunction.degree rank) (MultiphaseRankingFunction.rank rank) decreasing  []) in
          let maximum_coefficient = (maximum_coefficients coefficients) in
          let evaluate = (fun rank -> (apply (fun kind -> Approximation.sizebound kind appr) rank) (l,t,l')) in
          let var = (List.init (MultiphaseRankingFunction.degree rank) (fun i -> (evaluate ((List.nth (MultiphaseRankingFunction.rank rank) i) l')))) in
