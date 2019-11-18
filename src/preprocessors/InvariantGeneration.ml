eaae0de0be4b04f556bcbf11eee04d16bf766497
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2019-11-18 20:46:41 +0100

    drastically improved invariant generation by not directly performing widening

--- src/preprocessors/InvariantGeneration.ml
+++ src/preprocessors/InvariantGeneration.ml
@@ -9,9 +9,12 @@ open PolyTypes
     This way more restrictive information is available locally in the transitions. *)
 
 let logger = Logging.(get Preprocessor)
-   
+
 (** A map from program locations to anything. *)
-module LocationMap = Hashtbl.Make(Location)
+module LocationMap   = Hashtbl.Make(Location)
+
+(* The number of steps per transition to be performed during fixpoint iteration without widening*)
+let max_steps_without_widening = 10
 
 (** An abstract value for all values of variable at every program location. *)
 type 'a program_abstract = ('a Apron.Abstract1.t) LocationMap.t
@@ -23,20 +26,20 @@ module Koat2Apron =
     (** Converts a koat variable to its apron equivalent. *)
     let var_to_apron: Var.t -> Apron.Var.t =
       Apron.Var.of_string % Var.to_string
-      
+
     (** Converts a koat variable set to its apron equivalent. *)
     let vars_to_apron: VarSet.t -> Apron.Var.t array =
       VarSet.map_to_array var_to_apron
-      
+
     (** Converts a koat integer to its apron equivalent. *)
     (** TODO Usage of OurInt.to_int breaks usage of big_int *)
     let const_to_apron: OurInt.t -> Apron.Coeff.t =
       Apron.Coeff.s_of_int % OurInt.to_int
-      
+
     (** Converts a koat polynomial to its apron equivalent. *)
     let poly_to_apron (environment: Apron.Environment.t) (poly: Polynomial.t): Apron.Texpr1.t =
-      (* TODO Texpr1.Up: Round in which direction? *) 
-      let open Apron in 
+      (* TODO Texpr1.Up: Round in which direction? *)
+      let open Apron in
       Polynomial.fold
         ~const:(fun c ->
           Texpr1.Cst (const_to_apron c)
@@ -61,7 +64,7 @@ module Koat2Apron =
         )
         poly
       |> Apron.Texpr1.of_expr environment
-      
+
     (** Converts a koat atom to its apron equivalent *)
     let atom_to_apron (environment: Apron.Environment.t) (atom: Atom.t): Apron.Tcons1.t =
       let open Apron in
@@ -71,7 +74,7 @@ module Koat2Apron =
         ~le:(fun expr1 expr2 -> Tcons1.make (Texpr1.binop Texpr1.Sub expr2 expr1 Texpr1.Int Texpr1.Up) Tcons1.SUPEQ)
         ~lt:(fun expr1 expr2 -> Tcons1.make (Texpr1.binop Texpr1.Sub expr2 expr1 Texpr1.Int Texpr1.Up) Tcons1.SUP)
         atom
-      
+
     (** Converts a koat constraint to its apron equivalent *)
     let constraint_to_apron (environment: Apron.Environment.t) (constr: Constraint.t): Apron.Tcons1.earray =
       let open Apron in
@@ -89,9 +92,9 @@ module Koat2Apron =
         Tcons1.array_set constraints i (Array.get single_constraints i)
       done;
       constraints
-      
+
   end
-  
+
 (** This module provides functions which convert apron datastructures to equivalent koat datastructures. *)
 module Apron2Koat =
   struct
@@ -99,7 +102,7 @@ module Apron2Koat =
     (** Converts an apron variable to its koat equivalent. *)
     let var_from_apron: Apron.Var.t -> Var.t =
       Var.of_string % Apron.Var.to_string
-    
+
     (** Converts an apron variable set to its koat equivalent. *)
     let vars_from_apron: Apron.Var.t array -> VarSet.t =
       VarSet.of_enum % Array.enum % Array.map var_from_apron
@@ -114,7 +117,7 @@ module Apron2Koat =
       | Coeff.Scalar (Scalar.Mpqf float) -> (OurInt.of_int % int_of_float % Mpqf.to_float) float
       | Coeff.Scalar (Scalar.Mpfrf float) -> (OurInt.of_int % int_of_float % Mpfrf.to_float) float
       | Coeff.Interval _ -> raise (Failure "Apron Intervals are not allowed!")
-      
+
     (** Converts an apron polynomial to its koat equivalent. *)
     let poly_from_apron (expr: Apron.Texpr1.t): Polynomial.t =
       let open Apron in
@@ -132,7 +135,7 @@ module Apron2Koat =
       expr
       |> Apron.Texpr1.to_expr
       |> expr_to_poly
-      
+
     (** Converts an apron atom to its koat equivalent *)
     let atom_from_apron (single_constraint: Apron.Tcons1.t): Constraint.t =
       let open Apron in
@@ -148,19 +151,19 @@ module Apron2Koat =
     (** Converts a koat constraint to its apron equivalent *)
     let constraint_from_apron (constraint_array: Apron.Tcons1.earray): Constraint.t =
       (* If someone still needs a reason to switch to functional programming, here it is. *)
-      let result = ref [] in       
+      let result = ref [] in
       for i=0 to Apron.Tcons1.array_length constraint_array - 1 do
         result := atom_from_apron (Apron.Tcons1.array_get constraint_array i)::!result
       done;
       Constraint.all !result
 
   end
-    
+
 let transform_program program =
 
   let open Koat2Apron in
   let open Apron2Koat in
-  
+
   (** Creates the apron environment where all program variables are integer variables. *)
   let environment: Apron.Environment.t =
     Apron.Environment.make (vars_to_apron (Program.vars program)) [||]
@@ -184,14 +187,14 @@ let transform_program program =
     (* A completely unbound (non-deterministic) choice *)
     let any_value = Apron.(Texpr1.cst environment (Coeff.Interval Interval.top)) in
     (* TODO Perhaps there may be a better way to do this *)
-    let dist_value d = 
+    let dist_value d =
       Apron.(Texpr1.cst environment (Coeff.Interval (Interval.of_scalar
         ((ProbDistribution.lower_det_const d |> Option.map (Scalar.of_int % OurInt.to_int)) |? Scalar.of_infty 1)
         ((ProbDistribution.upper_det_const d |> Option.map (Scalar.of_int % OurInt.to_int)) |? Scalar.of_infty (-1))
       )))
-    in 
-    let update_element_map_fun u = 
-      match u with 
+    in
+    let update_element_map_fun u =
+      match u with
         | TransitionLabel.UpdateElement.Poly p -> poly_to_apron environment p
         | TransitionLabel.UpdateElement.Dist d -> dist_value d
     in
@@ -241,27 +244,47 @@ let transform_program program =
       |> TransitionSet.enum
       |> Stack.of_enum
     in
+    let transition_steps =
+      Program.transitions program
+      |> TransitionSet.enum
+      |> Enum.map (fun t -> Transition.id t,0)
+      |> Hashtbl.of_enum
+    in
+
     while not (Stack.is_empty worklist) do
       let (l,t,l') = Stack.pop worklist in
+      let trans_id = TransitionLabel.id t in
       let transfered_l_abstract = apply_transition (l,t,l') (LocationMap.find program_abstract l) in
-      if not (Apron.Abstract1.is_leq manager transfered_l_abstract (LocationMap.find program_abstract l')) then (
-        LocationMap.modify l' (fun old_abstract -> Apron.Abstract1.widening manager old_abstract transfered_l_abstract) program_abstract;
+      let old_abstract = LocationMap.find program_abstract l' in
+
+      if not (Apron.Abstract1.is_leq manager transfered_l_abstract old_abstract) then (
+        LocationMap.modify l' (fun _ ->
+          if Hashtbl.find transition_steps trans_id < max_steps_without_widening then
+            Apron.Abstract1.join manager old_abstract transfered_l_abstract
+          else
+            Apron.Abstract1.widening manager old_abstract transfered_l_abstract
+        ) program_abstract;
+
+        (* count step *)
+        Hashtbl.modify trans_id (Int.add 1) transition_steps;
+
+        (* add succesor transitons to worklist *)
         TransitionGraph.succ_e (Program.graph program) l'
         |> List.iter (fun transition ->
               (*TODO comparison faster with 2 component transition id*)
                if not (Enum.exists (Transition.equivalent transition) (Stack.enum worklist)) then
                  Stack.push transition worklist
              )
-      )
+      );
     done;
     program_abstract
   in
-  
+
   (** Converts a value of the abstract domain into a koat constraint which acts as an invariant. *)
   let extract_invariant (abstract: 'a Apron.Abstract1.t): Constraint.t =
     abstract
     |> Apron.Abstract1.to_tcons_array manager
-    |> constraint_from_apron 
+    |> constraint_from_apron
   in
 
   (** Determines for each location its invariant constraint and filters locations, which does not have an additional invariant. *)
