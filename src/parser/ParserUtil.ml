ba407e39b458de2695c7368b1975bf07dcc040fe
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2020-01-16 15:06:59 +0100

    added support to parse fractions

--- src/parser/ParserUtil.ml
+++ src/parser/ParserUtil.ml
@@ -96,14 +96,18 @@ let mk_program goal start vars (transitions: Transition.t list): Program.t =
   Program.from transitions start
 
 let ourfloat_of_decimal_string (str: string): OurFloat.t =
-  let str_before_point = String.split str ~by:(".") |> Tuple2.first in
-  let str_after_point = String.split str ~by:(".") |> Tuple2.second in
-  let numerator =
-    if str_after_point = "" then OurFloat.zero else OurFloat.of_string str_after_point
-  in
-  let denominator =
-    OurFloat.pow (OurFloat.of_int 10) (String.length str_after_point)
-  in
-  let fractional = if str_after_point = "" then OurFloat.zero else OurFloat.( numerator/denominator ) in
-  let leading = if str_before_point = "" then OurFloat.zero else OurFloat.of_string str_before_point in
-  OurFloat.(leading + fractional)
+  (* Check if fraction *)
+  if String.contains str '[' then
+    OurFloat.of_string (String.strip ~chars:"[]" str)
+  else
+    let str_before_point = String.split str ~by:(".") |> Tuple2.first in
+    let str_after_point = String.split str ~by:(".") |> Tuple2.second in
+    let numerator =
+      if str_after_point = "" then OurFloat.zero else OurFloat.of_string str_after_point
+    in
+    let denominator =
+      OurFloat.pow (OurFloat.of_int 10) (String.length str_after_point)
+    in
+    let fractional = if str_after_point = "" then OurFloat.zero else OurFloat.( numerator/denominator ) in
+    let leading = if str_before_point = "" then OurFloat.zero else OurFloat.of_string str_before_point in
+    OurFloat.(leading + fractional)
