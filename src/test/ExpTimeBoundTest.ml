79a840c14c6891e91f2b301f6062176f206c9e1c
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2020-03-01 21:52:36 +0100

    ExpTimeboundTest: Preprocess programs for analysis

--- src/test/ExpTimeBoundTest.ml
+++ src/test/ExpTimeBoundTest.ml
@@ -42,11 +42,15 @@ let tests =
           let prog = Readers.read_file (CacheManager.trans_id_counter cache) ("../../" ^ prog_path) in
           let gtset = Program.generalized_transitions prog in
           let gt = GeneralTransitionSet.any @@ GeneralTransitionSet.filter ((=) gt_id % GeneralTransition.id) gtset in
-          let approx = Approximation.create prog |> Bounds.find_exp_bounds false ~generate_invariants_bottom_up:Preprocessor.generate_invariants true cache prog |> Tuple2.second in
+          let (processed_prog,approx) =
+            (prog, Approximation.create prog )
+            |> Preprocessor.process (CacheManager.trans_id_counter cache) Preprocessor.process_til_fixpoint Preprocessor.[InvariantGeneration; CutUnsatisfiableTransitions; CutUnreachableLocations; CutZeroProbTransitions]
+            |> (fun (p,appr) -> Bounds.find_exp_bounds false ~generate_invariants_bottom_up:Preprocessor.generate_invariants true cache p appr)
+          in
           let exptime_compl = Approximation.exptimebound approx gt |> RealBound.asymptotic_complexity in
           let error_str =
               "Mismatch: Expected " ^ (RealBound.show_complexity exp_complexity) ^ " got " ^ (RealBound.show_complexity exptime_compl)
-              ^ " for gt " ^ (GeneralTransition.to_id_string gt) ^ " in prog\n" ^ (Program.to_string ~show_gtcost:true prog)
+              ^ " for gt " ^ (GeneralTransition.to_id_string gt) ^ " in prog\n" ^ (Program.to_string ~show_gtcost:true processed_prog)
           in
           name >:: (fun _ -> assert_bool error_str RealBound.(equal_complexity exp_complexity exptime_compl) )
         )
