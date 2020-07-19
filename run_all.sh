97ba21e623bdb3f3e8d8171dd5c9f368ff85e09b
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2020-07-19 15:31:57 +0200

    run_all: no bottom up

--- run_all.sh
+++ run_all.sh
@@ -3,5 +3,5 @@ cd examples/ProbabilisticExamples/paper
 for i in *.koat; do
   echo \# $i
   # Adapt the timeout as needed
-  timeout 300s ../../../src/main/koat2 analyse -i $i --bottom-up
+  timeout 300s ../../../src/main/koat2 analyse -i $i
 done
