34b5f7038804b29109838ebf0fba43b543b1bc0e
Marcel Hark <marcel.hark@rwth-aachen.de>
2020-07-29 08:06:00 +0200

    Ignoring static binary

--- run_webinterface.sh
+++ run_webinterface.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
-INPUT=${@: -1}
 TIMEOUT=300
 
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
+usage()
+{
+  echo "Usage: run.sh [ -i | --input FILE ] [ -r | --result ]
+                        [ -t | --timeout TIMEOUT ]"
+  exit 2
+}
 
-case $key in
-    -t|--timeout)
-    TIMEOUT="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -r|--result)
-    RESULT="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -i|--input)
-    INPUT="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    *)
-    shift
-    shift
-    ;;
-esac
+PARSED_ARGUMENTS=$(getopt -a -n run.sh -o t:i:r: --long input:,result:,timeout: -- "$@")
+VALID_ARGUMENTS=$?
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+  usage
+fi
+
+eval set -- "$PARSED_ARGUMENTS"
+while :
+do
+  case "$1" in
+    -i | --input)   INPUT="$2" ; shift 2 ;;
+    -r | --result)  RESULT="$2" ; shift 2 ;;
+    -t | --timeout) TIMEOUT="$2" ; shift 2 ;;
+    # -- means the end of the arguments; drop this, and break out of the while loop
+    --) shift; break ;;
+    # If invalid options were passed, then getopt should have reported an error,
+    # which we checked as VALID_ARGUMENTS when getopt was called...
+    *) echo "Unexpected option: $1 - this should not happen."
+       usage ;;
+  esac
 done
-set -- "${POSITIONAL[@]}" # restore positional parameters
 
 echo "INPUT  = ${INPUT}"
 echo "RESULT     = ${RESULT}"
