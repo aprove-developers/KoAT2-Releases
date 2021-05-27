5f51c7774b944bc1a29e92aac53dca1780b15777
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2021-05-27 17:27:00 +0200

    fix LocalSizeBound tests

--- src/test/LocalSizeBoundTest.ml
+++ src/test/LocalSizeBoundTest.ml
@@ -21,27 +21,31 @@ let tests =
                          assert_equal_bound (LocalSizeBound.as_bound expected) (convert_lsb bound)))
                   [
                     (* Bounded by constants *)
-                    (mk [], "x <= 0");
-                    (mk ~c:5 [], "x <= 5");
-                    (mk ~c:3 [], "x <= 7 - 10");
-                    (mk ~c:7 [], "x <= 7 + y - y");
-                    (mk ~c:10 [], "x <= y && y <= 10");
-                    (mk ~c:15 [], "x <= y + 7 && y <= 8");
-                    (mk ~c:10 [], "x <= y && x <= 10");
+                    (mk ~c:5 [], "x <= 0 && x >= -5");
+                    (mk ~c:5 [], "x <= 5 && x >= -2");
+                    (mk ~c:3 [], "x = 7 - 10");
+                    (mk ~c:7 [], "x <= 7 + y - y && x>= 0");
+                    (mk ~c:10 [], "x <= y && y <= 10 && x>=-2");
+                    (mk ~c:15 [], "x <= y + 7 && y <= 8 && x>=0");
+                    (mk ~c:10 [], "x <= y && x <= 10 && x>=0");
+
                     (* Bounded by variable *)
-                    (mk ["y"], "x <= y");
+                    (mk ["y"], "x <= y && x>=-y");
                     (mk ["y"], "x <= y && x >= y");
                     (mk ["y"], "x <= y && x > 5");
                     (* TODO Better heuristic for optimize vars: (AddsConstant 0 ["y"] "x <= y && y <= z"); *)
-                    (mk ["z"], "x <= z && y <= z");
+                    (mk ["z"], "x <= z && y <= z && x>=0");
+
                     (* Bounded by constant plus variable *)
-                    (mk ~c:5 ["y"], "x <= y + 5");
-                    (mk ~c:5 ["y"], "x <= y - 5");
-                    (mk ~c:2 ["y"], "x <= y + z - 5 && z <= 7");
+                    (mk ~c:5 ["y"], "x <= y + 5 && x>=-y -5");
+                    (mk ~c:0 ["y"], "x <= y - 5 && x>=-y+5");
+                    (mk ~c:2 ["y"], "x <= y + z - 5 && z <= 7 && x>=0");
+
                     (* With factor and positive coefficients *)
-                    (mk ~s:2 ["y"], "x <= 2*y");
-                    (mk ~s:2 ["y"; "z"], "x <= 2*y + 2*z");
-                    (mk ~s:3 ["y";"z"], "x <= 2*y + 3*z");
+                    (mk ~s:2 ["y"], "x <= 2*y && x>=0");
+                    (mk ~s:2 ["y"], "x <= 2*y && x>=-2*y");
+                    (mk ~s:2 ["y"; "z"], "x <= 2*y + 2*z && x> -2*y - 2*z");
+                    (mk ~s:3 ["y";"z"], "x <= 2*y + 3*z && x>= -2*y - 3*z");
                   ]
       );
     ]
