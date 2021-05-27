d303059f871eddc3ee723acf4bdbd80de998d8dd
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2021-05-27 09:58:34 +0200

    adapt SizeBounds

--- src/bounds/SizeBounds.ml
+++ src/bounds/SizeBounds.ml
@@ -16,25 +16,23 @@ let improve_scc program rvg appr = function
 let improve program ?(scc = None) applied_cfr appr =
   let execute () =
     let module C = Graph.Components.Make(RVG) in
-    [`Lower; `Upper]
-    |> List.fold_left (fun appr ->
-           let rvg = RVG.rvg program in
-           List.fold_left (fun appr rvg_scc ->
-              let current_time = Unix.gettimeofday() in
-              improve_scc program rvg appr rvg_scc
-              |> tap (fun _ ->
-                if applied_cfr then (
-                  CFR.delta_current_cfr := !CFR.delta_current_cfr +. (Unix.gettimeofday() -. current_time);
-                  CFR.poll_timeout ~applied_cfr:applied_cfr))
-            ) appr (
-            if not (Option.is_some scc) then
-             (List.rev (C.scc_list rvg))
-            else (
-             List.rev (List.filter (fun rvg_scc ->
-                (List.exists (fun (t,x) -> TransitionSet.mem t (Option.get scc)) rvg_scc)
-               || ((List.length rvg_scc) == 1)) (C.scc_list rvg))
-             )
-    )) appr
+    let rvg = RVG.rvg program in
+
+    List.fold_left (fun appr rvg_scc ->
+       let current_time = Unix.gettimeofday() in
+       improve_scc program rvg appr rvg_scc
+       |> tap (fun _ ->
+         if applied_cfr then (
+           CFR.delta_current_cfr := !CFR.delta_current_cfr +. (Unix.gettimeofday() -. current_time);
+           CFR.poll_timeout ~applied_cfr:applied_cfr))
+     ) appr (
+      if not (Option.is_some scc) then
+        (List.rev (C.scc_list rvg))
+      else (
+      List.rev (List.filter (fun rvg_scc -> (List.exists (fun (t,x) -> TransitionSet.mem t (Option.get scc)) rvg_scc)
+                                            || ((List.length rvg_scc) == 1)) (C.scc_list rvg))
+      )
+    )
   in Logger.with_log logger Logger.INFO
                   (fun () -> "improve_size_bounds", [])
                   execute
