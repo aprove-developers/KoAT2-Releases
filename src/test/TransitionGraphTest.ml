08553dcf0793105bcebc3abcce6ac242f733c3c7
mhark <marcel.hark@cs.rwth-aachen.de>
2021-05-27 10:48:18 +0200

    Adapted local_size_bound test

--- src/test/TransitionGraphTest.ml
+++ src/test/TransitionGraphTest.ml
@@ -12,7 +12,7 @@ let suite =
           ("examples_old_input/" ^ folder ^ "/") >::: (
             let files = Array.filter (fun s -> String.ends_with s ".koat") (Sys.readdir ("../../examples/" ^ folder))
             and test (file : string): unit = try ignore (Readers.read_file ("../../examples/" ^ folder ^ "/" ^ file)) with
-                                             | Readers.Error msg -> failwith msg
+                                             | ParserUtil.Error msg -> failwith msg
                                              | TransitionLabel.RecursionNotSupported -> skip_if true "Recursion not supported" in
             Array.to_list (Array.map (fun s -> (s >:: (fun _ -> test s))) files)) in
         "Examples" >::: List.map test_folder ["KoAT-2013"; "KoAT-2014"; "SAS10"; "T2"]
@@ -22,7 +22,7 @@ let suite =
           ("examples_new_input/" ^ folder ^ "/") >::: (
             let files = Array.filter (fun s -> String.ends_with s ".koat") (Sys.readdir ("../../examples/" ^ folder))
             and test (file : string): unit = try ignore (Readers.read_file ("../../examples/" ^ folder ^ "/" ^ file)) with
-                                             | Readers.Error msg -> failwith msg
+                                             | ParserUtil.Error msg -> failwith msg
                                              | TransitionLabel.RecursionNotSupported -> skip_if true "Recursion not supported" in
             Array.to_list (Array.map (fun s -> (s >:: (fun _ -> test s))) files)) in
         "Examples" >::: List.map test_folder ["CageKoAT-Input-Examples/weightedExamples/badExamples" ;"CageKoAT-Input-Examples/weightedExamples/constantWeights"; "CageKoAT-Input-Examples/weightedExamples/simplePolyWeights"; "CageKoAT-Input-Examples/debugExamples"; "CageKoAT-Input-Examples/cexamples"]
@@ -46,7 +46,7 @@ let suite =
                        let t = TransitionGraph.find_edge (Program.graph program) (Location.of_string l) (Location.of_string l') in
                        TransitionLabel.(assert_equal_bound
                                                   (Bound.of_poly (Readers.read_polynomial bound))
-                                                  LocalSizeBound.(sizebound_local program `Upper t (Var.of_string var) |> Option.map as_bound |? default `Upper)
+                                                  LocalSizeBound.(sizebound_local program t (Var.of_string var) |> option_lsb_as_bound)
                        )
                      )
              )
