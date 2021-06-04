827ecc9546afea02a5dd348153ba069caba616cf
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2021-06-04 10:11:20 +0200

    Cache RVGs to improve performance

--- src/main/SizeCommand.ml
+++ src/main/SizeCommand.ml
@@ -16,7 +16,7 @@ let run (params: params) =
   Logging.(use_loggers [Size, Logger.DEBUG]);
   let appr = Approximation.empty 10 3
   and program = Readers.read_file params.program in
-  SizeBounds.improve program false appr
+  SizeBounds.improve program (RVGTypes.RVG.rvg program) false appr
   |> Approximation.to_string program
   |> print_string
 
