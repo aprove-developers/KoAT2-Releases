aprove-developers/KoAT2-Releases

commit 1ec0386b193b55dffa92abb353568a0aa6ed420a

TrivialSizeBounds: Substitute with pre-transitions in RVG
This also avoids the costly call to Program.pre, resulting in a speed-up
of the analysis.

src/bounds/TrivialSizeBounds.ml
@@ -4,6 +4,8 @@ open BoundsInst
 open ProgramTypes
 open RVGTypes
 
+module TransitionTable = RankingFunction.TransitionTable
+
 (** Modules used to infer size-bounds for trivial components. That is an scc which consists only of one result variable without a loop to itself.
     Corresponds to 'SizeBounds for trivial SCCs'.*)
 
@@ -13,11 +15,16 @@ let logger = Logging.(get Size)
 (** Returns the maximum of all incoming sizebounds applied to the local sizebound.
     Corresponds to 'SizeBounds for trivial SCCs':
     S'(alpha) = max(S_l(alpha)(S(t',v_1),...,S(t',v_n)) for all t' in pre(t)). *)
-let incoming_bound program get_sizebound lsb t =
+let incoming_bound rvg get_sizebound lsb t v =
+  (* since this is a trivial scc*)
   let execute () =
     let substitute_with_prevalues t' = LocalSizeBound.as_substituted_bound (fun v -> get_sizebound t' v) lsb in
-    t
-    |> Program.pre program
+    let pre_transitions =
+      RVG.pre rvg (t,v)
+      |> Enum.map RV.transition
+      |> Enum.uniq_by Transition.same
+    in
+    pre_transitions
     |> Enum.map substitute_with_prevalues
     |> Bound.sum
   in Logger.with_log logger Logger.DEBUG
@@ -28,7 +35,7 @@ let incoming_bound program get_sizebound lsb t =
 
 (** Computes a bound for a trivial scc. That is an scc which consists only of one result variable without a loop to itself.
     Corresponds to 'SizeBounds for trivial SCCs'. *)
-let compute program get_sizebound (t,v) =
+let compute program rvg get_sizebound (t,v) =
   let execute () =
     let (lsb: LocalSizeBound.t Option.t) =
       LocalSizeBound.sizebound_local_rv program (t, v)
@@ -40,7 +47,7 @@ let compute program get_sizebound (t,v) =
     else
       LocalSizeBound.(
       lsb
-      |> Option.map (fun lsb -> incoming_bound program get_sizebound lsb t)
+      |> Option.map (fun lsb -> incoming_bound rvg get_sizebound lsb t v)
       |? Bound.infinity)
 
   in Logger.with_log logger Logger.DEBUG

src/bounds/SizeBounds.ml
@@ -7,7 +7,7 @@ let logger = Logging.(get Size)
 
 let improve_scc program rvg appr = function
   | [((l,t,l'),v)] when not (RVG.mem_edge rvg ((l,t,l'),v) ((l,t,l'),v)) ->
-     let new_bound = TrivialSizeBounds.compute program (Approximation.sizebound appr) ((l,t,l'),v) in
+     let new_bound = TrivialSizeBounds.compute program rvg (Approximation.sizebound appr) ((l,t,l'),v) in
      Approximation.add_sizebound new_bound (l,t,l') v appr
   | scc ->
      let new_bound = NontrivialSizeBounds.compute program rvg (Approximation.timebound appr) (Approximation.sizebound appr) scc in

commit c0d6706a62a286598ac13d5ef9a969ddd4d2f66c

Fixed a critical bug in TrivialSizeBounds.
For constant updates, i.e. x -> 1 there is no predecessor in the
RVG. Hence, a trivial size bound of 0 instead of 1 is computed.

This bug was introduced in 1ec0386b193b55dffa92abb353568a0aa6ed420a .

src/bounds/TrivialSizeBounds.ml
@@ -16,15 +16,19 @@ let logger = Logging.(get Size)
 let incoming_bound rvg get_sizebound lsb t v =
   (* since this is a trivial scc*)
   let execute () =
-    let substitute_with_prevalues t' = LocalSizeBound.as_substituted_bound (fun v -> get_sizebound t' v) lsb in
-    let pre_transitions =
-      RVG.pre rvg (t,v)
-      |> Enum.map RV.transition
-      |> Enum.uniq_by Transition.same
-    in
-    pre_transitions
-    |> Enum.map substitute_with_prevalues
-    |> Bound.sum
+    (* If the LSB is constant there are no pre-transitions in the RVG *)
+    if LocalSizeBound.is_constant lsb then
+      LocalSizeBound.as_bound lsb
+    else
+      let substitute_with_prevalues t' = LocalSizeBound.as_substituted_bound (fun v -> get_sizebound t' v) lsb in
+      let pre_transitions =
+        RVG.pre rvg (t,v)
+        |> Enum.map RV.transition
+        |> Enum.uniq_by Transition.same
+      in
+      pre_transitions
+      |> Enum.map substitute_with_prevalues
+      |> Bound.sum
   in Logger.with_log logger Logger.DEBUG
                      (fun () -> "compute_highest_incoming_bound", ["lsb", (Bound.to_string % LocalSizeBound.as_bound) lsb;
                                                                    "transition", Transition.to_id_string t])

src/transitionsystem/LocalSizeBound.ml
@@ -60,6 +60,8 @@ let constant t = t.constant
 
 let vars t = t.vars
 
+let is_constant = VarSet.is_empty % vars
+
 let to_string lsb =
   "{" ^
   "factor: " ^ Int.to_string lsb.factor ^ "; " ^

src/transitionsystem/LocalSizeBound.mli
@@ -24,6 +24,9 @@ val constant : t -> int
 (** Returns a set of of variables which affect the local sizebound *)
 val vars : t -> VarSet.t
 
+(** Is the LocalSizeBound constant, i.e. does it contain no variables? *)
+val is_constant : t -> bool
+
 (** Converts the templated bound to a string. *)
 val to_string : t -> string
 