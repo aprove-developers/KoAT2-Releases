35ad168ecf1737edd01433cc38b8ec91741886d2
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2021-06-16 20:48:33 +0200

    Improve CutUnsatisfiableTransitions

--- src/preprocessors/CutUnsatisfiableTransitions.ml
+++ src/preprocessors/CutUnsatisfiableTransitions.ml
@@ -12,16 +12,24 @@ open ProgramTypes
 let logger = Logging.(get Preprocessor)
 
 (** Returns a set of transistions which have a conflicting guard (e.g. 0 > 0). *)
-let unsatisfiable_transitions graph : TransitionSet.t =
+let unsatisfiable_transitions program graph : TransitionSet.t =
   let combine (l,t,l') set =
-    if SMT.Z3Opt.unsatisfiable (Formula.mk (TransitionLabel.guard t)) then
-      TransitionSet.add (l,t,l') set
-    else set in
+    if Program.is_initial_location program l then
+      if SMT.Z3Opt.unsatisfiable (Formula.mk (TransitionLabel.guard t)) then
+        TransitionSet.add (l,t,l') set
+      else set
+    else
+      (* There needs to be a transition distinct from (l,t,l') to enter (l,t,l') *)
+      let intrans = TransitionSet.remove (l,t,l') % TransitionSet.of_enum @@ Program.pre program (l,t,l') in
+      if TransitionSet.is_empty intrans then
+        TransitionSet.add (l,t,l') set
+      else set
+  in
   TransitionGraph.fold_edges_e combine graph TransitionSet.empty
 
 (** Returns program without unsatisfiable transitions. *)
 let transform_program program =
-  let unsatisfiable_transitions = unsatisfiable_transitions (Program.graph program) in
+  let unsatisfiable_transitions = unsatisfiable_transitions program (Program.graph program) in
   if TransitionSet.is_empty unsatisfiable_transitions then
     MaybeChanged.same program
   else
