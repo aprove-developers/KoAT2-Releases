e5cd4bc92ccaecb0f63ad49e62d4370fca2252f7
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2021-06-18 13:11:20 +0200

    Remove dead code

--- src/transitionsystem/RVGTypes.mli
+++ src/transitionsystem/RVGTypes.mli
@@ -51,13 +51,6 @@ sig
   (** Returns the predecessors of a result variable in the result variable graph. *)
   val pre : t -> RV.t -> RV.t Enum.t
 
-  (** Returns all the entry points of the SCC.
-      Those are all result variables that are in the RVG, but not in the SCC and lead to any result variable in the RVG. *)
-  val entry_points : t -> RV.t list -> RV.t Enum.t
-
-  (** Returns all transitions that are used in the SCC of the RVG. *)
-  val transitions : RV.t list -> Transition.t Enum.t
-
   (** TODO doc *)
   val rvg : Program.t -> t
 
