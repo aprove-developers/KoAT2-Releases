ad4e286dfe786b3f1fb1873814dea6be1da1cd76
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2021-07-30 12:55:17 +0200

    Clean up MPRF code for nested MPRFs

--- src/bounds/MPRF_Coefficient.mli
+++ src/bounds/MPRF_Coefficient.mli
@@ -3,8 +3,5 @@ open BoundsInst
 (** Computes coefficients for Multiphase Ranking Functions necessary for upper time-bounds. *)
 (** Handles multiphase ranking functions (see [Ben-Amram and Genaim 2017]) and computes coefficients for upper time-bounds. *)
 
-val sumBound_of_list : Bound.t list -> Bound.t
-(** Returns for a list of Bounds b1,...,bn the sum of bounds b1 + ... + bn. *)
-
 val coefficient : MultiphaseRankingFunction.t -> int
 (** Returns maximal coefficient for a Multiphase Ranking Function as in [Ben-Amram and Genaim 2017]. *)
