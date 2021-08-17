a31275912e721ffba50bf0650c29de6301c0bbc0
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2021-08-17 14:35:43 +0200

    CFR: Use complete SCC for refinement

--- src/bounds/CFR.mli
+++ src/bounds/CFR.mli
@@ -13,4 +13,4 @@ val compute_timeout_time : Program.t -> Approximation.t -> TransitionSet.t -> fl
 val uid : string Batteries.ref
 
 (** Unrolls all transitions listed in nonLinearTransitions.starrt  *)
-val apply_cfr :  ProgramTypes.TransitionSet.t -> TransitionSet.t -> Program.t -> Approximation.t -> (Program.t * Approximation.t * TransitionSet.t) option
+val apply_cfr :  TransitionSet.t -> TransitionSet.t -> TransitionSet.t -> Program.t -> Approximation.t -> (Program.t * Approximation.t * TransitionSet.t) option
