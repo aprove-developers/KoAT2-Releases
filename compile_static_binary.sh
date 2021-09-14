ac183717389303ce554d730a49313bd5e54960ff
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2021-09-14 16:38:32 +0200

    Image: Only support docker

--- compile_static_binary.sh
+++ compile_static_binary.sh
@@ -2,12 +2,6 @@
 #To be executed from the top directory of the repository
 #The static binary is compiled in a Dockerfile and then copied into the main file
 ./build_container.sh
-if  command -v podman &> /dev/null; then
-  podman create -ti --name koat2_dummy koat2 bash
-  podman cp koat2_dummy:/home/koat2/bin/koat2 ./src/main/koat2_static
-  podman rm -f koat2_dummy
-else
-  docker create -ti --name koat2_dummy koat2 bash
-  docker cp koat2_dummy:/home/koat2/bin/koat2 ./src/main/koat2_static
-  docker rm -f koat2_dummy
-fi
+docker create -ti --name koat2_dummy koat2 bash
+docker cp koat2_dummy:/home/koat2/bin/koat2 ./src/main/koat2_static
+docker rm -f koat2_dummy
