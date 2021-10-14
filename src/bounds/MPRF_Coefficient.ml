b625f38026e1217031aeef32b048920d8d2370f0
nils-lommen <nils.lommen@rwth-aachen.de>
2021-10-14 13:06:46 +0200

    adjust floating point -> int conversion in MPRF_Coeff

--- src/bounds/MPRF_Coefficient.ml
+++ src/bounds/MPRF_Coefficient.ml
@@ -10,7 +10,7 @@ let rec fac = function
 
 let rec gamma = function
   | 1 -> 1.0
-  | i -> 2.0 +. (gamma (i - 1)) /. ((float_of_int i) -. 1.0) +. 1.0 /. (float_of_int (fac (i - 1)))
+  | i -> 2.0 +. (gamma (i - 1)) /. (float_of_int (i - 1)) +. 1.0 /. (float_of_int (fac (i - 1)))
 
 let coefficient (rank: MultiphaseRankingFunction.t) =
   let depth = MultiphaseRankingFunction.depth rank in
