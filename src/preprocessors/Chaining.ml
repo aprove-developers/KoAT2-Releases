f43e640005a68c633017ef21f46cb6db0fc538b1
nils-lommen <nils.lommen@rwth-aachen.de>
2021-11-11 11:44:03 +0100

    reuse chaining

--- src/preprocessors/Chaining.ml
+++ src/preprocessors/Chaining.ml
@@ -38,11 +38,11 @@ let chain location graph : TransitionGraph.t =
   |> Enum.fold TransitionGraph.remove_edge_e skipped
 
 (** Performs chaining on the TransitionGraph. *)
-let transform_graph (graph: TransitionGraph.t): TransitionGraph.t MaybeChanged.t =
+let transform_graph ?(scc=TransitionSet.empty) (graph: TransitionGraph.t): TransitionGraph.t MaybeChanged.t =
   let try_chaining location maybe_changed_graph =
     let open MaybeChanged in
     maybe_changed_graph >>= (fun graph ->
-      if chainable graph location then (
+      if LocationSet.mem location (TransitionSet.locations scc) && chainable graph location then (
         Logger.(log logger INFO (fun () -> "chaining", ["location", Location.to_string location]));
         changed (chain location graph)
       ) else
