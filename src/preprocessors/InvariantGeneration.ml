8da68ac15bde91bf3708f928f03c8539db8ace15
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2021-11-30 14:59:33 +0100

    Better Invariants: Narrowing & Delay Widening

--- src/preprocessors/InvariantGeneration.ml
+++ src/preprocessors/InvariantGeneration.ml
@@ -14,6 +14,10 @@ let logger = Logging.(get Preprocessor)
 
 (** A map from program locations to anything. *)
 module LocationMap = Hashtbl.Make(Location)
+module TransitionMap = Hashtbl.Make(Transition)
+
+(* The number of steps per transition to be performed during fixpoint iteration without widening*)
+let max_steps_without_widening = 5
 
 (** An abstract value for all values of variable at every program location. *)
 type 'a program_abstract = ('a Apron.Abstract1.t) LocationMap.t
@@ -163,6 +167,14 @@ let transform_program program =
   let open Koat2Apron in
   let open Apron2Koat in
 
+  let locations =
+    Program.graph program
+    |> TransitionGraph.locations
+  in
+
+  let transitions = Program.transitions program in
+  let vars = Program.vars program in
+
   (** Creates the apron environment where all program variables are integer variables. *)
   let environment: Apron.Environment.t =
     Apron.Environment.make (vars_to_apron (Program.vars program)) [||]
@@ -182,7 +194,7 @@ let transform_program program =
   in
 
   (** Applies the update to the abstract value. *)
-  let apply_update (vars: VarSet.t) (update: Var.t -> Polynomial.t Option.t) (abstract: 'a Apron.Abstract1.t): 'a Apron.Abstract1.t =
+  let apply_update (update: Var.t -> Polynomial.t Option.t) (abstract: 'a Apron.Abstract1.t): 'a Apron.Abstract1.t =
     (* A completely unbound (non-deterministic) choice *)
     let any_value = Apron.(Texpr1.cst environment (Coeff.Interval Interval.top)) in
     let assignments =
@@ -192,6 +204,11 @@ let transform_program program =
       |> Array.map (Option.map_default (poly_to_apron environment) any_value)
     in
     Apron.Abstract1.assign_texpr_array manager abstract (vars_to_apron vars) assignments None
+    (* This is somehow necessary otherwise apron gets confused over variables with the same name (i.e. the same variables).  *)
+    |> Apron.Abstract1.to_tcons_array manager
+    |> constraint_from_apron
+    |> constraint_to_apron environment
+    |> Apron.Abstract1.of_tcons_array manager environment
   in
 
   (** Applies the transition to the abstract value.
@@ -199,7 +216,7 @@ let transform_program program =
   let apply_transition ((l,t,l'): Transition.t) (abstract: 'a Apron.Abstract1.t): 'a Apron.Abstract1.t =
     abstract
     |> apply_guard (TransitionLabel.guard t)
-    |> apply_update (Program.vars program) (TransitionLabel.update t)
+    |> apply_update (TransitionLabel.update t)
   in
 
   (** The bottom element of the static analysis for the whole program.
@@ -207,13 +224,11 @@ let transform_program program =
       All other values at other program locations are undefined in the beginning.
       This will change through assignments in the program. *)
   let bottom: 'a program_abstract =
-    program
-    |> Program.graph
-    |> TransitionGraph.locations
+    locations
     |> LocationSet.enum
     |> Enum.map (fun location ->
            if Program.is_initial_location program location then
-             (location, Apron.Abstract1.top manager environment)
+              (location, Apron.Abstract1.top manager environment)
            else
              (location, Apron.Abstract1.bottom manager environment)
          )
@@ -231,17 +246,49 @@ let transform_program program =
       |> TransitionSet.enum
       |> Stack.of_enum
     in
+    let transition_steps =
+      TransitionMap.create (TransitionSet.cardinal transitions)
+      |> tap (fun m -> TransitionSet.iter (fun t -> TransitionMap.add m t 0) transitions)
+    in
+
+    let narrowing_abstract =
+      let location_abstract l =
+        TransitionSet.filter (fun (_,_,l') -> Location.equal l l') transitions
+        |> TransitionSet.enum
+        |> Enum.map (fun t -> apply_transition t (Apron.Abstract1.top manager environment))
+        |> Enum.fold (Apron.Abstract1.join manager) (Apron.Abstract1.bottom manager environment)
+      in
+      LocationSet.enum locations
+      (* The initial location does not have any ingoing transitions *)
+      |> Enum.filter (not % Program.is_initial_location program)
+      |> Enum.map (fun location -> (location, location_abstract location))
+      |> LocationMap.of_enum
+    in
+
     while not (Stack.is_empty worklist) do
       let (l,t,l') = Stack.pop worklist in
       let transfered_l_abstract = apply_transition (l,t,l') (LocationMap.find program_abstract l) in
-      if not (Apron.Abstract1.is_leq manager transfered_l_abstract (LocationMap.find program_abstract l')) then (
-        LocationMap.modify l' (fun old_abstract -> Apron.Abstract1.widening manager old_abstract transfered_l_abstract) program_abstract;
+      let old_abstract = LocationMap.find program_abstract l' in
+
+      if not (Apron.Abstract1.is_leq manager transfered_l_abstract old_abstract) then (
+        LocationMap.modify l' (fun _ ->
+          if TransitionMap.find transition_steps (l,t,l') < max_steps_without_widening then (
+            TransitionMap.modify (l,t,l') (Int.add 1) transition_steps;
+            Apron.Abstract1.join manager old_abstract transfered_l_abstract
+          ) else
+            Apron.Abstract1.meet manager
+              (Apron.Abstract1.widening manager old_abstract transfered_l_abstract)
+              (LocationMap.find narrowing_abstract l')
+        ) program_abstract;
+
+        (* add succesor transitons to worklist *)
         TransitionGraph.succ_e (Program.graph program) l'
         |> List.iter (fun transition ->
-               if not (Enum.exists (Transition.same transition) (Stack.enum worklist)) then
+              (*TODO comparison faster with 2 component transition id*)
+               if not (Enum.exists (Transition.equivalent transition) (Stack.enum worklist)) then
                  Stack.push transition worklist
              )
-      )
+      );
     done;
     program_abstract
   in
