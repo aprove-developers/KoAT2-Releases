32f06bf6637ea22c965cd7c9a03aac1ed2579edd
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2021-12-23 17:38:14 +0100

    Fix bugs introduced in the last commit

--- src/bounds/TrivialSizeBounds.ml
+++ src/bounds/TrivialSizeBounds.ml
@@ -13,7 +13,7 @@ let logger = Logging.(get Size)
 (** Returns the maximum of all incoming sizebounds applied to the local sizebound.
     Corresponds to 'SizeBounds for trivial SCCs':
     S'(alpha) = max(S_l(alpha)(S(t',v_1),...,S(t',v_n)) for all t' in pre(t)). *)
-let incoming_bound rvg get_sizebound lsb t v =
+let incoming_bound pre_transitions get_sizebound lsb t v =
   (* since this is a trivial scc*)
   let execute () =
     (* If the LSB is constant there are no pre-transitions in the RVG *)
@@ -21,11 +21,6 @@ let incoming_bound rvg get_sizebound lsb t v =
       lsb
     else
       let substitute_with_prevalues t' = Bound.substitute_f (fun v -> get_sizebound t' v) lsb in
-      let pre_transitions =
-        RVG.pre rvg (t,v)
-        |> Enum.map RV.transition
-        |> Enum.uniq_by Transition.same
-      in
       pre_transitions
       |> Enum.map substitute_with_prevalues
       |> Bound.sum
@@ -34,6 +29,18 @@ let incoming_bound rvg get_sizebound lsb t v =
                                                                    "transition", Transition.to_id_string t])
                   ~result:Bound.to_string
                   execute
+
+let incoming_bound_lsb rvg get_sizebound lsb t v =
+  let pre_transitions =
+    RVG.pre rvg (t,v)
+    |> Enum.map RV.transition
+    |> Enum.uniq_by Transition.same
+  in
+  incoming_bound pre_transitions get_sizebound lsb t v
+
+let incoming_bound_lifted_update program get_sizebound upd t v =
+  let pre_transitions = Program.pre program t in
+  incoming_bound pre_transitions get_sizebound (Bound.of_poly upd) t v
 
 (** Computes a bound for a trivial scc. That is an scc which consists only of one result variable without a loop to itself.
     Corresponds to 'SizeBounds for trivial SCCs'. *)
@@ -55,13 +62,13 @@ let compute program rvg get_sizebound (t,v) =
               | None   -> Bound.infinity
     else
       match lsb with
-        | Some b -> incoming_bound rvg get_sizebound (LocalSizeBound.as_bound b) t v
+        | Some b -> incoming_bound_lsb rvg get_sizebound (LocalSizeBound.as_bound b) t v
         | None ->
             let tlabel = Transition.label t in
             match TransitionLabel.update tlabel v with
               | Some u ->
                   if VarSet.subset (Polynomials.Polynomial.vars u) (TransitionLabel.input_vars tlabel) then
-                    Bound.of_poly u
+                    incoming_bound_lifted_update program get_sizebound u t v
                   else Bound.infinity
               | None   -> Bound.infinity
   in Logger.with_log logger Logger.DEBUG
