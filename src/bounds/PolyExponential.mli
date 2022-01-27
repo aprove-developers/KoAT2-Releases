90c4d9c51af5b99c475706479ed511c82bca92f3
Nils Lommen <nils.lommen@rwth-aachen.de>
2022-01-27 20:19:18 +0100

    add guards to kernel

--- src/bounds/PolyExponential.mli
+++ src/bounds/PolyExponential.mli
@@ -37,6 +37,6 @@ sig
 
     val compute_closed_form : (Var.t * Polynomial.t) list -> t list
 
-    val monotonic_kernel : Formula.t -> t -> t * ((int * int) * (int * int)) list
+    val monotonic_kernel : Formula.t -> Formula.t -> t -> t * ((int * int) * (int * int)) list
 
 end
