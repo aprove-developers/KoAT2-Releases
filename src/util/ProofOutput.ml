9278df7c533fc6d139e1ba6620b4558b1175f196
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2022-06-23 13:36:54 +0200

    fix compilation with ocaml < 4.14.0

--- src/util/ProofOutput.ml
+++ src/util/ProofOutput.ml
@@ -17,11 +17,11 @@ let get_format () = !format
 
 let add_to_proof str_computation =
   if !proof_enabled then
-    proof :=  Lazy.map (fun p -> p<>str_computation ()) !proof (* fun () -> !proof ()<>str_computation () *)
+    proof :=  let p = !proof in lazy(Lazy.force p<>str_computation ())
 
 let add_to_proof_with_format str_computation =
   if !proof_enabled then
-    proof := Lazy.map (fun p -> p<>str_computation !format) !proof (* fun () -> !proof ()<>str_computation () *)
+    proof := let p = !proof in lazy(Lazy.force p<>str_computation !format)
 
 let add_str_paragraph_to_proof f =
   add_to_proof @@ (FormattedString.mk_paragraph % FormattedString.mk_str_line % f )
