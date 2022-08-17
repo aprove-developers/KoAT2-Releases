f63489aa5819f62f224eddc1ca0f244a1dd93e30
Georg-Albrecht <georg.albrecht@rwth-aachen.de>
2022-08-18 01:36:06 +0200

    removed LaCaml code, updating guard

--- src/bounds/JordanNormalForm.py
+++ src/bounds/JordanNormalForm.py
@@ -2,6 +2,8 @@ from sympy import Matrix
 from sympy import lcm_list
 from sympy import sqrt
 from sympy import fraction
+from sympy import sympify
+
 
 def get_denominator(n):  # returns the quotient of a fraction
     return fraction(n)[1]
@@ -17,21 +19,18 @@ def normalize(m: Matrix) -> Matrix:  # turns a rational matrix into an integer m
     res = factor * m
     return res
 
+
 def matrix_is_func(m: Matrix, func) -> bool:
     list_is_func = lambda x: all(func(i) for i in x)
-    llist_is_func = lambda x: all(list_is_func(i) for i in x)
-    return llist_is_func(m.tolist())
+    return all(list_is_func(i) for i in m.tolist())
 
 
 def matrix_is_rational(m: Matrix) -> bool:
-    # https://stackoverflow.com/questions/59530438/how-to-check-whether-a-symbolic-expression-is-rational
-    rational = lambda x: all(i.exp.is_Integer for i in x.atoms(Pow))
-    return matrix_is_func(m, rational)
+    return matrix_is_func(m, lambda x: sympify(x).is_rational)
 
 
-def matrix_is_integer(m:Matrix) -> bool:
-    integer =  lambda x : get_denominator(x) == 1
-    return matrix_is_func(m, integer)
+def matrix_is_integer(m: Matrix) -> bool:
+    return matrix_is_func(m, lambda x: sympify(x).is_integer)
 
 
 def jordan_normal_form(matrix_as_list):
