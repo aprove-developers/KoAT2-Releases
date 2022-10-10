f7bd61c65f9de4f92f670a0dfef8b0fb43702e01
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2022-10-10 18:10:38 +0200

    fix CI

--- build_container.sh
+++ build_container.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build -t koat2 --build-arg KOAT2_VERSION_STRING="$(git rev-parse --short HEAD) from $(git log -1 --format=%cs)" .
+docker build -t koat2 $@ --build-arg KOAT2_VERSION_STRING="$(git rev-parse --short HEAD) from $(git log -1 --format=%cs)" .
 
