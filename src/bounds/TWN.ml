ceabca549f232cfae4197252aaeada2f03e6202e
Nils Lommen <nils.lommen@rwth-aachen.de>
2022-10-13 14:19:03 +0200

    slightly refactor twn-termination

--- src/bounds/TWN.ml
+++ src/bounds/TWN.ml
@@ -170,12 +170,14 @@ let red_le poly_list =
 
 module Valuation = Valuation.Make(OurInt)
 
-let termination_ twn order npe varmap =
+let check_update_invariant twn_loop atom =
+  let poly = Atom.poly atom in
+  let poly_updated = Polynomial.substitute_f (TWNLoop.update_full twn_loop) poly in
+  let atom_updated = Atom.mk_lt poly_updated Polynomial.zero in
+  SMTSolver.tautology Formula.(implies (mk [atom]) (mk [atom_updated]))
 
-  let update = fun v -> match TWNLoop.update twn v with
-    | Some p -> p
-    | None -> Polynomial.of_var v in
-  let self_impl, rest = TWNLoop.invariant twn |> List.partition (fun a -> let f = Constraint.lift a |> Formula.mk in SMTSolver.tautology (Formula.implies f (Formula.map_polynomial (Polynomial.substitute_f update) f))) in
+let termination_ twn order npe varmap =
+  let self_impl, rest = TWNLoop.invariant twn |> List.partition @@ check_update_invariant twn in
   let formula =
     Formula.any (
     List.map (fun constr ->
@@ -188,9 +190,7 @@ let termination_ twn order npe varmap =
               (constr |> List.unique ~eq:Atom.equal) (Formula.mk_true)) ((TWNLoop.guard_without_inv twn |> Formula.mk_and (Formula.mk rest) |> Formula.constraints))) in
   let model = SMTSolver.get_model (Formula.mk_and (Formula.mk self_impl) formula |> Formula.simplify) in
   (Option.is_none model)
- |> tap (fun bool ->  (* Logger.log logger Logger.INFO (fun () -> "termination", ["is_satisfiable", Bool.to_string (not bool)]);
-                      Logger.log logger Logger.DEBUG (fun () -> "termination", ["formula", Formula.to_string formula]);
-                      if Option.is_some model then Logger.log logger Logger.DEBUG (fun () -> "termination", ["model", (Option.get model |> Valuation.to_string)]); *)
+ |> tap (fun bool ->
     proof_append
         FormattedString.(
         mk_str_line ("Termination: " ^ (string_of_bool bool))
@@ -318,16 +318,16 @@ let complexity loop =
         chain loop |> tap (fun loop -> Logger.log logger Logger.INFO (fun () -> "negative", ["chained", TWNLoop.to_string loop])), true
       else loop, false in
     Logger.log logger Logger.INFO (fun () -> "order", ["order", Util.enum_to_string Var.to_string (List.enum order)]);
-    (* proof_append (FormattedString.mk_str_line ("  order: " ^ (Util.enum_to_string (Var.to_string ~pretty:true) (List.enum order)))); *)
+    proof_append (FormattedString.mk_str_line ("  order: " ^ (Util.enum_to_string (Var.to_string ~pretty:true) (List.enum order))));
     let pe = PE.compute_closed_form (List.map (fun var ->
         let update_var = TWNLoop.update t_ var in
         (var, if Option.is_some update_var then Option.get update_var else Polynomial.of_var var)) order) in
         Logger.log logger Logger.INFO (fun () -> "closed-form", (List.combine (List.map Var.to_string order) (List.map PE.to_string pe)));
-        (* proof_append (
+        proof_append (
           FormattedString.(mk_str "closed-form:" <> (
           (List.combine (List.map (Var.to_string ~pretty:true) order) (List.map PE.to_string_pretty pe))
           |> List.map (fun (a,b) -> a ^ ": " ^ b)
-          |> List.map (FormattedString.mk_str_line) |> FormattedString.mappend |> FormattedString.mk_block))); *)
+          |> List.map (FormattedString.mk_str_line) |> FormattedString.mappend |> FormattedString.mk_block)));
     let npe = PE.normalize pe in
         Logger.log logger Logger.INFO (fun () -> "constrained-free closed-form", (List.combine (List.map Var.to_string order) (List.map PE.to_string npe)));
     let varmap = Hashtbl.of_list (List.combine order npe) in
@@ -553,12 +553,6 @@ let lift_option (xs:'a option list) : 'a list option =
     None
 
 exception No_Cycle
-
-let check_update_invariant twn_loop atom =
-  let poly = Atom.poly atom in
-  let poly_updated = Polynomial.substitute_f (TWNLoop.update_full twn_loop) poly in
-  let atom_updated = Atom.mk_lt poly_updated Polynomial.zero in
-  SMTSolver.tautology Formula.(implies (mk [atom]) (mk [atom_updated]))
 
 let check_non_increasing twn_loop t =
   List.for_all (fun atom ->
