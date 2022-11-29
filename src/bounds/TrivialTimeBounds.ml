bf5b2ca6a48c737036409389305d811cf01b517b
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2022-11-29 16:24:44 +0100

    Generalised TrivialTimeBounds

--- src/bounds/TrivialTimeBounds.ml
+++ src/bounds/TrivialTimeBounds.ml
@@ -6,20 +6,27 @@ open ProgramModules
 (** This preprocessor infers for all transitions which are not part of an scc a time bound of one.
     Those transitions can only be executed once and preprocessing might increase performance and also might lead to better bounds. *)
 
-(** Transition graph represents scc. *)
-module SCC = Graph.Components.Make(TransitionGraph)
+module Make(PM: ProgramTypes.ProgramModules) = struct
+  open PM
 
-(** This preprocessor infers for all transitions which are not part of an scc a time bound of one.
-    Those transitions can only be executed once and preprocessing might increase performance and also might lead to better bounds. *)
-let compute program appr =
-  let graph = Program.graph program in
-  let (_, scc_number) = SCC.scc graph in
-  let same_scc l1 l2 =
-    scc_number l1 = scc_number l2
-  in
-  let one_bounded_transitions =
-    TransitionGraph.transitions graph
-    |> TransitionSet.filter (fun (l,t,l') -> not (same_scc l l'))
-  in
-  TransitionSet.fold (Approximation.add_timebound Bound.one) one_bounded_transitions appr
+  module Approximation = Approximation.Make(PM)
 
+  (** Transition graph represents scc. *)
+  module SCC = Graph.Components.Make(TransitionGraph)
+
+  (** This preprocessor infers for all transitions which are not part of an scc a time bound of one.
+      Those transitions can only be executed once and preprocessing might increase performance and also might lead to better bounds. *)
+  let compute program appr =
+    let graph = Program.graph program in
+    let (_, scc_number) = SCC.scc graph in
+    let same_scc l1 l2 =
+      scc_number l1 = scc_number l2
+    in
+    let one_bounded_transitions =
+      TransitionGraph.transitions graph
+      |> TransitionSet.filter (fun (l,t,l') -> not (same_scc l l'))
+    in
+    TransitionSet.fold (Approximation.add_timebound Bound.one) one_bounded_transitions appr
+end
+
+include Make(ProgramModules)
