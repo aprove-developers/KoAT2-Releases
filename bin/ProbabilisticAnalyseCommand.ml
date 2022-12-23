8532889455f03ceddd31319f4fd6db6268bd4895
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2022-12-23 16:43:13 +0100

    prob-analyse cmd: input file flag & mprf-depth arg

--- bin/ProbabilisticAnalyseCommand.ml
+++ bin/ProbabilisticAnalyseCommand.ml
@@ -14,13 +14,16 @@ let description = "Search for a probabilistic ranking function"
 let command = "prob-analyse"
 
 type params = {
-  input : string; [@pos 0]
-  (** Path to the koat input file which defines the probabilistic integer transition system. *)
+  input : string; [@aka ["i"]]
+  (** Either an absolute or relative path to the koat input file which defines the probabilistic integer transition system. *)
 
   preprocessors : Program.t Preprocessor.t list;
     [@enum Preprocessor.(List.map (fun p -> show p, p) all_probabilistic)]
     [@default Preprocessor.all_probabilistic ]
   (** The preprocessors which should be applied before running the actual algorithm. *)
+
+  mprf_depth : int; [@default 1] [@aka ["d"]]
+  (** The maximum depth of a Multiphase Ranking Function to bound search space.*)
 
   preprocessing_strategy : Program.t Preprocessor.strategy; [@enum Preprocessor.["once", process_only_once; "fixpoint", process_till_fixpoint]] [@default Preprocessor.process_till_fixpoint]
   (** The strategy which should be used to apply the preprocessors. *)
@@ -40,7 +43,7 @@ let run (params: params) =
 
   Printf.printf "prog %s\n\n" (Program.to_string_pretty program);
 
-  let classical_analysis_conf = { OverapprAnalysis.default_configuration with run_mprf_depth = Some 5 } in
+  let classical_analysis_conf = { OverapprAnalysis.default_configuration with run_mprf_depth = Some params.mprf_depth } in
 
   let program, class_appr =
     preprocess program
