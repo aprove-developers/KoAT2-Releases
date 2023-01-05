6e6ae1f5de3f8a5270d3598edf08a96ef240fc5c
Nils Lommen <nils.lommen@rwth-aachen.de>
2023-01-05 16:05:41 +0100

    twn: cache

--- src/bounds/Solvable/TWN.ml
+++ src/bounds/Solvable/TWN.ml
@@ -67,100 +67,24 @@ module Make(PM: ProgramTypes.ClassicalProgramModules) = struct
       (* We have not yet computed a (local) runtime bound. *)
       let loops_opt = SimpleCycle.find_loops heuristic_for_cycle appr program scc t in
       if Option.is_some loops_opt then
-        List.map (fun (entry,loop) ->
-          TWN_Complexity.complexity loop |> lift appr entry)
-        (Option.get loops_opt)
+        let cycle, loops = Option.get loops_opt in
+        let local_bounds = List.map (fun (entry,loop) -> entry, TWN_Complexity.complexity loop) loops in
+        List.iter (fun t -> TimeBoundTable.add time_bound_table t local_bounds) cycle;
+        List.map (Tuple2.uncurry @@ lift appr) local_bounds
         |> List.enum
         |> Bound.sum
-      else
-        Bound.infinity) in
+      else (
+        TimeBoundTable.add time_bound_table (l,t,l') [(l,t,l'),Bound.infinity];
+        Bound.infinity)) in
       if Option.is_some bound then
         Tuple2.first @@ Option.get bound
       else
         Bound.infinity
     ) else (
+      Printf.printf "hi id %i\n" (TransitionLabel.id t);
       (* We already have computed a (local) runtime bound and just lift it again.*)
       let xs = Option.get opt in
-      let bound_with_sizebound = Bound.sum_list (List.map (fun (entry, bound) -> lift appr entry bound) xs) in
+      let bound_with_sizebound = Bound.sum_list (List.map (Tuple2.uncurry @@ lift appr) xs) in
       bound_with_sizebound
     )
-
-    (* TWN_Proofs.proof := FormattedString.Empty;
-    let opt = TimeBoundTable.find_option time_bound_table (l,t,l') in
-    let bound =
-    if Option.is_none opt then (
-      let bound =
-        Timeout.timed_run 5. (fun () -> try
-          let loops = SimpleCycle.SimpleCycle(ProgramModules).find_cycle appr program scc t;
-          let entries = [] in (* TODO *)
-
-          (* TWN_Proofs.proof_append FormattedString.((mk_header_small (mk_str "Cycles:")) <>
-            (List.combine (List.map Transition.to_string_pretty entries_org) (List.map (TWNLoop.to_string ~pretty:true) twn_loops)
-            |> List.map (fun (a,b) -> FormattedString.mk_str_line ("entry: " ^ a) <> FormattedString.mk_block (FormattedString.mk_str_line ("results in twn-loop: " ^ b)))
-            |> FormattedString.mappend)); *)
-          let global_local_bounds =
-              List.map (fun (entry, loop) -> (* TODO extend for automorphisms (entry, loop, automorphism) *)
-              (* TWN_Proofs.proof_append FormattedString.(mk_header_small (mk_str @@ "Cycle by " ^  (Transition.to_id_string_pretty entry_org))); *)
-              let program_with_one_entry =
-                let non_entries =
-                  TransitionSet.to_list (TransitionSet.diff (Program.transitions program) (TransitionSet.of_list entries))
-                in
-                Program.from_enum
-                  (Program.start program)
-                  (List.enum non_entries (* TODO simplify *)
-                  |> Enum.append (Enum.singleton entry))
-              in
-
-              (* let twn_inv =
-                program_with_one_entry
-                |> InvariantGeneration.transform_program
-                |> MaybeChanged.unpack
-                |> Program.transitions
-                |> TransitionSet.filter (fun t -> List.exists (Transition.equal t) handled_transitions)
-                |> TransitionSet.to_list
-                |> List.map (TransitionLabel.invariant % Transition.label)
-                |> fun invariants -> List.flatten invariants |> List.filter (fun atom -> List.for_all (List.exists (Atom.equal atom)) invariants)
-                |> TWNLoop.add_invariant loop in
-              let eliminated_t = EliminateNonContributors.eliminate_t
-                  (TWNLoop.input_vars twn_inv) (Formula.vars @@ TWNLoop.guard twn_inv) (TWNLoop.update twn_inv) (TWNLoop.remove_non_contributors twn_inv)
-              in
-              if VarSet.is_empty (TWNLoop.vars eliminated_t) then
-                Bound.infinity, ([entry_org], Bound.infinity)
-              else (
-                let bound = Automorphism.transform_bound automorphism @@ TWN_Complexity.complexity eliminated_t in
-                if Bound.is_infinity bound then
-                  raise (TWN_Termination.Non_Terminating (handled_transitions, [entry_org]));
-                  TWN_Proofs.proof_append FormattedString.(
-                    mk_header_small (mk_str @@ "Lift Bound: ")
-                    <> mk_str_line @@ "Compute new entries: " ^ (new_entries |> List.enum |> Util.enum_to_string Transition.to_id_string_pretty)
-                    <> mk_str_line @@ "Non increasing transitions: " ^ (non_increasing |> List.enum |> Util.enum_to_string Transition.to_id_string_pretty));
-                  lift appr new_entries bound, (new_entries, bound)))
-            (List.map2 (fun (a,b) (c,d) -> (a,b,c,d)) (List.combine entries_org non_incr_entries) (List.combine twn_loops automorphisms))
-          in
-          List.iter (fun t -> TimeBoundTable.add time_bound_table t (handled_transitions, List.map Tuple2.second global_local_bounds)) handled_transitions;
-          global_local_bounds |> List.map Tuple2.first |> Bound.sum_list
-          with
-          | No_Cycle -> Logger.log logger Logger.DEBUG (fun () -> "twn", ["no twn_cycle found", ""]); Bound.infinity
-          | TWN_Termination.Non_Terminating (handled_transitions,entries)->
-              Logger.log logger Logger.DEBUG (fun () -> "twn", ["non terminating", ""]);
-              List.iter (fun t -> TimeBoundTable.add time_bound_table t (handled_transitions, List.map (fun t -> ([t], Bound.infinity)) entries)) handled_transitions;
-              Bound.infinity)   *)
-      in
-      if Option.is_some bound then
-        bound |> Option.get |> Tuple2.first
-      else (
-        Logger.log logger Logger.INFO (fun () -> "twn", ["Timeout", Bound.to_string Bound.infinity]);
-        Bound.infinity)
-    )
-    else (
-      let cycle, xs = Option.get opt in
-      let bound_with_sizebound = Bound.sum_list (List.map (fun (entry, bound) -> lift appr entry bound) xs) in
-      bound_with_sizebound |> tap (fun b -> Logger.log logger Logger.INFO (fun () -> "twn", ["global_bound", Bound.to_string b]))
-                          |> tap (fun b -> TWN_Proofs.proof_append FormattedString.((mk_str_line (b |> Bound.to_string ~pretty:true))))) in
-      (if Bound.compare_asy bound (Approximation.timebound appr (l,transition,l')) < 0 then
-        let formatted = FormattedString.((mk_header_big @@ mk_str "Time-Bound by TWN-Loops:") <>
-                mk_header_small (mk_str ("TWN-Loop t" ^ (TransitionLabel.id transition |> Util.natural_to_subscript) ^ " with runtime bound " ^ Bound.to_string ~pretty:true bound)) <>
-                !TWN_Proofs.proof) in
-        ProofOutput.add_to_proof @@ fun () -> formatted);
-      bound*)
 end
