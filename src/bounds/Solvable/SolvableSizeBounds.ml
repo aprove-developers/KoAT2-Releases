abd2979dd34d9f7517dfbb7d6546395241ba6f6d
Nils Lommen <nils.lommen@rwth-aachen.de>
2023-02-14 12:09:13 +0100

    add cache for size bounds

--- src/bounds/Solvable/SolvableSizeBounds.ml
+++ src/bounds/Solvable/SolvableSizeBounds.ml
@@ -71,31 +71,52 @@ let heuristic_for_cycle appr entry program loop =
   && VarSet.for_all (fun var -> Polynomial.is_linear @@ Loop.update_var loop var) (Program.input_vars program)
   && VarSet.for_all (fun var -> Polynomial.no_constant_addend @@ Loop.update_var loop var) (Program.input_vars program)
 
-let lift appr var (entry,local_bound) =
-  Bound.substitute_f (fun var -> Approximation.sizebound appr entry var) local_bound
 
-let improve_t program trans (l,t,l') appr =
+module VT = struct
+  type t = Transition.t * Var.t
+
+  let equal (t1,v1) (t2,v2) = Transition.equal t1 t2 && Var.equal v1 v2
+
+  let hash = Hashtbl.hash
+end
+
+module SizeBoundTable = Hashtbl.Make(VT)
+
+let size_bound_table: (Transition.t * Bound.t) list SizeBoundTable.t = SizeBoundTable.create 10
+
+let lift appr t var (entry,closed_form) =
+  (* Insert runtime bound. *)
+  let local_size = Bound.substitute (Var.of_string "n") ~replacement:(Approximation.timebound appr t) closed_form in
+  Bound.substitute_f (fun var -> Approximation.sizebound appr entry var) local_size
+
+let improve_t program trans t appr =
   VarSet.fold (fun var appr ->
-      if not @@ Bound.is_linear @@ Approximation.sizebound appr (l,t,l') var then
-          let loops_opt = SimpleCycle.find_loops ~relevant_vars:(Option.some @@ VarSet.singleton var) heuristic_for_cycle appr program trans t in
-          if Option.is_some loops_opt then
-              let cycle, loops = Option.get loops_opt in
-              let local_bounds =
-                  (* We first compute for every var (with a closed form) and every entry a local size bound *)
-                  List.map (fun (entry,(loop,_)) ->
-                      let blocks = Check_Solvable.check_solvable loop in
-                      if Option.is_none blocks then ((l,t,l'),Bound.infinity)
-                      else
-                        let block = List.find (List.mem var) @@ Option.get blocks in
-                        entry, run_python var block (matrix_of_linear_assignments t block) |? Bound.infinity) loops
-              in
-              (* Lifting previously computed local size bounds and store them in appr. *)
-              let lifted_bound = List.map (lift appr var) local_bounds |> List.enum |> Bound.sum in
-              List.fold_right (fun t -> Approximation.add_sizebound lifted_bound t var) cycle appr
-          else
-              appr
+    if SizeBoundTable.mem size_bound_table (t, var) then
+      let local_bounds = SizeBoundTable.find size_bound_table (t, var) in
+      let lifted_bound = List.map (lift appr t var) local_bounds |> List.enum |> Bound.sum in
+      Approximation.add_sizebound lifted_bound t var appr
+    else
+      if not @@ Bound.is_polynomial @@ Approximation.sizebound appr t var then
+        let loops_opt = SimpleCycle.find_loops ~relevant_vars:(Option.some @@ VarSet.singleton var) heuristic_for_cycle appr program trans (Transition.label t) in
+        if Option.is_some loops_opt then
+          let cycle, loops = Option.get loops_opt in
+          let local_bounds =
+            (* We first compute for every var (with a closed form) and every entry a local size bound *)
+            List.map (fun (entry,(loop,_)) ->
+                let blocks = Check_Solvable.check_solvable loop in
+                if Option.is_none blocks then (t,Bound.infinity)
+                else
+                  let block = List.find (List.mem var) @@ Option.get blocks in
+                  entry, run_python var block (matrix_of_linear_assignments (Transition.label t) block) |? Bound.infinity) loops
+          in
+          List.iter (fun t -> SizeBoundTable.add size_bound_table (t,var) local_bounds) cycle;
+          (* Lifting previously computed local size bounds and store them in appr. *)
+          let lifted_bound = List.map (lift appr t var) local_bounds |> List.enum |> Bound.sum in
+          List.fold_right (fun t -> Approximation.add_sizebound lifted_bound t var) cycle appr
+        else
+          appr
       else
-          appr) (TransitionLabel.input_vars t) appr
+        appr) (TransitionLabel.input_vars (Transition.label t)) appr
 
 let improve program ?(scc = None) appr =
   let trans = scc |? TransitionSet.filter (Approximation.is_time_bounded appr) @@ Program.transitions program in
