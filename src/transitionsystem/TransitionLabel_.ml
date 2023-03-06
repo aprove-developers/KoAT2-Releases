aprove-developers/KoAT2-Releases

commit 36575aa6fe51eb336ebd8d3aeb8e2f8bf14e3fe7

some changes for cfr

src/transitionsystem/TransitionLabel_.ml
@@ -12,7 +12,7 @@ type t = {
     id : int;
     update : Polynomial.t VarMap.t;
     guard : Guard.t;
-    invariant :Invariant.t;
+    invariant : Invariant.t;
     cost : Polynomial.t;
   }
 
@@ -77,7 +77,7 @@ let fill_up_arg_vars_up_to_num n t =
   {t with update = fill_up_update_arg_vars_up_to_num n t.update}
 
 
-let mk ~id ~cost ~assignments ~patterns ~guard ~vars =
+let mk ~id ~cost ~assignments ~patterns ~guard =
   let map_to_arg_vars =
     Enum.combine (List.enum patterns) (LazyList.enum Var.args)
     |> RenameMap.of_enum
@@ -373,14 +373,9 @@ let rename rename_map t =
   |> tap (fun _ -> Hashtbl.clear vars_memoization)
 
 let remove_non_contributors non_contributors t =
-    let update_ = VarSet.fold (fun var u -> VarMap.remove var u) non_contributors t.update in
-    {
-    id = unique ();
-    update = update_;
-    guard = t.guard;
-    invariant = t.invariant;
-    cost = t.cost;
-    }
+  let patterns = List.filter (flip VarSet.mem non_contributors) (VarSet.to_list @@ input_vars t) in
+  let assignments = List.map (flip VarMap.find t.update) patterns in
+  mk ~cost:t.cost ~assignments:assignments ~patterns:patterns ~guard:t.guard ~id:None
 
 (* We execute CFRefinement with guard && invariant -> We need to separate invariant afterwards. *)
 let separate_guard_invariant t invariant' = {t with guard = List.filter (fun atom -> List.exists (fun atom_inv -> Atoms.Atom.equal atom atom_inv) invariant' |> not) t.guard;

src/bounds/Analysis.ml
@@ -306,6 +306,9 @@ let scc_cost_bounds ~conf program scc appr =
 
 let handle_timeout_cfr method_name non_linear_transitions =
   Program.reset_pre_cache ();
+  TWN.reset_cfr();
+  TWNSizeBounds.reset_cfr();
+  SolvableSizeBounds.reset_cfr();
   Logger.log logger_cfr Logger.INFO (fun () -> "TIMEOUT_CFR_" ^ method_name, ["scc", (TransitionSet.to_string non_linear_transitions)])
 
 
@@ -359,6 +362,9 @@ let handle_timeout_cfr method_name non_linear_transitions =
             add_missing_lsbs program_cfr lsbs;
             Logger.log logger_cfr Logger.DEBUG (fun () -> "apply_" ^ method_name, ["already_used:", (TransitionSet.to_string !already_used_cfr)]);
             Program.reset_pre_cache ();
+            TWN.reset_cfr();
+            TWNSizeBounds.reset_cfr();
+            SolvableSizeBounds.reset_cfr();
             let rvg_with_sccs_cfr = RVG.rvg_with_sccs (Option.map (LSB.vars % Tuple2.first) % LSB_Table.find lsbs) program_cfr in
             (* The new sccs which do not occur in the original program. *)
             let cfr_sccs = program_cfr
@@ -382,6 +388,9 @@ let handle_timeout_cfr method_name non_linear_transitions =
             if (Bound.compare_asy org_bound cfr_bound) < 1 then (
               ProofOutput.add_to_proof (fun () -> FormattedString.mk_str_header_big "CFR did not improve the program. Rolling back");
               Program.reset_pre_cache ();
+              TWN.reset_cfr();
+              TWNSizeBounds.reset_cfr();
+              SolvableSizeBounds.reset_cfr();
               Logger.log logger_cfr Logger.INFO (fun () -> "NOT_IMPROVED",
                                                           ["original bound", (Bound.to_string org_bound); method_name ^ " bound", (Bound.show_complexity (Bound.asymptotic_complexity cfr_bound))]);
               (program, appr, rvg_with_sccs)
@@ -454,4 +463,4 @@ let handle_timeout_cfr method_name non_linear_transitions =
     in
     let appr_with_costbounds = CostBounds.infer_from_timebounds program appr in
     program, appr_with_costbounds
-  end
+end

src/bounds/Solvable/SolvableSizeBounds.ml
@@ -73,6 +73,13 @@ module SizeBoundTable = Hashtbl.Make(VT)
 
 let size_bound_table: (Bound.t * (Transition.t * Polynomial.t VarMap.t) list) SizeBoundTable.t = SizeBoundTable.create 10
 
+(** Internal memoization: The idea is to use this cache if we applied cfr and
+  1) delete it and use the original cache if we get a timeout or
+  2) if the analysis of the unrolled scc is completed successfully use this cache as the main memory.
+  TODO Currently, we just reset the cache. *)
+let reset_cfr () =
+  SizeBoundTable.clear size_bound_table
+
 let lift appr t var bound (entry,traversal) =
   (* Insert runtime bound. *)
   let local_size = Bound.substitute (Var.of_string "n") ~replacement:(Approximation.timebound appr t) bound

src/bounds/Solvable/SolvableSizeBounds.mli
@@ -1,3 +1,5 @@
 open ProgramModules
 
 val improve : Program.t -> ?scc:TransitionSet.t option -> Approximation.t -> Approximation.t
+
+val reset_cfr : unit -> unit

src/bounds/Solvable/TWN.ml
@@ -25,6 +25,13 @@ module Make(PM: ProgramTypes.ClassicalProgramModules) = struct
   (* Keys: transition, values: bounds of entry transitions. *)
   let time_bound_table: (Transition.t * Bound.t) list TimeBoundTable.t = TimeBoundTable.create 10
 
+  (** Internal memoization: The idea is to use this cache if we applied cfr and
+    1) delete it and use the original cache if we get a timeout or
+    2) if the analysis of the unrolled scc is completed successfully use this cache as the main memory.
+    TODO Currently, we just reset the cache. *)
+  let reset_cfr () =
+    TimeBoundTable.clear time_bound_table
+
   let lift appr entry bound =
     let bound_with_sizebound = Bound.substitute_f (Approximation.sizebound appr entry) bound in
       Bound.mul (Approximation.timebound appr entry) bound_with_sizebound

src/bounds/Solvable/TWN.mli
@@ -8,4 +8,6 @@ module Make(PM: ProgramTypes.ClassicalProgramModules): sig
   module Approximation : module type of  Approximation.MakeForClassicalAnalysis(PM)
 
   val time_bound : configuration -> Transition.t -> TransitionSet.t -> Program.t -> Approximation.t -> Bound.t
+
+  val reset_cfr : unit -> unit
 end

src/bounds/Solvable/TWNSizeBounds.ml
@@ -26,6 +26,13 @@ module SizeBoundTable = Hashtbl.Make(VT)
 
 let size_bound_table: (PE.t Option.t * (Transition.t * Polynomial.t VarMap.t) list) SizeBoundTable.t = SizeBoundTable.create 10
 
+(** Internal memoization: The idea is to use this cache if we applied cfr and
+  1) delete it and use the original cache if we get a timeout or
+  2) if the analysis of the unrolled scc is completed successfully use this cache as the main memory.
+  TODO Currently, we just reset the cache. *)
+let reset_cfr () =
+  SizeBoundTable.clear size_bound_table
+
 let lift appr t var closed_form (entry,traversal) =
   (* Insert runtime bound. *)
   let local_size =

src/bounds/Solvable/TWNSizeBounds.mli
@@ -1,3 +1,5 @@
 open ProgramModules
 
 val improve : Program.t -> ?scc:TransitionSet.t option -> Approximation.t -> Approximation.t
+
+val reset_cfr : unit -> unit

commit cda31ab9f71d9ff69724c824ce5c5530f77738a9

fix a bug introduced in 36575aa

src/transitionsystem/TransitionLabel_.ml
@@ -373,7 +373,7 @@ let rename rename_map t =
   |> tap (fun _ -> Hashtbl.clear vars_memoization)
 
 let remove_non_contributors non_contributors t =
-  let patterns = List.filter (flip VarSet.mem non_contributors) (VarSet.to_list @@ input_vars t) in
+  let patterns = List.filter (flip VarSet.mem (VarSet.diff (input_vars t) non_contributors)) (VarSet.to_list @@ input_vars t) in
   let assignments = List.map (flip VarMap.find t.update) patterns in
   mk ~cost:t.cost ~assignments:assignments ~patterns:patterns ~guard:t.guard ~id:None
 