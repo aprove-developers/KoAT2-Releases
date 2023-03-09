63f394a36144299722e06fc0c0655be232d57a17
Nils Lommen <nils.lommen@rwth-aachen.de>
2023-03-09 10:06:00 +0100

    more restrictive heuristics for size bounds

--- src/bounds/Solvable/TWNSizeBounds.ml
+++ src/bounds/Solvable/TWNSizeBounds.ml
@@ -46,37 +46,40 @@ let lift appr t var closed_form (entry,traversal) =
 
 let improve_t program trans t appr =
   VarSet.fold (fun var appr ->
-    if SizeBoundTable.mem size_bound_table (t,var) then
-      let closed_form,entry_traversal = SizeBoundTable.find size_bound_table (t,var) in
-      let lifted_bound = List.map (lift appr t var closed_form) entry_traversal |> List.enum |> Bound.sum in
-      Approximation.add_sizebound lifted_bound t var appr
+    if Polynomial.is_linear (TransitionLabel.update (Transition.label t) var |? Polynomial.of_var var) then
+      appr
     else
-      if not @@ Bound.is_polynomial @@ Approximation.sizebound appr t var then
-        let loops_opt = SimpleCycle.find_loop ~relevant_vars:(Option.some @@ VarSet.singleton var) heuristic_for_cycle appr program trans t in
-        if Option.is_some loops_opt then
-          let loop, entries_traversal = Option.get loops_opt in
-          let local_bound =
-              (* We first compute for var (with a closed form) a local size bound *)
-              let order = Check_TWN.check_triangular loop in
-              if List.is_empty order then None
-              else
-                let closed_form =
-                    PE.compute_closed_form @@ List.map (fun var ->
-                        (var, Loop.update_var loop var)) order
-                    |> List.combine order
-                    |> List.find (Var.equal var % Tuple2.first)
-                    |> Tuple2.second
-                in
-                Option.some closed_form
-          in
-          SizeBoundTable.add size_bound_table (t,var) (local_bound,entries_traversal);
-            (* Lifting previously computed local size bounds and store them in appr. *)
-          let lifted_bound = List.map (lift appr t var local_bound) entries_traversal |> List.enum |> Bound.sum in
-          Approximation.add_sizebound lifted_bound t var appr
+      if SizeBoundTable.mem size_bound_table (t,var) then
+        let closed_form,entry_traversal = SizeBoundTable.find size_bound_table (t,var) in
+        let lifted_bound = List.map (lift appr t var closed_form) entry_traversal |> List.enum |> Bound.sum in
+        Approximation.add_sizebound lifted_bound t var appr
       else
-        appr
-    else
-      appr) (TransitionLabel.input_vars (Transition.label t)) appr
+        if not @@ Bound.is_polynomial @@ Approximation.sizebound appr t var then
+          let loops_opt = SimpleCycle.find_loop ~relevant_vars:(Option.some @@ VarSet.singleton var) heuristic_for_cycle appr program trans t in
+          if Option.is_some loops_opt then
+            let loop, entries_traversal = Option.get loops_opt in
+            let local_bound =
+                (* We first compute for var (with a closed form) a local size bound *)
+                let order = Check_TWN.check_triangular loop in
+                if List.is_empty order then None
+                else
+                  let closed_form =
+                      PE.compute_closed_form @@ List.map (fun var ->
+                          (var, Loop.update_var loop var)) order
+                      |> List.combine order
+                      |> List.find (Var.equal var % Tuple2.first)
+                      |> Tuple2.second
+                  in
+                  Option.some closed_form
+            in
+            SizeBoundTable.add size_bound_table (t,var) (local_bound,entries_traversal);
+              (* Lifting previously computed local size bounds and store them in appr. *)
+            let lifted_bound = List.map (lift appr t var local_bound) entries_traversal |> List.enum |> Bound.sum in
+            Approximation.add_sizebound lifted_bound t var appr
+        else
+          appr
+      else
+        appr) (TransitionLabel.input_vars (Transition.label t)) appr
 
 let improve program ?(scc = None) appr =
   let trans = scc |? TransitionSet.filter (Approximation.is_time_bounded appr) @@ Program.transitions program in
