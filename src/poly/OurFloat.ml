5cfd32e63ad1236bc3c72606c08363e013d2a029
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2023-03-10 09:49:37 +0100

    Migr. OurFloat and OurInt from BigInt/Num to Zarith
    Num seems more or less unmaintained and Zarith should be faster

--- src/poly/OurFloat.ml
+++ src/poly/OurFloat.ml
@@ -1,37 +1,52 @@
 open Batteries
 
-include Number.MakeNumeric(Num)
+exception Div_Zero of string
 
-let is_integral = Num.is_integer_num
+(** [n] must be non-negative. Raises Div_Zero for [0^0] *)
+let pow_ourint b e =
+  let open Q in
+  let rec pow_ourint_tail_rec_pos_e acc (e:Z.t) =
+    if Z.(equal e zero) then acc else pow_ourint_tail_rec_pos_e (acc*b) Z.(e-one)
+  in
+  if b = zero then (
+    if Z.(equal zero e) then raise (Div_Zero "OurFloat.pow_ourint 0^0") else zero
+  )
+  else if Z.Compare.(e >= Z.zero)  then
+    pow_ourint_tail_rec_pos_e one e
+  else
+    one / pow_ourint_tail_rec_pos_e one (Z.neg e)
+
+include Number.MakeNumeric(struct
+    include Q
+    let succ = Q.((+) one)
+    let pred a = Q.(a - one)
+    let modulo _ _  = failwith "modulo not defined for floats"
+    let pow b e = pow_ourint b (to_bigint e)
+  end)
+include Q
+
+let pow b e = pow_ourint b Z.(of_int e)
+
+let is_integral t = OurInt.equal OurInt.one t.den
 let sum = Enum.fold (+) zero
 let list_sum = sum % List.enum
 
 let (=~=) = equal
 
-let pow i (n: int) = pow i (of_int n)
+let of_ourint = of_bigint
 
-let max a b =
-  if Compare.(a >= b) then
-    a
-  else
-    b
-
-let min a b =
-  if Compare.(a <= b) then
-    a
-  else
-    b
-
-let of_ourint = Num.num_of_big_int
-
-let to_ourint_ceiled =
-  (* OurInt.of_int (Num.to_int (Num.ceil (add x zero))) (\** TODO maybe we have to add here one*\) *)
-  Num.big_int_of_num % Num.ceil
-
-let (>) = Num.(>/)
-let (<) = Num.(</)
-
-let (>=) = Num.(>=/)
-let (<=) = Num.(<=/)
+(** Floors the result *)
+let to_ourint t = Z.fdiv t.num t.den
 
 let minus_one = sub zero one
+
+module Compare = struct
+  let (<) = (<)
+  let (<=) = (<=)
+  let (>=) = (>=)
+  let (>) = (>)
+end
+
+let floor t = Z.fdiv t.num t.den
+
+let ceil t = Z.(floor t + if Z.equal zero (t.num mod t.den) then zero else one)
--- src/poly/OurInt.ml
+++ src/poly/OurInt.ml
@@ -1,53 +1,31 @@
 open Batteries
 
-include Number.MakeNumeric(Big_int)
+let is_zero =
+  Z.(equal zero)
+
+let pow_ourint i n =
+  let open Z in
+  let rec helper i m = if is_zero m then one else mul i (helper i (m - one)) in
+  helper i n
+
+include Number.MakeNumeric(struct include Z let modulo = ((mod)) let pow = pow_ourint end)
+include Z
+
 let (=~=) = equal
 
 let is_integral _ = true
 
-(* TODO Make it possible to pow with Big_int *)
-let pow i (n: int) = pow i (of_int n)
-
-let max a b =
-  if Compare.(a >= b) then
-    a
-  else
-    b
-
-let min a b =
-  if Compare.(a <= b) then
-    a
-  else
-    b
-
 let of_ourint =
   identity
 
-let minus_one = sub zero one
-
-let is_zero a =
-  equal a zero
-
 let is_negative a =
-  compare zero a > 0
+  Compare.(zero > a)
 
-let is_ge a b =
-  compare a b >= 0
+let is_ge =
+  Compare.(>=)
 
-let is_gt a b =
-  compare a b > 0
-
-let pow_ourint i n =
-    let rec helper i m = if is_zero m then one else mul i (helper i (m - one)) in
-    helper i n
-
-let rec gcd a b =
-  if is_zero b then abs a
-  else gcd b (modulo a b)
-
-let lcm a b =
-  if is_zero a || is_zero b then zero
-  else abs (mul a (div b (gcd a b)))
+let is_gt =
+  Compare.(>)
 
 let rec lcm_list = function
   | [] -> one
@@ -58,5 +36,3 @@ let rec max_list = function
   | x::xs -> max x (max_list xs)
 
 let sum_list = List.fold (+) zero
-
-let sign a = if is_zero a then 0 else if is_negative a then -1 else 1
