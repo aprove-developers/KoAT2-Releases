08eee131db4f7792171f4081752b13052a1703f2
janek <janek.stoppkotte@rwth-aachen.de>
2023-06-03 17:38:47 +0200

    Revert "fixed Mutex lock bug" since it did not solve the problem
    This reverts commit e7f1df871f35033b94cac6a8f426ed888675080e.

--- src/bounds/Solvable/TWN.ml
+++ src/bounds/Solvable/TWN.ml
@@ -30,8 +30,6 @@ module Make(PM: ProgramTypes.ClassicalProgramModules) = struct
   (* Keys: transition, values: bounds of entry transitions. *)
   let time_bound_table: (Transition.t * Bound.t) list TimeBoundTable.t = TimeBoundTable.create 10
   let termination_table: (Transition.t * bool) list TimeBoundTable.t = TimeBoundTable.create 10
-
-  let trans_table = Hashtbl.create 10
   (** Internal memoization: The idea is to use this cache if we applied cfr and
     1) delete it and use the original cache if we get a timeout or
     2) if the analysis of the unrolled scc is completed successfully use this cache as the main memory.
@@ -65,7 +63,6 @@ module Make(PM: ProgramTypes.ClassicalProgramModules) = struct
     | `Transformation -> Option.is_some @@ Check_Solvable.check_solvable loop (*  *)
 
   let time_bound conf (l,t,l') scc program appr =
-    Hashtbl.clear trans_table;
     TWN_Proofs.proof_reset();
     TWN_Proofs.proof_append @@ mk_str_header_big @@ "TWN: " ^ (Transition.to_id_string_pretty (l,t,l'));
     let bound =
@@ -73,12 +70,12 @@ module Make(PM: ProgramTypes.ClassicalProgramModules) = struct
       if Option.is_none opt then (
         let bound = Timeout.timed_run 5. (fun () ->
         (* We have not yet computed a (local) runtime bound. *)
-        let loops_opt = SimpleCycle.find_loops ~relax_loops:conf.relax_loops trans_table (heuristic_for_cycle conf.transformation_type) appr program scc (l,t,l') in
+        let loops_opt = SimpleCycle.find_loops ~relax_loops:conf.relax_loops (heuristic_for_cycle conf.transformation_type) appr program scc (l,t,l') in
         if Option.is_some loops_opt then
           let cycle, loops = Option.get loops_opt in
           let upd_invariant_cand = List.map (Constraint.atom_list % TransitionLabel.invariant % Tuple3.second) cycle |> List.flatten in
           let local_bounds = List.map (fun (entry,(loop,aut)) -> entry, Automorphism.apply_to_bound (TWN_Complexity.complexity ~entry:(Option.some entry) upd_invariant_cand loop) aut) loops in
-          List.iter (fun (l,t,l') -> TimeBoundTable.add time_bound_table (l,Hashtbl.find_default trans_table t t,l') local_bounds) cycle;
+          List.iter (fun t -> TimeBoundTable.add time_bound_table t local_bounds) cycle;
           List.map (Tuple2.uncurry @@ lift (l,t,l') appr) local_bounds
           |> List.enum
           |> Bound.sum
@@ -102,7 +99,6 @@ module Make(PM: ProgramTypes.ClassicalProgramModules) = struct
     bound
 
     let terminates conf (l,t,l') scc program appr =
-      Hashtbl.clear trans_table;
       TWN_Proofs.proof_reset();
       TWN_Proofs.proof_append @@ mk_str_header_big @@ "TWN: " ^ (Transition.to_id_string_pretty (l,t,l'));
       let compute_new_bound =
@@ -112,12 +108,11 @@ module Make(PM: ProgramTypes.ClassicalProgramModules) = struct
             let upd_invariant_cand = List.map (Constraint.atom_list % TransitionLabel.invariant % Tuple3.second) cycle |> List.flatten in
             let is_bounded entry loop = TWN_Termination.termination ~entry:(Option.some entry) upd_invariant_cand loop in
             let local_bounds = List.map (fun (entry,(loop,_)) -> entry, is_bounded entry loop) loops in
-            let orig t = Hashtbl.find_default trans_table t t in
-            List.iter (fun (l,t,l') -> TimeBoundTable.add termination_table (l,orig t,l') local_bounds) cycle;
+            List.iter (fun t -> TimeBoundTable.add termination_table t local_bounds) cycle;
             List.for_all Tuple2.second local_bounds in
           let handle_missing_loops = TimeBoundTable.add termination_table (l,t,l') [(l,t,l'),false] in
 
-          SimpleCycle.find_loops ~relax_loops:conf.relax_loops trans_table (heuristic_for_cycle conf.transformation_type) appr program scc (l,t,l')
+          SimpleCycle.find_loops ~relax_loops:conf.relax_loops (heuristic_for_cycle conf.transformation_type) appr program scc (l,t,l')
           (*If no simple loops were found we cannot prove termination*)
           |> Option.map_default compute_termination (handle_missing_loops; false)) in
         (* In case no bound was computed this maps to false otherwise to the bound*)
