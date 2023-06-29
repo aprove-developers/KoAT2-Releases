77c90026b6f3cb35bd6f1c08d4a07f386929b07a
Nils Lommen <nils.lommen@rwth-aachen.de>
2023-06-29 16:29:59 +0200

    remove some superfuous files

--- apply_to_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# ./test_folder path strategy
-for filename in $1*.koat; do 
-    echo "path ${filename}"
-    LANG=C koat2 analyse -i "${filename}" $2
-    LANG=C koat2 analyse -i "${filename}" $3
-done
