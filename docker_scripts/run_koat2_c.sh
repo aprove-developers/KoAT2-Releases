6372eb32a5de2c0bc9ab79a4de51adddcba12b36
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2023-08-02 16:48:37 +0200

    docker_scripts: C should only place files in WD
    Currently the script to analyse C files in the docker container writes a temporary C file to the same directory of the input.
    This is inconvenient for evaluation scripts that mount a benchmark folder as this will polute the benchmark set.
    Write to the working directory instead.

--- docker_scripts/run_koat2_c.sh
+++ docker_scripts/run_koat2_c.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sed 's/typedef enum {false, true} bool;/const int false = 0;\nconst int true = 1;/g' ${@: -1} > ${@: -1}.const.c
-clang -c ${@: -1}.const.c -emit-llvm -Wno-logical-op-parentheses -o out.bc # last argument i.e. file name
+sed 's/typedef enum {false, true} bool;/const int false = 0;\nconst int true = 1;/g' ${@: -1} > replaced.c
+clang -c replaced.c -emit-llvm -Wno-logical-op-parentheses -o out.bc # last argument i.e. file name
 llvm2kittel -uniform-complexity-tuples -division-constraint exact out.bc > out
 koat2 ${@:2:$#-2} out
