1bbb328cfbb8c52c795200a190bacda8d83f2ba2
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2023-08-03 15:04:49 +0200

    Consider value of ProofOutput.format at the time of adding proof

--- src/util/ProofOutput.ml
+++ src/util/ProofOutput.ml
@@ -21,7 +21,8 @@ let add_to_proof str_computation =
 
 let add_to_proof_with_format str_computation =
   if !proof_enabled then
-    proof := let p = !proof in lazy(Lazy.force p<>str_computation !format)
+    let format = !format in
+    proof := let p = !proof in lazy(Lazy.force p<>str_computation format)
 
 let add_str_paragraph_to_proof f =
   add_to_proof @@ (FormattedString.mk_paragraph % FormattedString.mk_str_line % f )
