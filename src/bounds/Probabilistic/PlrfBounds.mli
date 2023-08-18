37671990dbefa7a93ed67454bb459477b12e7831
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2023-08-18 11:02:56 +0200

    PlrfBounds: Add interface file

---
+++ src/bounds/Probabilistic/PlrfBounds.mli
@@ -0,0 +1,11 @@
+open OurBase
+open ProbabilisticProgramModules
+open Approximation.Probabilistic
+
+val improve_timebounds_plrf :
+  compute_refined_plrfs:bool ->
+  Program.t ->
+  GeneralTransitionSet.t ->
+  ClassicalApproximation.t * ExpApproximation.t ->
+  ExpApproximation.t MaybeChanged.t
+(** Improve the approximation by computing new PLRFs and lifting them to new expected time bound *)
