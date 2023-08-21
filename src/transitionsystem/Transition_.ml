5df9478b6ed9dff1395c24d7ebd58a0654febe35
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2023-08-21 15:25:59 +0200

    Transition.equal/compare: Use derived comparator

--- src/transitionsystem/Transition_.ml
+++ src/transitionsystem/Transition_.ml
@@ -5,82 +5,75 @@ include Constraints
 
 (** Creates a Transition module for a given location type *)
 module Make (TL : ProgramTypes.TransitionLabel) (L : ProgramTypes.Location) = struct
-  module Inner = struct
-    type location = L.t
-    type location_comparator_witness = L.comparator_witness
-    type transition_label = TL.t
-    type transition_label_comparator_witness = TL.comparator_witness
-    type t = location * transition_label * location
-
-    let equal_ equal_lbl (l1, t1, l1') (l2, t2, l2') = L.equal l1 l2 && equal_lbl t1 t2 && L.equal l1' l2'
-    let equal = equal_ TL.equal
-    let equivalent = equal_ TL.equivalent
-
-    let compare_f compare_lbl (l1, t1, l1') (l2, t2, l2') =
-      if not (L.equal l1 l2) then
-        L.compare l1 l2
-      else if compare_lbl t1 t2 <> 0 then
-        compare_lbl t1 t2
-      else if not (L.equal l1' l2') then
-        L.compare l1' l2'
-      else
-        0
-
-
-    let compare_equivalent = compare_f TL.compare_equivalent
-    let compare (_, t1, _) (_, t2, _) = Int.compare (TL.id t1) (TL.id t2)
-    let src (src, _, _) = src
-    let label (_, label, _) = label
-    let target (_, _, target) = target
-    let id = TL.id % label
-    let map_label f (l, label, l') = (l, f label, l')
-    let cost t = TL.cost (label t)
-    let hash (transition : t) = id transition |> Hashtbl.hash
-
-    let to_id_string (l, label, l') =
-      (Int.to_string % TL.id) label ^ ": " ^ L.to_string l ^ "->" ^ L.to_string l'
-
-
-    let to_id_string_pretty (l, label, l') =
-      "t" ^ Util.natural_to_subscript (TL.id label) ^ ": " ^ L.to_string l ^ "→" ^ L.to_string l'
-
-
-    let to_string (l, t, l') =
-      Int.to_string (TL.id t)
-      ^ ":" ^ L.to_string l
-      ^ TL.(update_to_string_lhs t)
-      ^ " -" ^ TL.cost_to_string t ^ "> " ^ L.to_string l' ^ TL.update_to_string_rhs t
-      ^
-      if Constraint.is_true (TL.guard t) then
-        ""
-      else
-        ":|:" ^ TL.(Guard.to_string (TL.guard t))
-
-
-    let to_string_pretty (l, t, l') =
-      "t"
-      ^ Util.natural_to_subscript (TL.id t)
-      ^ ": " ^ L.to_string l
-      ^ TL.(update_to_string_lhs_pretty t)
-      ^ (if Polynomials.Polynomial.(equal one (TL.cost t)) then
-           " → "
-         else
-           " -" ^ TL.cost_to_string t ^ "> ")
-      ^ L.to_string l' ^ TL.update_to_string_rhs_pretty t
-      ^
-      if Constraint.is_true (TL.guard t) then
-        ""
-      else
-        " :|: " ^ TL.(Guard.to_string ~pretty:true (TL.guard t))
-  end
-
-  include Inner
+  type location = L.t
+  type location_comparator_witness = L.comparator_witness
+  type transition_label = TL.t
+  type transition_label_comparator_witness = TL.comparator_witness
+  type t = location * transition_label * location
 
   type comparator_witness =
     (TL.comparator_witness, L.comparator_witness) TransitionComparator.comparator_witness
 
   let comparator = TransitionComparator.comparator TL.comparator L.comparator
   let sexp_of_t = Sexplib0.Sexp_conv.sexp_of_opaque
+  let equal = Comparator.equal_of_comparator comparator
+  let equivalent (l1, t1, l1') (l2, t2, l2') = L.equal l1 l2 && TL.equivalent t1 t2 && L.equal l1' l2'
+  let compare = Comparator.compare_of_comparator comparator
+
+  let compare_equivalent (l1, t1, l1') (l2, t2, l2') =
+    if not (L.equal l1 l2) then
+      L.compare l1 l2
+    else if TL.compare_equivalent t1 t2 <> 0 then
+      TL.compare_equivalent t1 t2
+    else if not (L.equal l1' l2') then
+      L.compare l1' l2'
+    else
+      0
+
+
+  let src (src, _, _) = src
+  let label (_, label, _) = label
+  let target (_, _, target) = target
+  let id = TL.id % label
+  let map_label f (l, label, l') = (l, f label, l')
+  let cost t = TL.cost (label t)
+  let hash (transition : t) = id transition |> Hashtbl.hash
+
+  let to_id_string (l, label, l') =
+    (Int.to_string % TL.id) label ^ ": " ^ L.to_string l ^ "->" ^ L.to_string l'
+
+
+  let to_id_string_pretty (l, label, l') =
+    "t" ^ Util.natural_to_subscript (TL.id label) ^ ": " ^ L.to_string l ^ "→" ^ L.to_string l'
+
+
+  let to_string (l, t, l') =
+    Int.to_string (TL.id t)
+    ^ ":" ^ L.to_string l
+    ^ TL.(update_to_string_lhs t)
+    ^ " -" ^ TL.cost_to_string t ^ "> " ^ L.to_string l' ^ TL.update_to_string_rhs t
+    ^
+    if Constraint.is_true (TL.guard t) then
+      ""
+    else
+      ":|:" ^ TL.(Guard.to_string (TL.guard t))
+
+
+  let to_string_pretty (l, t, l') =
+    "t"
+    ^ Util.natural_to_subscript (TL.id t)
+    ^ ": " ^ L.to_string l
+    ^ TL.(update_to_string_lhs_pretty t)
+    ^ (if Polynomials.Polynomial.(equal one (TL.cost t)) then
+         " → "
+       else
+         " -" ^ TL.cost_to_string t ^ "> ")
+    ^ L.to_string l' ^ TL.update_to_string_rhs_pretty t
+    ^
+    if Constraint.is_true (TL.guard t) then
+      ""
+    else
+      " :|: " ^ TL.(Guard.to_string ~pretty:true (TL.guard t))
 end
 
 module MakeClassical (TL : ProgramTypes.ClassicalTransitionLabel) (L : ProgramTypes.Location) = struct
