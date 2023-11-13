341a16d506c0e5ee0e37712746bace9a5a3dc398
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2023-11-13 16:04:52 +0100

    Fixed bug introduced in 3a649de0
    Prior, the argument `appr` was shadowed

--- src/bounds/CFR/CFR.ml
+++ src/bounds/CFR/CFR.ml
@@ -8,21 +8,16 @@ module CFR (Bound : BoundType.Bound) = struct
   (** Generates the approximation for the new program_cfr from the one of the original program. *)
   let merge_appr (program : Program.t) (program_cfr : Program.t) appr =
     let unchanged_trans = Base.Set.inter (Program.transitions program) (Program.transitions program_cfr) in
-    let appr = Approximation.empty |> TrivialTimeBounds.compute program_cfr in
+    let appr_cfr = Approximation.empty |> TrivialTimeBounds.compute program_cfr in
     unchanged_trans
     |> Base.Set.fold
          ~f:(fun appr_cfr trans ->
-           let timebound = appr |> flip Approximation.timebound trans
-           and costbound = appr |> flip Approximation.costbound trans in
-           (* Base.Set.fold
-              ~f:(fun appr_cfr x ->
-                let sizebound_x = Approximation.sizebound appr trans x in
-                appr_cfr |> Approximation.add_sizebound sizebound_x trans x)
-              (Program.vars program) ~init:appr_cfr *)
+           let timebound = Approximation.timebound appr trans
+           and costbound = Approximation.costbound appr trans in
            appr_cfr
            |> Approximation.add_timebound timebound trans
            |> Approximation.add_costbound costbound trans)
-         ~init:appr
+         ~init:appr_cfr
 
 
   let lift_to_program transform program =
