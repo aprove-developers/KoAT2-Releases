c2fe20772d47526e84477ac41104828a2830f49f
Eleanore Meyer <eleanore.meyer@cs.rwth-aachen.de>
2023-11-14 10:44:23 +0100

    Add comment to GenericProgram

--- src/transitionsystem/GenericProgram_.ml
+++ src/transitionsystem/GenericProgram_.ml
@@ -9,6 +9,7 @@ type (!'trans_label, !'trans_label_cmp_wit) pre_cache =
 (** This allows us to show type equality of programs created with Make outside of this module.
     For instance in the case of probabilistic programs we want the abstract type t of probabilistic programs and overapproximated probabilistic programs to coincide *)
 
+(* This should not be exposed. Move to Program_ ?  How can we achieve this without having the cyclic dependency Program -> ProgramTypes -> Program.GenericProgram *)
 type (!'trans_label, !'trans_label_cmp_wit, !'graph) t = {
   start : Location.t;
   graph : 'graph;
