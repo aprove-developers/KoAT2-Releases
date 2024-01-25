b17be71a5952519345d1f2338841285b934123dd
Nils Lommen <nils.lommen@rwth-aachen.de>
2024-01-25 14:22:36 +0100

    functors for twn size bounds

--- src/bounds/Solvable/TWNSizeBounds.mli
+++ src/bounds/Solvable/TWNSizeBounds.mli
@@ -1,4 +1,6 @@
-open ProgramModules
+module Make (PM : ProgramTypes.ClassicalProgramModules) : sig
+  module Approximation : module type of Approximation.MakeForClassicalAnalysis (Bounds.Bound) (PM)
 
-val improve : Program.t -> ?scc:TransitionSet.t option -> Approximation.t -> Approximation.t
-val reset_cfr : unit -> unit
+  val improve : PM.Program.t -> ?scc:PM.TransitionSet.t option -> Approximation.t -> Approximation.t
+  val reset_cfr : unit -> unit
+end
