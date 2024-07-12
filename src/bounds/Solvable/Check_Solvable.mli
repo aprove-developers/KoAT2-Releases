0adc597ea5769a21f4d6ee43840a867ac36f3508
Nils <nils.lommen@rwth-aachen.de>
2024-07-12 12:53:54 +0200

    remove unnecessary

--- src/bounds/Solvable/Check_Solvable.mli
+++ src/bounds/Solvable/Check_Solvable.mli
@@ -6,6 +6,5 @@ module Make (Bound : BoundType.Bound) (PM : ProgramTypes.ClassicalProgramModules
 
   val check_solvable : Loop.t -> blocks option
   val check_solvable_ : PM.Transition.t -> blocks option
-  val compute_closed_form_ : Loop.t -> Var.t list list -> (Var.t * ComplexPE.t) list
   val compute_closed_form : Loop.t -> (Var.t * ComplexPE.t) list option
 end
