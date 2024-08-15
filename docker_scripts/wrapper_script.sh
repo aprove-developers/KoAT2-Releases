9c9b7b46d2cf3834083a9c22af0c9452291aa285
Nils <nils.lommen@rwth-aachen.de>
2024-08-15 15:53:36 +0200

    update docker scripts

--- docker_scripts/wrapper_script.sh
+++ docker_scripts/wrapper_script.sh
@@ -1,19 +1,19 @@
 #! /usr/bin/env bash
 
-if [ "$1" = "its" ]; then
+if [[ "$1" = "its" ]] ; then
   timeout 300s koat2 ${@:2}
   KOAT_EXIT=$?
 fi
-if [ "$1" = "c" ]; then
+if [[ "$1" = "c" ]] ; then
   timeout 300s run_koat2_c.sh $@
   KOAT_EXIT=$?
 fi
-if [ "$1" = "smt2" ]; then
+if [[ "$1" = "smt2" ]] ; then
   timeout 300s run_koat2_smt2.sh $@
   KOAT_EXIT=$?
 fi
 # timeout returns 124
-if [ $KOAT_EXIT -eq 124 ]; then
+if [[ $KOAT_EXIT -eq 124 ]] ; then
   echo TIMEOUT
   exit 0
 fi
