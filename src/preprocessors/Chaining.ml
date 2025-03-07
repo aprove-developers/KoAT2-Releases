565ac838e0ab171a36071c31c4b156ab58f8eddb
nilslommen <lommen@cs.rwth-aachen.de>
2025-03-07 15:41:33 +0100

    fix bug in chaining

--- src/preprocessors/Chaining.ml
+++ src/preprocessors/Chaining.ml
@@ -14,26 +14,38 @@ let skip_location location graph =
   TransitionGraph.(pred_e, succ_e)
   |> Tuple2.mapn (fun f -> f graph location)
   |> Tuple2.mapn Sequence.of_list |> uncurry Sequence.cartesian_product
-  |> Sequence.map ~f:(fun ((l, t, l1), (l'1, t', l')) ->
+  |> Sequence.map ~f:(fun ((l, t, l1), (l', t', l1')) ->
          if TransitionLabel.has_rec_calls t || TransitionLabel.has_rec_calls t' then
-           Sequence.of_list [ (l, t, l1); (l'1, t', l') ]
+           Sequence.empty
          else
            let t_non_rec = TransitionLabel.to_non_rec t and t'_non_rec = TransitionLabel.to_non_rec t' in
            let chained =
              ( l,
                TransitionLabel.of_non_rec @@ TransitionLabel.TransitionLabelNonRec.append t_non_rec t'_non_rec,
-               l' )
+               l1' )
            in
            ProofOutput.add_str_paragraph_to_proof (fun () ->
                "Chain transitions "
                ^ Transition.to_id_string_pretty (l, t, l1)
                ^ " and "
-               ^ Transition.to_id_string_pretty (l'1, t', l')
+               ^ Transition.to_id_string_pretty (l', t', l1')
                ^ " to "
                ^ Transition.to_id_string_pretty chained);
            Sequence.of_list [ chained ])
   |> Sequence.concat
   |> (flip TransitionGraph.add_transitions) graph
+
+
+let unskippable_fc_transitions location graph =
+  TransitionGraph.(pred_e, succ_e)
+  |> Tuple2.mapn (fun f -> f graph location)
+  |> Tuple2.mapn Sequence.of_list |> uncurry Sequence.cartesian_product
+  |> Sequence.map ~f:(fun (t, t') ->
+         if Transition.has_rec_calls t || Transition.has_rec_calls t' then
+           Sequence.of_list [ t; t' ]
+         else
+           Sequence.empty)
+  |> Sequence.concat
 
 
 (** Returns true if the specific location is chainable in the graph. *)
@@ -50,9 +62,19 @@ let chainable ?(conservative = false) graph location : bool =
 
 
 (** Performs a chaining step removing the location from the graph. *)
-let chain location graph : TransitionGraph.t =
+let chain location graph : TransitionGraph.t MaybeChanged.t =
   let skipped = skip_location location graph in
-  TransitionGraph.succ_e skipped location |> List.fold ~f:TransitionGraph.remove_edge_e ~init:skipped
+  let removed_skipped =
+    TransitionGraph.succ_e skipped location |> List.fold ~f:TransitionGraph.remove_edge_e ~init:skipped
+  in
+  let add_unskippable =
+    Sequence.fold ~init:removed_skipped ~f:TransitionGraph.add_edge_e
+      (unskippable_fc_transitions location graph)
+  in
+  if TransitionGraph.equivalent graph add_unskippable then
+    MaybeChanged.same graph
+  else
+    MaybeChanged.changed add_unskippable
 
 
 (** Performs chaining on the TransitionGraph. *)
@@ -66,7 +88,7 @@ let transform_graph ?(conservative = false) ?(scc = None) (graph : TransitionGra
       && chainable ~conservative graph location
     then (
       Logger.(log logger INFO (fun () -> ("chaining", [ ("location", Location.to_string location) ])));
-      changed (chain location graph))
+      chain location graph)
     else
       same graph
   in
